c5a94228257bdcd7f4511097fe227078a8f9eeb8
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-04-02 17:49:22 -0700

    support removing

--- app/services/pipeline_service/commands/send.rb
+++ app/services/pipeline_service/commands/send.rb
@@ -3,7 +3,7 @@ module PipelineService
 
 
     class Send
-      attr_reader :message
+      attr_reader :message, :persisted_message
 
       MESSAGE_NAME = 'enrollment'
       SOURCE       = 'canvas'
@@ -14,6 +14,7 @@ module PipelineService
         @password          = ENV['PIPELINE_PASSWORD']
         @domain_name       = ENV['CANVAS_DOMAIN']
 
+
         raise 'Missing environment variables' if config_missing?
 
         @api_instance    = args[:message_api] || PipelinePublisher::MessagesApi.new
@@ -23,6 +24,7 @@ module PipelineService
         @enrollment      = args[:enrollment]
         @user            = args[:user]
         @queue           = args[:queue] || false
+        @message_type    = args[:message_type] || :upsert
       end
 
       def call
@@ -39,14 +41,17 @@ module PipelineService
 
       attr_reader :payload, :enrollment, :username, :password,
         :user, :api_instance, :payload, :publisher, :host,
-        :serializer, :domain_name, :message_builder, :queue, :job
+        :serializer, :domain_name, :message_builder, :queue, :job,
+        :message_type
 
       def persist
+        @persisted_message = HashWithIndifferentAccess.new(
+          JSON.parse(message.to_json)
+        ).delete_blank.to_json
+        
         HTTParty.post(
           "https://lrs.strongmind.com/pipeline-watcher-staging",
-          body: HashWithIndifferentAccess.new(
-            JSON.parse(message.to_json)
-          ).delete_blank.to_json
+          body: persisted_message
         )
       end
 
@@ -90,7 +95,7 @@ module PipelineService
             domain_name: domain_name
           },
           identifiers: { id: enrollment.id },
-          data: payload
+          data: message_type == :removed ? {} : payload
         )
       end
     end
--- spec/services/pipeline/commands/send_spec.rb
+++ spec/services/pipeline/commands/send_spec.rb
@@ -8,7 +8,7 @@ describe PipelineService::Commands::Send do
   let(:user)         { double('user') }
   let(:api)          { double('api', messages_post: nil) }
   let(:serializer)   { double('serializer', enrollment_json: {
-    :user=>{:first_name=>"Test", :last_name=>"User"}})
+    :user => { :first_name=>"Test", :last_name=>"User" }})
   }
   let(:test_message) { double('message') }
 
@@ -20,36 +20,45 @@ describe PipelineService::Commands::Send do
   end
 
   subject do
+    build
+  end
+
+  def build(message_type: :upserted)
     described_class.new(
       enrollment: enrollment,
       user: user,
       message_api: api,
       serializer: serializer,
-      queue: false
+      queue: false,
+      message_type: message_type
     )
   end
 
   describe '#call' do
-    before do
-      allow(subject).to receive(:build_pipeline_message) { test_message }
+    context 'upsert' do
+      before do
+        allow(subject).to receive(:build_pipeline_message) { test_message }
+      end
+
+      it 'sends a message to the pipeline' do
+        expect(api).to receive(:messages_post).with(test_message)
+        subject.call
+      end
     end
 
-    it 'sends a message to the pipeline' do
-      expect(api).to receive(:messages_post).with(test_message)
-      subject.call
-    end
-
-    it 'removes empty fields before persisting' do
-      subject.call
+    context 'delete' do
+      it 'sends a blank object if the message type is delete' do
+        expect(build(message_type: :removed).call.message.data).to eq(
+          { }
+        )
+      end
     end
   end
 
   describe "#message" do
     it 'shows the message that was sent' do
       expect(subject.call.message.data).to eq(
-        {
-          :user => { :first_name => "Test", :last_name => "User" }
-        })
+        { :user => { :first_name => "Test", :last_name => "User" }})
     end
   end
 
