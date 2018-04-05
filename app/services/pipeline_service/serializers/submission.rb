d8c30487c0399bfc4772c6da19fe85faf56e6602
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-04-05 16:13:55 -0700

    bye byebug

--- app/services/pipeline_service/serializers/submission.rb
+++ app/services/pipeline_service/serializers/submission.rb
@@ -3,20 +3,8 @@ module PipelineService
     # This ugly thing lets us call the canvas user api
     class Submission
       include Api::V1::Submission
-      # attr_accessor :services_enabled, :context, :current_user, :params, :request
-      #
-      # def service_enabled?(service); @services_enabled.include? service; end
-      #
-      # def avatar_image_url(*args); "avatar_image_url(#{args.first})"; end
-      #
-      # def course_student_grades_url(course_id, user_id); ""; end
-      #
-      # def course_user_url(course_id, user_id); ""; end
-      #
+
       def initialize(object:)
-      #   @domain_root_account = ::Account.default
-      #   @params = {}
-      #   @request = OpenStruct.new
         @object = object
         @admin = PipelineService::Account.account_admin
       end
@@ -30,7 +18,6 @@ module PipelineService
       end
 
       def call
-        byebug
         self.submission_json(@object, @object.assignment, @admin, {}, nil, [])
       end
     end
