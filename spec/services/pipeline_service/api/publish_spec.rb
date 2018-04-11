637fc79104991535474d089b368effe0ec949497
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-04-11 16:00:10 -0700

    clean up some formatting

--- spec/services/pipeline_service/api/publish_spec.rb
+++ spec/services/pipeline_service/api/publish_spec.rb
@@ -1,10 +1,10 @@
 describe PipelineService::API::Publish do
   let(:serializer_instance) { double('serializer', call: nil) }
   let(:pipeline_serializer) { double('serializer', new: serializer_instance) }
-  let(:submission) {double('submission', pipeline_serializer: pipeline_serializer, id: 1)}  
-  let(:queue_client) { double('queue_client') }
-  let(:command_instance) { double('command_class', call: nil) }
-  let(:command_class) { double('command_class', new: command_instance) }
+  let(:submission)          { double('submission', pipeline_serializer: pipeline_serializer, id: 1) }
+  let(:queue_client)        { double('queue_client') }
+  let(:command_instance)    { double('command_class', call: nil) }
+  let(:command_class)       { double('command_class', new: command_instance) }
 
   subject do
     described_class.new(
