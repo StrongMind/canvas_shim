314fa2c0ee113538bca5c32e69bf7d852d347acd
Elle Bayless <ebayless8315@fs1546mbpro.avaflipswitch.com>
2018-04-12 16:04:57 -0700

    include url helpers

--- app/services/pipeline_service/serializers/submission.rb
+++ app/services/pipeline_service/serializers/submission.rb
@@ -4,6 +4,11 @@ module PipelineService
     class Submission
       include ::Api
       include ::Api::V1::Submission
+      include ActionView::Helpers
+      include ActionDispatch::Routing::UrlFor
+      include Rails.application.routes.url_helpers
+
+
 
       def avatar_url_for_user(*);'';end
       def retrieve_course_external_tools_url(*);'';end
@@ -11,14 +16,14 @@ module PipelineService
       def params;{};end
       def polymorphic_url(*);'';end
 
-
-
       def initialize(object:)
+        default_url_options[:host] = ENV['CANVAS_DOMAIN']
         @object = object
         @admin = PipelineService::Account.account_admin
       end
 
       def call
+        @current_user = @admin
         submission_json(@object, @object.assignment, @admin, {}, nil, [])
       end
     end
