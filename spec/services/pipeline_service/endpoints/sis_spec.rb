16e667f96db006720db3e3c7bdfd331680270e78
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-04-20 15:36:29 -0700

    format to shorten lines

--- spec/services/pipeline_service/endpoints/sis_spec.rb
+++ spec/services/pipeline_service/endpoints/sis_spec.rb
@@ -1,7 +1,8 @@
 describe PipelineService::Endpoints::SIS do
-  let(:message) { {noun: 'student_enrollment', id: 1, data: {state: 'completed'} } }
+  let(:message) do
+    {noun: 'student_enrollment', id: 1, data: {state: 'completed'} }
+  end
   let(:http_client) { double("HTTPClient") }
-
 
   before do
     ENV['SIS_ENROLLMENT_UPDATE_API_KEY'] = 'key'
@@ -9,15 +10,27 @@ describe PipelineService::Endpoints::SIS do
   end
 
   describe '#call' do
-    subject { described_class.new(message: message, args: { http_client: http_client }) }
+    subject do
+      described_class.new(
+        message: message,
+        args: { http_client: http_client }
+      )
+    end
     it 'will post student enrollments' do
       expect(http_client).to receive(:post)
       subject.call
     end
 
     context 'filtered' do
-      let(:message) { { noun: 'teacher_enrollment', id: 1, data: { state: 'completed' } } }
-      subject { described_class.new(message: message, args: { http_client: http_client }) }
+      let(:message) do
+        { noun: 'teacher_enrollment', id: 1, data: { state: 'completed' } }
+      end
+      subject do
+        described_class.new(
+          message: message,
+          args: { http_client: http_client }
+        )
+      end
       it 'will only post student enrollments' do
         expect(http_client).to_not receive(:post)
         subject.call
