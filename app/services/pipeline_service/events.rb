416f0e218c6cebfb2b19f824007c2dcbbec51379
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-04-26 11:34:19 -0700

    dont need this interface

--- app/services/pipeline_service/events.rb
+++
@@ -1,8 +0,0 @@
-module PipelineService
-  module Events
-    def self.parse_pipeline_message(message, args={})
-      (args[:command] || Commands::RunEvents)
-        .new(message).call
-    end
-  end
-end
