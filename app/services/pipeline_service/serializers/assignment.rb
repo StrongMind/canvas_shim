044c4a6254312e0c715f8849e8fb20b077057cf8
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-05-01 15:17:28 -0700

    add an assignment serializer

---
+++ app/services/pipeline_service/serializers/assignment.rb
@@ -0,0 +1,34 @@
+module PipelineService
+  # This ugly thing lets us call the canvas assignment api
+  module Serializers
+    class Assignment
+      include ::Api
+      include ::Api::V1::Assignment
+      include ActionView::Helpers
+      include ActionDispatch::Routing::UrlFor
+      include Rails.application.routes.url_helpers
+
+      def initialize(object:)
+        default_url_options[:host] = host
+        @object = object
+      end
+
+      def call
+        fetch_admin
+        assignment_json(object, admin, {})
+      end
+
+      private
+
+      attr_reader :object, :admin, :host
+
+      def fetch_admin
+        @admin = PipelineService::Account.account_admin
+      end
+
+      def host
+        ENV['CANVAS_DOMAIN']
+      end
+    end
+  end
+end
