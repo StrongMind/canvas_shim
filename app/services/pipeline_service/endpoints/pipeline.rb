8d228bd7d3c1ca86a63b32c86a01265d070a72dc
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-05-01 10:57:15 -0700

    objects add #peform command to objects that can be backgrounded

--- app/services/pipeline_service/endpoints/pipeline.rb
+++ app/services/pipeline_service/endpoints/pipeline.rb
@@ -1,21 +1,49 @@
 module PipelineService
   module Endpoints
     class Pipeline
-      def initialize(message:, args: {})
-        @message     = message
+      def initialize(message, args={})
+        @message = message
+        @message_builder_class = args[:message_builder_class] || MessageBuilder
+        @http_client = args[:http_client] || PipelinePublisher::MessagesApi
+        @publisher   = args[:publisher] || PipelinePublisher
+        @endpoint    = ENV['PIPELINE_ENDPOINT']
+        @username    = ENV['PIPELINE_USER_NAME']
+        @password    = ENV['PIPELINE_PASSWORD']
         @args        = args
+        raise 'Missing config' if missing_config?
       end
 
       def call
-        enqueue
+        Delayed::Job.enqueue(self)
+      end
+
+      def peform
+        post
       end
 
       private
 
       attr_reader :message, :args
 
-      def enqueue
-        Delayed::Job.enqueue PostMessageJob.new(message, args)
+      attr_reader :http_client, :message, :message_builder_class, :endpoint,
+        :username, :password, :publisher
+
+      def missing_config?
+        [endpoint, username, password].any?(&:nil?)
+      end
+
+      def configure_publisher
+        publisher.configure do |config|
+          config.host     = endpoint
+          config.username = username
+          config.password = password
+        end
+      end
+
+      def post
+        http_client.new.messages_post(
+          message_builder_class.new(message).build
+        )
       end
     end
   end
