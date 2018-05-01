b89f6759fe8e20a22b449ee0e8acfd78af596ca3
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-05-01 11:05:45 -0700

    update tests

--- app/services/pipeline_service/api/publish.rb
+++ app/services/pipeline_service/api/publish.rb
@@ -5,10 +5,11 @@ module PipelineService
         @object         = object
         @command_class  = args[:command_class] || PipelineService::Commands::Publish
         @args = args
+        @queue = args[:queue] || Delayed::Job
       end
 
       def call
-        Delayed::Job.enqueue self
+        queue.enqueue self
       end
 
       def perform
@@ -17,7 +18,7 @@ module PipelineService
 
       private
 
-      attr_reader :object, :jobs, :command_class, :args
+      attr_reader :object, :jobs, :command_class, :args, :queue
 
       def subscriptions
         Events::Subscription.new(event: 'graded_out', responder: Events::Responders::SIS)
--- spec/services/pipeline_service/api/publish_spec.rb
+++ spec/services/pipeline_service/api/publish_spec.rb
@@ -1,21 +1,21 @@
 describe PipelineService::API::Publish do
-  # let(:serializer_instance) { double('serializer_instance', call: nil) }
-  # let(:pipeline_serializer) { double('serializer_class', new: serializer_instance) }
   let(:serializer_fetcher)  { double('fetcher') }
   let(:submission)          { double('submission', id: 1, class: 'Submission', assignment: double('assignment')) }
-  let(:command_instance)    { double('command_class', call: nil) }
+  let(:command_instance)    { double('command_instance', call: nil) }
   let(:command_class)       { double('command_class', new: command_instance) }
+  let(:queue)               { double('queue') }
 
   subject do
     described_class.new(
       submission,
-      command_class: command_class
+      command_class: command_class,
+      queue: queue
     )
   end
 
   describe '#call' do
     it 'enqueues' do
-      expect(command_instance).to receive(:call)
+      expect(queue).to receive(:enqueue).with(subject)
       subject.call
     end
   end
