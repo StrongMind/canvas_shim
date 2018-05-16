7e427eb5110e29a084fc7bf0a2ebe19a5dbe8973
Chris Young <cyoung1664@FS1548MBPRO.local>
2018-05-16 08:20:05 -0700

    use api in assignment call

--- app/services/pipeline_service/serializers/assignment.rb
+++ app/services/pipeline_service/serializers/assignment.rb
@@ -1,39 +1,62 @@
 module PipelineService
-  # This ugly thing lets us call the canvas assignment api
   module Serializers
     class Assignment
-      include ::Api
-      include ::Api::V1::Assignment
-      include ActionView::Helpers
-      include ActionDispatch::Routing::UrlFor
-      include Rails.application.routes.url_helpers
-
-      include BaseMethods
+      KEY = 'IQQI1GysDSk8XdjZheKZ3eaa8Qu5J9ZfB2pZ8YMEVgWMvzXInpTv15Frh0GGEZ0l'
 
       def initialize(object:)
-        default_url_options[:host] = host
         @object = object
-        @domain_root_account = ::Account.default
-        @params = {}
-        @request = OpenStruct.new
-        @admin = PipelineService::Account.account_admin
       end
 
       def call
-        fetch_admin
-        assignment_json(object, admin, {})
+        post
       end
 
       private
 
-      attr_reader :object, :admin, :host
+      attr_reader :object
 
-      def fetch_admin
-        @admin = PipelineService::Account.account_admin
+      def domain
+        ENV['CANVAS_DOMAIN']
       end
 
-      def host
-        ENV['CANVAS_DOMAIN']
+      def endpoint
+        [
+          protocol,
+          domain,
+          ':',
+          port,
+          '/api/v1/courses/',
+          course_id,
+          '/assignments/',
+          object.id
+        ].join('')
+      end
+
+      def port
+        '3000'
+      end
+
+      def headers
+        { Authorization: "Bearer #{KEY}" }
+      end
+
+      def post
+        http_client.get(
+          endpoint, headers: headers
+        ).parsed_response
+      end
+
+      def course_id
+        object.course.id
+      end
+
+      def http_client
+        HTTParty
+      end
+
+      def protocol
+        return 'https://' if ENV['CANVAS_SSL'] == 'true'
+        'http://'
       end
     end
   end
