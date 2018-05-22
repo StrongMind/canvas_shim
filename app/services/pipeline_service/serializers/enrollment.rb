f8de2c7cac25ff64fc4c1de5487d4b08828ad64e
Chris Young <cyoung1664@FS1548MBPRO.local>
2018-05-22 10:33:28 -0700

    start using the api to fetch enrollments

--- app/services/pipeline_service/serializers/enrollment.rb
+++ app/services/pipeline_service/serializers/enrollment.rb
@@ -1,30 +1,60 @@
 module PipelineService
-  # This ugly thing lets us call the canvas user api
   module Serializers
     class Enrollment
-      include ::Api::V1::User
-      include BaseMethods
-      
-      attr_accessor :services_enabled, :context, :current_user, :params, :request
-
-      def service_enabled?(service); @services_enabled.include? service; end
-
-      def avatar_image_url(*args); "avatar_image_url(#{args.first})"; end
-
-      def course_student_grades_url(course_id, user_id); ""; end
-
-      def course_user_url(course_id, user_id); ""; end
-
       def initialize(object:)
-        @domain_root_account = ::Account.default
-        @params = {}
-        @request = OpenStruct.new
         @object = object
-        @admin = PipelineService::Account.account_admin
       end
 
       def call
-        enrollment_json(@object, @admin, {})
+        fetch
+      end
+
+      private
+
+      attr_reader :object
+
+      def domain
+        ENV['CANVAS_DOMAIN']
+      end
+
+      def endpoint
+        [
+          protocol,
+          domain,
+          ':',
+          port,
+          '/api/v1/accounts/',
+          account_id,
+          '/enrollments/',
+          object.id
+        ].join('')
+      end
+
+      def use_ssl?
+        ENV['CANVAS_SSL'] == 'true'
+      end
+
+      def port
+        return '3000' if Rails.env == 'development'
+        return '443' if use_ssl?
+        '80'
+      end
+
+      def headers
+        { Authorization: "Bearer #{PipelineService::TokenBuilder.build}" }
+      end
+
+      def fetch
+        PipelineService::HTTPClient.get(endpoint, headers: headers).parsed_response
+      end
+
+      def account_id
+        object.root_account.id
+      end
+
+      def protocol
+        return 'https://' if use_ssl?
+        'http://'
       end
     end
   end
