af9b71eeefb144a80c8c7e4f40f3b78f6a32b045
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-05-25 17:35:42 -0700

    remove hash_spec

--- spec/services/pipeline_service/serializers/hash_spec.rb
+++
@@ -1,8 +0,0 @@
-describe PipelineService::Serializers::Hash do
-  let(:my_hash) { {} }
-  subject { described_class.new(object: my_hash) }
-
-  it 'returns the object unchanged' do
-    expect(subject.call).to eq my_hash
-  end
-end
