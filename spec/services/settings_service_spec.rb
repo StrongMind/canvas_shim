43b75fa6eedb696e08ae9ab10531b4b00c1776b9
Rawya Husain <rawya.husain@strongmind.com>
2018-05-31 09:56:43 -0700

    Play-503: Added User Endpoint

--- spec/services/settings_service_spec.rb
+++ spec/services/settings_service_spec.rb
@@ -30,4 +30,34 @@ describe SettingsService do
       )
     end
   end
+
+  describe '#get_user_settings' do
+    it 'calls the update setting command' do
+      expect(
+        SettingsService::Commands::GetUserSettings
+      ).to receive(:new).with(
+        id: 1
+      ).and_return(double('command', call: nil))
+
+      described_class.get_user_settings(id: 1)
+    end
+  end
+
+  describe '#update_user_setting' do
+    it 'calls the get settings command' do
+      expect(
+        SettingsService::Commands::UpdateUserSetting
+      ).to receive(:new).with(
+        id: 1,
+        setting: "foo",
+        value: "bar"
+      ).and_return(double('command', call: nil))
+
+      described_class.update_user_setting(
+        id: 1,
+        setting: 'foo',
+        value: 'bar'
+      )
+    end
+  end
 end
