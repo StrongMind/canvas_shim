98ff261b7e78c044ac01cd1539a00ad102bc4a29
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-06-04 17:20:48 -0700

    accept json in the body

--- app/services/settings_service/rest_api.rb
+++ app/services/settings_service/rest_api.rb
@@ -4,29 +4,27 @@ module SettingsService
   class RestAPI < Grape::API
     Grape::Middleware::Auth::Strategies.add(:auth_by_token, AuthMiddleware, ->(options) { [options[:realm]] })
     format :json
+    default_format :json
     version 'v1'
 
     auth :auth_by_token, { realm: 'Settings Service API'} do |credentials|
-    #   byebug
-    #   # lookup the user's password here
-    #   # AccessToken.authenticate(token_string, token_key = :crypted_token)
-    #   # { 'user1' => 'password1' }[username]
+      byebug
+      #   # lookup the user's password here
+      #   # AccessToken.authenticate(token_string, token_key = :crypted_token)
+      #   # { 'user1' => 'password1' }[username]
     end
 
     resource :enrollments do
       route_param :id do
         desc 'Get settings associated with a user'
         get do
+          puts "This is executable!"
           {type: 'enrollments', setting2: 'yes'}
         end
 
         desc 'Add settings to users'
         put do
-          update_enrollment_setting(
-            id: params[:enrollment_id],
-            setting: params[:id],
-            value: params[:value]
-          )
+          {}
         end
       end
     end
@@ -34,11 +32,21 @@ module SettingsService
     resource :users do
       route_param :id do
         get do
-          { type: 'users', setting2: 'no' }
+          puts "This is executable!"
+          {type: 'users', setting2: 'no'}
         end
 
-        put do
-          { }
+        params do
+          optional :json, type: JSON
+        end
+
+        post do
+          SettingsService.update_user_setting(
+            id: params[:id],
+            setting: params[:json].keys.first,
+            value: params[:json].values.first
+          )
+          params
         end
       end
     end
