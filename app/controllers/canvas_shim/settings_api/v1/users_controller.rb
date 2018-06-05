905e714bda5e3d9e308ca59e6a14f24442f6284d
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-06-05 16:10:18 -0700

    check auth and permission in users controller.rb

--- app/controllers/canvas_shim/settings_api/v1/users_controller.rb
+++ app/controllers/canvas_shim/settings_api/v1/users_controller.rb
@@ -4,6 +4,7 @@ module CanvasShim
       class UsersController < ApplicationController
         skip_before_action :verify_authenticity_token
         before_action :validate_key
+
         def update
           settings.each do |key, value|
             SettingsService.update_user_setting(
@@ -11,6 +12,7 @@ module CanvasShim
               setting: key,
               value: value
             )
+            render json: {status: 'ok'}
           end
         end
 
@@ -20,14 +22,16 @@ module CanvasShim
           JSON.parse(params[:settings])
         end
 
-        def validate_key
-          token = AccessToken.authenticate(headers['Authorization'].gsub('Bearer ', ''))
-          if token
-            token.user.system_admin?
-          else
-            render status: 402, 'Bad token'
-          end
+        def valid_token?(token)
+          return false if token.nil?
+          token.user.user_roles(Account.site_admin).include?('root_admin')
+        end
 
+        def validate_key
+          token = AccessToken.authenticate(request.headers['HTTP_AUTHORIZATION'].try(:gsub, 'Bearer ', ''))
+
+          return true if valid_token?(token)
+          render(json: {status: 'error'}, status: 401)
         end
       end
     end
