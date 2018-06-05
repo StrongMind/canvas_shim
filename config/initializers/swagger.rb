7a78b0b513ba64c87b908a92726db013f1b37402
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-06-05 14:01:15 -0700

    turn off api docs

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,10 +1,10 @@
-GrapeSwaggerRails.options.url      = '/swagger_doc.json'
-GrapeSwaggerRails.options.url = '/canvas_shim/settings_api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_name = 'SettingsService'
-GrapeSwaggerRails.options.api_auth = 'bearer'
-GrapeSwaggerRails.options.api_key_name = 'Authorization'
-GrapeSwaggerRails.options.api_key_type = 'header'
-
-GrapeSwaggerRails.options.before_action do
-  GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
-end
+# GrapeSwaggerRails.options.url      = '/swagger_doc.json'
+# GrapeSwaggerRails.options.url = '/canvas_shim/settings_api/v1/swagger_doc'
+# GrapeSwaggerRails.options.app_name = 'SettingsService'
+# GrapeSwaggerRails.options.api_auth = 'bearer'
+# GrapeSwaggerRails.options.api_key_name = 'Authorization'
+# GrapeSwaggerRails.options.api_key_type = 'header'
+#
+# GrapeSwaggerRails.options.before_action do
+#   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
+# end
