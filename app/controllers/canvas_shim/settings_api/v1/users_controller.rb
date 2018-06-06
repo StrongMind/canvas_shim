2c216fc430afaa156ba8aef94f6023a528305e71
Chris Young <cyoung1664@FS1548MBPRO.local>
2018-06-06 07:55:35 -0700

    stop using swagger docs

--- app/controllers/canvas_shim/settings_api/v1/users_controller.rb
+++ app/controllers/canvas_shim/settings_api/v1/users_controller.rb
@@ -3,13 +3,6 @@ module CanvasShim
   module SettingsApi
     module V1
       class UsersController < ApplicationController
-        swagger_controller :users, 'Users'
-        swagger_api :update do
-          param   :body, :settings, :settings, :required, "A hash of settings"
-          summary 'Updates a user setting'
-          notes   'Format: {"yoursetting": "setting_value"}'
-        end
-
         skip_before_action :verify_authenticity_token
         before_action :validate_key
 
@@ -37,7 +30,7 @@ module CanvasShim
 
         def validate_key
           return true
-          token = AccessToken.authenticate(request.headers['HTTP_AUTHORIZATION'].try(:gsub, 'Bearer ', ''))
+          token = SettingsService::AuthToken.authenticate(request.headers['HTTP_AUTHORIZATION'].try(:gsub, 'Bearer ', ''))
 
           return true if valid_token?(token)
           render(json: {status: 'error'}, status: 401)
