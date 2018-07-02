33c69a30f430c65d872d2133c83e6863111b978d
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2018-07-02 11:08:13 -0700

    only zero out if setting is true

--- spec/services/grades_service/commands/zero_out_assignment_grades_spec.rb
+++ spec/services/grades_service/commands/zero_out_assignment_grades_spec.rb
@@ -7,6 +7,9 @@ describe GradesService::Commands::ZeroOutAssignmentGrades do
     subject { described_class.new(assignment) }
 
     context "when the assignment is on time" do
+      before(:each) do
+        ENV['ZERO_OUT_PASTDUE_ASSIGNMENTS'] = 'true'
+      end
       let(:assignment) { double('assignment', due_date: Time.zone.now)}
 
       it "do nothing" do
@@ -16,6 +19,9 @@ describe GradesService::Commands::ZeroOutAssignmentGrades do
     end
 
     context 'when the assignment is past due' do
+      before(:each) do
+        ENV['ZERO_OUT_PASTDUE_ASSIGNMENTS'] = 'true'
+      end
       let(:assignment) do
         double(
           'past due assignment, with student submission',
@@ -25,7 +31,28 @@ describe GradesService::Commands::ZeroOutAssignmentGrades do
         )
       end
 
+      context "and zero out assignment setting is off" do
+        before(:each) do
+          ENV['ZERO_OUT_PASTDUE_ASSIGNMENTS'] = 'false'
+        end
+        let(:assignment) do
+          double(
+            'past due assignment, with student submission',
+            due_date: 2.hours.ago,
+            context: context,
+            submissions: []
+          )
+        end
+        it "do nothing" do
+          expect(subject).to_not receive(:students_without_submissions)
+          subject.call!
+        end
+      end
+
       context "and the student has a submission" do
+        before(:each) do
+          ENV['ZERO_OUT_PASTDUE_ASSIGNMENTS'] = 'true'
+        end
         let(:submission) { double('submission', student: student) }
 
         it 'will not grade the student' do
@@ -35,6 +62,9 @@ describe GradesService::Commands::ZeroOutAssignmentGrades do
       end
 
       context "and the student does not have a submission" do
+        before(:each) do
+          ENV['ZERO_OUT_PASTDUE_ASSIGNMENTS'] = 'true'
+        end
         let(:submission) { double('submission', student: nil) }
         before do
           allow(assignment).to receive(:submissions).and_return([])
