36212f8652315648e1ebb28dfb5cdc38d05fafdc
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-07-05 18:11:48 -0700

    refactor to avoid counting

--- app/services/courses_service/commands/distribute_due_dates.rb
+++ app/services/courses_service/commands/distribute_due_dates.rb
@@ -5,15 +5,12 @@ module CoursesService
         @course = args[:course]
         @startdate = course.start_at
         @enddate = course.end_at
-        @assignments_per_day = (assignments.count / course_days).to_i
-        @course_day = 1
+        @assignments_per_day = (assignments.count / course_days_count).to_i
       end
 
       def call
-        assignment_groups.each do |assignment_group|
-          skip_weekends
-          update_assignments(assignment_group)
-          @course_day += 1 if day_within_course?
+        assignment_groups.each.with_index do |assignment_group, i|
+          update_assignments(assignment_group, course_days[i])
         end
       end
 
@@ -21,24 +18,8 @@ module CoursesService
 
       attr_reader :course, :startdate, :enddate, :assignments_per_day
 
-      def update_assignments(assignment_group)
-        assignment_group.each do |assignment|
-          assignment.update(due_at: startdate + @course_day.day)
-        end
-      end
-
-      def skip_weekends
-        while weekend? && day_within_course?(@course_day) do
-          @course_day += 1
-        end
-      end
-
-      def day_within_course?
-        @course_day < course_days
-      end
-
-      def weekend?
-        !calendar.business_day?(startdate + @course_day.day)
+      def update_assignments(assignment_group, day)
+        assignment_group.each { |assignment| assignment.update(due_at: day) }
       end
 
       def assignment_groups
@@ -46,10 +27,13 @@ module CoursesService
       end
 
       def assignments
-        ContentTag.where(
-          content_type: 'Assignment',
-          context_id: course.id
-        ).order(:position).map { |tag| tag.assignment }
+        content_tags.map { |tag| tag.assignment }
+      end
+
+      def content_tags
+        ContentTag
+          .where(content_type: 'Assignment', context_id: course.id)
+          .order(:position)
       end
 
       def calendar
@@ -57,6 +41,13 @@ module CoursesService
       end
 
       def course_days
+        course_days_count.times.map do |i|
+          day = startdate + i.days
+          day unless !calendar.business_day?(day)
+        end.compact
+      end
+
+      def course_days_count
         calendar.business_days_between(startdate, enddate)
       end
     end
