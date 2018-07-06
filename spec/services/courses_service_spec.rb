736f5248ccb245d88dc1ab7ba478c3e7b56bd00e
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-07-06 12:55:42 -0700

    make sure the command instance gets called

--- spec/services/courses_service_spec.rb
+++ spec/services/courses_service_spec.rb
@@ -1,15 +1,16 @@
 describe CoursesService do
   describe '#distribute_due_dates' do
     let(:instance) { double(:command_instance, call: nil) }
-    let(:course) { double(:course) }
-    let(:command) {CoursesService::Commands::DistributeDueDates}
+    let(:course)   { double(:course) }
+    let(:command)  { CoursesService::Commands::DistributeDueDates }
+
     before do
       allow(command).to receive(:new).and_return(instance)
     end
+
     it 'calls the command' do
-      expect(command).to receive(:new)
+      expect(instance).to receive(:call)
       described_class.distribute_due_dates(course: course)
     end
   end
-
 end
