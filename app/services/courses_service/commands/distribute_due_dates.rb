c08641bb680eb0026bfc357d1b3e8c1b0590cf01
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-07-05 17:45:57 -0700

    beginning of test

--- app/services/courses_service/commands/distribute_due_dates.rb
+++ app/services/courses_service/commands/distribute_due_dates.rb
@@ -1,26 +1,19 @@
-require 'business'
 module CoursesService
   module Commands
     class DistributeDueDates
-      def initialize(course)
-        @course = course
+      def initialize(args)
+        @course = args[:course]
         @startdate = course.start_at
         @enddate = course.end_at
-        @assignments_per_day = (assignments.count / business_days).to_i
+        @assignments_per_day = (assignments.count / course_days).to_i
+        @course_day = 1
       end
 
       def call
-        day = 1
-        assignments.in_groups_of(assignments_per_day, false).each do |assignment_group|
-          while !calendar.business_day?(startdate + day.day) && day < business_days do
-            day = day + 1
-          end
-
-          assignment_group.each do |assignment|
-            assignment.update(due_at: startdate + day.day)
-          end
-
-          day = day + 1
+        assignment_groups.each do |assignment_group|
+          skip_weekends
+          update_assignments(assignment_group)
+          @course_day += 1 if day_within_course?
         end
       end
 
@@ -28,21 +21,44 @@ module CoursesService
 
       attr_reader :course, :startdate, :enddate, :assignments_per_day
 
+      def update_assignments(assignment_group)
+        assignment_group.each do |assignment|
+          assignment.update(due_at: startdate + @course_day.day)
+        end
+      end
+
+      def skip_weekends
+        while weekend? && day_within_course?(@course_day) do
+          @course_day += 1
+        end
+      end
+
+      def day_within_course?
+        @course_day < course_days
+      end
+
+      def weekend?
+        !calendar.business_day?(startdate + @course_day.day)
+      end
+
+      def assignment_groups
+        assignments.in_groups_of(assignments_per_day, false)
+      end
+
       def assignments
         ContentTag.where(
           content_type: 'Assignment',
-          context_id: Course.first.id
-        ).order(:position).map {|tag| tag.assignment }
+          context_id: course.id
+        ).order(:position).map { |tag| tag.assignment }
       end
 
       def calendar
         Business::Calendar.new(working_days: %w( mon tue wed thu fri ))
       end
 
-      def business_days
+      def course_days
         calendar.business_days_between(startdate, enddate)
       end
-
     end
   end
 end
---
+++ spec/services/courses_service/commands/distribute_due_dates_spec.rb
@@ -0,0 +1,29 @@
+class ContentTag
+end
+
+describe CoursesService::Commands::DistributeDueDates do
+  let(:course) do
+    double(
+      'course',
+      start_at: Date.today,
+      end_at: Date.today + 1.day,
+      id: 1
+    )
+  end
+
+  let(:assignment) { double(:assignment, update: nil) }
+  let(:content_tag) { double(:content_tag, assignment: assignment) }
+  let(:content_tags) { double(:content_tags, order: [content_tag]) }
+
+  subject { described_class.new(course: course) }
+
+  before do
+    allow(ContentTag).to receive(:where).and_return(content_tags)
+  end
+
+  describe "#call" do
+    it 'runs' do
+      subject.call
+    end
+  end
+end
