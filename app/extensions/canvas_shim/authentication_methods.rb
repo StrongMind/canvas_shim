d32af205abaefcac4ec5c771670dcd2124bfd5c2
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-08-01 12:19:57 -0700

    test that proves things blow up

--- app/extensions/canvas_shim/authentication_methods.rb
+++ app/extensions/canvas_shim/authentication_methods.rb
@@ -6,12 +6,15 @@ module CanvasShim
       sis_id = @current_user && @current_user.pseudonym && @current_user.pseudonym.sis_user_id
       return unless sis_id
 
-      lock_out = HTTParty.get(
-        "https://flms.flipswitch.com/AttendanceTwo/IsLockedOut?schoolId=17&studentId=#{sis_id}",
-        headers: { "CanvasAuth" => "98454055-2148-4F9B-A170-FD61562998CE" }
-      ).body == 'true'
+      begin
+        lock_out = HTTParty.get(
+          "https://flms.flipswitch.com/AttendanceTwo/IsLockedOut?schoolId=17&studentId=#{sis_id}",
+          headers: { "CanvasAuth" => "98454055-2148-4F9B-A170-FD61562998CE" }
+        ).body == 'true'
 
-      redirect_to('https://flms.flipswitch.com') if lock_out
+        redirect_to('https://flms.flipswitch.com') if lock_out
+      rescue
+      end
     end
   end
 end
--- spec/extensions/canvas_shim/authentication_methods_spec.rb
+++ spec/extensions/canvas_shim/authentication_methods_spec.rb
@@ -5,6 +5,7 @@ end
 describe CanvasShim::AuthenticationMethods do
   subject { described_class }
   let(:controller) { SomeRailsController.new }
+  let(:response) { double('response', body: 'true') }
 
   describe '#load_user' do
     before do
@@ -24,6 +25,13 @@ describe CanvasShim::AuthenticationMethods do
       it 'does not redirect if not locked out' do
         expect(controller).to_not receive(:redirect_to)
         controller.load_user
+      end
+    end
+
+    context 'HTTP Library blows up' do
+      it 'fails silently' do
+        expect(HTTParty).to receive(:get).and_raise('boom')
+        expect {controller.load_user}.to_not raise_error
       end
     end
   end
