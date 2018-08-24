731d5965919ac9e197b96afd71ff1132cde6e4bb
Chris Young <cyoung1664@FS1548MBPRO.local>
2018-08-24 16:56:01 -0700

    guard for nil in api

--- app/services/grades_service.rb
+++ app/services/grades_service.rb
@@ -1,5 +1,6 @@
 module GradesService
   def self.zero_out_grades!
+    return unless SettingsService.get_settings(object: :school, id: 1)['zero_out_past_due'] == 'on'
     ::Assignment.find_each do |assignment|
       Commands::ZeroOutAssignmentGrades.new(assignment).call!
     end
