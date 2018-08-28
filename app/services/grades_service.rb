15224de45bcdc5bcf7ab997430dcbd6e31ed45e0
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-08-28 09:35:35 -0700

    dont check for nil

--- app/services/grades_service.rb
+++ app/services/grades_service.rb
@@ -4,13 +4,14 @@ module GradesService
       return unless SettingsService.get_settings(object: :school, id: 1)['zero_out_past_due'] == 'on'
     end
 
+    # Question: Do we get assignments with no submissions?
     ::Assignment
       .left_outer_joins(:submissions)
       .where.not(due_at: nil)
       .where('due_at < ?', 1.hour.ago)
-      .where('submissions.score in (?)', [nil, 0])
+      .where('submissions.score = ?', nil)
       .distinct
-      .in_batches(of: 1000) do |assignments|
+      .in_batches do |assignments|
         assignments.each do |assignment|
           GradesService::Commands::ZeroOutAssignmentGrades.new(assignment).call!
         end
