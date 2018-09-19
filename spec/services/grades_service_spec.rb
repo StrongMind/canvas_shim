5a6de80651ed111fdf04c728eaebdcc0ee48f147
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2018-09-19 16:38:26 -0700

    test to prove we only call the command once when we have duplicate ids

--- spec/services/grades_service_spec.rb
+++ spec/services/grades_service_spec.rb
@@ -1,21 +1,25 @@
 describe GradesService do
-  let(:assignment) { double('assignment') }
+  let(:assignment) { double('assignment', id: 1) }
   let(:instance) { double('command_instance', call!: nil)}
-  let(:scope) { double('scope') }
+  let(:scope) { double('scope', map: [assignment]) }
 
   before do
     allow(GradesService::Commands::ZeroOutAssignmentGrades)
       .to receive(:new).and_return(instance)
-
-    allow(scope).to receive(:in_batches).and_yield([assignment])
 
     allow(GradesService).to receive(:scope).and_return(scope)
     allow(GradesService::Commands::ZeroOutAssignmentGrades).to receive(:new).and_return(instance)
     allow(SettingsService).to receive(:get_settings).and_return({'zero_out_past_due' => 'on'})
   end
 
-  it 'calls the command' do
-    expect(instance).to receive(:call!)
-    described_class.zero_out_grades!(seconds_to_sleep: 0)
+
+  context "when there are duplicate ids in the scope" do
+    let(:assignments) {[assignment, assignment, assignment2, assignment2]}
+    let(:scope) { double('scope', map: assignments) }
+    let(:assignment2) { double('assignment', id: 2) }
+    it "the command is only called once per id" do
+      expect(instance).to receive(:call!).twice
+      described_class.zero_out_grades!
+    end
   end
 end
