37086fa87633bcc06657284aa604814e07d31935
Tesla <tesla@adminisatorspro.avaflipswitch.com>
2018-10-16 14:48:52 -0700

    Add rake tasks

--- lib/canvas_shim/engine.rb
+++ lib/canvas_shim/engine.rb
@@ -1,6 +1,8 @@
 module CanvasShim
   class Engine < ::Rails::Engine
     config.autoload_paths << File.expand_path("app/services", __dir__)
+    config.autoload_paths << File.expand_path("app/deploy", __dir__)
+    
     isolate_namespace CanvasShim
 
     config.generators do |g|
