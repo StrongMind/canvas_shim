2b9aa81e8d3e4d99af6dca31d884843b84e1d555
Tesla <tesla@adminisatorspro.avaflipswitch.com>
2018-10-17 10:19:43 -0700

    remove unused variable, and code

--- app/deploy/canvas_shim_asset_uploader.rb
+++ app/deploy/canvas_shim_asset_uploader.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk-s3'
 class CanvasShimAssetUploader < Canvas::Cdn::S3Uploader
-  BUCKET_NAME = ENV['CDN_OBJECT_STORE'] || 'canvas-docker-cdn-dev'
+  BUCKET_NAME = ENV['ASSET_OBJECT_STORE'] || ''
   REGION = ENV['AWS_REGION'] || 'us-west-2'
   ACCESS_KEY_ID = ENV['S3_ACCESS_KEY_ID'] || ''
   SECRET_ACCESS_KEY = ENV['S3_ACCESS_KEY'] || ''
@@ -8,7 +8,6 @@ class CanvasShimAssetUploader < Canvas::Cdn::S3Uploader
   def initialize(folder='dist')
     require 'aws-sdk-s3'
     @folder = folder
-    @config = Canvas::Cdn.config
     @s3 = Aws::S3::Resource.new(
       access_key_id: ACCESS_KEY_ID,
       secret_access_key: SECRET_ACCESS_KEY,
@@ -17,8 +16,4 @@ class CanvasShimAssetUploader < Canvas::Cdn::S3Uploader
     @bucket = @s3.bucket(BUCKET_NAME)
     @mutex = Mutex.new
   end
-
-  # def upload!
-  #   super
-  # end
 end
