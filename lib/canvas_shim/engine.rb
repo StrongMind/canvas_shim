9ab222a70adf79787ff7b17b53a1e09b8b6698bc
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-10-25 14:38:00 -0700

    require delayed job

--- lib/canvas_shim/engine.rb
+++ lib/canvas_shim/engine.rb
@@ -1,8 +1,9 @@
+require 'delayed_job'
 module CanvasShim
   class Engine < ::Rails::Engine
     config.autoload_paths << File.expand_path("app/services", __dir__)
     config.autoload_paths << File.expand_path("app/deploy", __dir__)
-    
+
     isolate_namespace CanvasShim
 
     config.generators do |g|
