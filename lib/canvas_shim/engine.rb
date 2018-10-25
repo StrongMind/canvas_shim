5b14eb08cb10c24c6fd2bb9811c625e16cc2144e
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-10-25 15:52:40 -0700

    include things more right

--- lib/canvas_shim/engine.rb
+++ lib/canvas_shim/engine.rb
@@ -1,5 +1,20 @@
 require 'delayed_job'
 require 'csv'
+require "rails"
+require 'grape'
+require 'grape-swagger'
+require 'grape-swagger-ui'
+require 'aws-sdk-dynamodb'
+require "pipeline_publisher_ruby"
+require "business"
+require 'httparty'
+require 'aws-sdk-core'
+require 'aws-sdk-s3'
+require 'inst-jobs'
+require 'pg'
+require 'byebug'
+require "rspec-rails"
+
 module CanvasShim
   class Engine < ::Rails::Engine
     config.autoload_paths << File.expand_path("app/services", __dir__)
