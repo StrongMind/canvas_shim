2fb9abf73d94be8725a0a4921953fa934b7db8fa
Tesla Team <tesla@strongmind.com>
2018-10-31 17:30:45 -0700

    seeing the results we expect now in tests

--- spec/test_app/spec/models/submission_spec.rb
+++ spec/test_app/spec/models/submission_spec.rb
@@ -7,21 +7,19 @@ describe Submission do
         ENV['PIPELINE_USER_NAME'] = 'name'
         ENV['PIPELINE_PASSWORD'] = 'password'
         allow(PipelineService::HTTPClient).to receive(:post)
+        assignment.update(course: course)
       end
 
+      let(:assignment) { Assignment.create }
+      let(:user) { User.create }
+      let(:content_tag) { ContentTag.create(content: assignment) }
+      let(:context_module) { ContextModule.create(content_tags: [content_tag]) }
+      let(:course) { Course.create(context_modules: [context_module]) }
+      let(:data_result) { {}.tap {|result| result[context_module.id] = 50} }
+
       it 'actually happens' do
-        assignment = Assignment.create
-        user = User.create
-        content_tag = ContentTag.create(content: assignment)
-        context_module = ContextModule.create(content_tags: [content_tag])
-        course = Course.create(context_modules: [context_module])
-
-        assignment = Assignment.create(course: course)
-
-        s = Submission.create(user: user, assignment: assignment)
-
-        expect(PipelineService::HTTPClient).to receive(:post).with(hash_including(data: {}))
-        s.update(score: 50)
+        expect(PipelineService::HTTPClient).to receive(:post).with(hash_including(data: data_result))
+        submission = Submission.create(user: user, assignment: assignment, score: 50)
       end
     end
   end
