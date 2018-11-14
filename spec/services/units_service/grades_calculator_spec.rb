4aa269531e69a2e9413f513374b37c03f7770c77
Tesla Team <tesla@strongmind.com>
2018-11-14 14:09:39 -0700

    calculate weighted averages

--- spec/services/units_service/grades_calculator_spec.rb
+++ spec/services/units_service/grades_calculator_spec.rb
@@ -3,10 +3,12 @@ describe UnitsService::GradesCalculator do
   let(:unit) { double('unit', id: 1) }
   let(:course) { double('course') }
   let(:submissions) { [checkpoint_submission, discussion_group_submission] }
-  let(:checkpoint_submission) { double(:submission, excused?: false, score: 70) }
-  let(:discussion_group_submission) { double(:submission, excused?: false, score: 50) }
-  let(:checkpoint_assignment) { double(:assignment, assignment_group: 'checkpoints') }
-  let(:discussion_assignment) { double(:assignment, assignment_group: 'discussion_groups') }
+  let(:checkpoint_submission) { double(:submission, excused?: false, score: 70, assignment: checkpoint_assignment) }
+  let(:discussion_group_submission) { double(:submission, excused?: false, score: 50, assignment: discussion_assignment) }
+  let(:checkpoint_assignment_group) { double(:checkpoint_assignment_group, name: 'checkpoints') }
+  let(:discussion_assignment_group) { double(:checkpoint_assignment_group, name: 'discussion_groups') }
+  let(:checkpoint_assignment) { double(:assignment, assignment_group: checkpoint_assignment_group) }
+  let(:discussion_assignment) { double(:assignment, assignment_group: discussion_assignment_group) }
 
   let(:unit_submissions) do
     result = {}
@@ -26,17 +28,16 @@ describe UnitsService::GradesCalculator do
   end
 
   describe '#call' do
-    # SCENARIO
-    # There are 2 assignments in a Unit:
-    # Checkpoint 1 – 100pts
-    # Discussion 1 – 100pts
-    # The Checkpoint category is worth 20% of the student’s overall grade
-    # The Discussion category is worth 10% of the student’s overall grade
-    # The student scored 70% on Checkpoint 1, and 50% on Discussion 1
-    # Their Unit Grade would be calculated using the following:
+    # 70% average checkpoint grade, weighted at 20% (divided by a sum category weight of 30%)
+    # 70 * (20/30)  = 46.667
+    # +
+    # 50% average discussion grade, weighted at 10% (divided by a sum category weight of 30%)
+    # 50 * (10/30) = 16.667
+    # Then, you add the categories:
+    # 46.667 + 16.667 = 63.33% Unit Grade for the student
 
     it 'works' do
-      expect(subject.call).to eq({1=>63.33})
+      expect(subject.call[1]).to be_within(0.1).of(63.33)
       subject.call
     end
   end
