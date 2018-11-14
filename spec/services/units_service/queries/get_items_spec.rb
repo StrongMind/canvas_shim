35be5397122ab77fc9adca7628cf5577f4e53de0
Tesla Team <tesla@strongmind.com>
2018-11-14 10:48:02 -0700

    filter out excused and unpublished

--- spec/services/units_service/queries/get_items_spec.rb
+++ spec/services/units_service/queries/get_items_spec.rb
@@ -2,8 +2,13 @@ describe UnitsService::Queries::GetItems do
   let(:course) { Course.create(context_modules: [context_module]) }
   let(:context_module) { ContextModule.create(content_tags: [content_tag]) }
   let(:content_tag) { ContentTag.create(content: assignment) }
-  let(:assignment) { Assignment.create }
+  let(:assignment) { Assignment.create(workflow_state: 'published') }
   let(:submission) { Submission.create(assignment: assgnment) }
+
+  # {<context_module>: []}
+  let(:empty_result) do
+    {}.tap { |hash| hash[context_module] = []}
+  end
 
   subject { described_class.new(course: course) }
 
@@ -20,9 +25,7 @@ describe UnitsService::Queries::GetItems do
   context 'tags without content' do
     let(:content_tag) { ContentTag.create(content: nil) }
     it 'does not return a content tag' do
-      result = {}
-      result[context_module] = []
-      expect(subject.query).to eq(result)
+      expect(subject.query).to eq(empty_result)
     end
   end
 
@@ -30,9 +33,14 @@ describe UnitsService::Queries::GetItems do
     let(:unsubmittable_content) { Course.create }
     let(:content_tag) { ContentTag.create(content: unsubmittable_content) }
     it 'does not return a content tag' do
-      result = {}
-      result[context_module] = []
-      expect(subject.query).to eq(result)
+      expect(subject.query).to eq(empty_result)
+    end
+  end
+
+  context 'where assignment is not published' do
+    let(:assignment) { Assignment.create }
+    it 'does not return the content tag' do
+      expect(subject.query).to eq(empty_result)
     end
   end
 end
