bb24eb6319dd0c38dacc8869533bae31b240f801
Tesla Team <tesla@strongmind.com>
2018-11-19 16:09:06 -0700

    fix get_submissions

--- spec/test_app/db/migrate/20181029220255_create_context_modules.rb
+++ spec/test_app/db/migrate/20181029220255_create_context_modules.rb
@@ -2,6 +2,7 @@ class CreateContextModules < ActiveRecord::Migration[5.0]
   def change
     create_table :context_modules do |t|
       t.integer :course_id
+      t.integer :position
     end
   end
 end
