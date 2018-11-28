d7116589a3d50550689837ca6d23bb80b7cf07a7
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-11-27 18:38:45 -0700

    test3

---
+++ app/lib/basic_lti/basic_outcomes.rb
@@ -0,0 +1,11 @@
+module BasicLTI
+  module BasicOutcomes
+    class LtiResponse
+      def initialize
+      end
+      def create_homework_submission
+        puts 'create_homework'
+      end
+    end
+  end
+end
