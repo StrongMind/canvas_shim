468a38a7d00f6900e671c1be68601b7f70a57c8b
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-12-17 14:34:59 -0700

    add set_unread_status, on save, to DiscussionEntry

---
+++ app/models/discussion_entry.rb
@@ -0,0 +1,19 @@
+class DiscussionEntry
+  after_save :set_unread_status
+
+  def set_unread_status
+    topic = self.discussion_topic
+    course = self.discussion_topic.course
+    topic_microservice_domain = ENV['TOPIC_MICROSERVICE_DOMAIN']
+    return unless topic_microservice_domain
+
+    course.teachers.each do |teacher|
+      endpoint = "http://#{topic_microservice_domain}/teachers/#{teacher.id}/topics/#{topic.id}"
+      if self.unread?(teacher)
+        HTTParty.post(endpoint)
+      else
+        HTTParty.delete(endpoint)
+      end
+    end
+  end
+end
