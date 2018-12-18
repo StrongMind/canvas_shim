cceff1aa520a366795bb1cefb099799a85d44b40
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-12-17 17:18:13 -0700

    some debug stuff

--- app/models/discussion_entry.rb
+++ app/models/discussion_entry.rb
@@ -2,9 +2,11 @@ class DiscussionEntry
   after_save :set_unread_status
 
   def set_unread_status
+    puts 'set_unread_status'
     topic = self.discussion_topic
     course = self.discussion_topic.course
     topic_microservice_domain = ENV['TOPIC_MICROSERVICE_DOMAIN']
+    puts 'set_unread_status 1'
     return unless topic_microservice_domain
 
     course.teachers.each do |teacher|
