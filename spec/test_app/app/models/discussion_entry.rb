2665ed3ab43d79d8fb0ecae0f4a2722dab91c06d
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-12-18 12:03:22 -0700

    unit tests for discussion enty callback

--- spec/test_app/app/models/discussion_entry.rb
+++ spec/test_app/app/models/discussion_entry.rb
@@ -1,8 +1,12 @@
 class DiscussionEntry < ActiveRecord::Base
   belongs_to :discussion_topic
 
+  def unread= value
+    @unread = value
+  end
+
   def unread?(teacher)
-    true
+    @unread
   end
 end
 require File.expand_path('../../app/models/discussion_entry', CanvasShim::Engine.called_from)
