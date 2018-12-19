02fbb7954b351c0fb92d116e2f2d10189420a33b
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-12-18 18:12:26 -0700

    make unread a field for testing

--- spec/test_app/db/migrate/20181218165252_create_discussion_entries.rb
+++ spec/test_app/db/migrate/20181218165252_create_discussion_entries.rb
@@ -2,6 +2,7 @@ class CreateDiscussionEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :discussion_entries do |t|
       t.integer :discussion_topic_id
+      t.boolean :unread
     end
   end
 end
