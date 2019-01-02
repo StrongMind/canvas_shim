daf2c7b8b303717b80a4e9ba20dc1592193ed630
John VanBuskirk <tesla+john@strongmind.com>
2019-01-02 16:45:54 -0700

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
     topic_microservice_endpoint = ENV['TOPIC_MICROSERVICE_ENDPOINT']
     api_key = ENV['TOPIC_MICROSERVICE_API_KEY']
     return {} unless topic_microservice_endpoint && api_key
-    return {} if self.enrollments.where(type: 'TecherEnrollment').empty?
+    return {} if self.enrollments.where(type: 'TeacherEnrollment').empty?
     endpoint = "#{topic_microservice_endpoint}/teachers/#{ENV['CANVAS_DOMAIN']}:#{self.id}/topics/"
     ret = HTTParty.get(endpoint, headers: { "x-api-key": api_key })
     ret.body
--- lib/canvas_shim/version.rb
+++ lib/canvas_shim/version.rb
@@ -1,3 +1,3 @@
 module CanvasShim
-  VERSION = '0.1.21'
+  VERSION = '0.1.22'
 end
