322fc146024d288c1cd2d0554680391bb54450fe
John VanBuskirk and Mark Ng <tesla+john+mark@strongmind.com>
2019-01-10 17:01:07 -0700

    wip add current score to unit grade service

--- spec/services/units_service/commands/get_unit_grades_spec.rb
+++ spec/services/units_service/commands/get_unit_grades_spec.rb
@@ -2,6 +2,7 @@ describe UnitsService::Commands::GetUnitGrades do
   let(:course) { double('course', assignment_groups: [], id: 1) }
   let(:pseudonym) { double('pseudonym', sis_user_id: 1001) }
   let(:user) { double('user', id: 1, pseudonym: pseudonym) }
+  let(:enrollment) {double('enrollment', computed_current_score: 90)}
   let(:query_instance) { double('query instance', query: nil) }
   let(:current_time) { Time.now }
   let(:unit) { double('unit', id: 1, created_at: current_time, position: 3 ) }
@@ -15,11 +16,13 @@ describe UnitsService::Commands::GetUnitGrades do
     ENV['CANVAS_DOMAIN'] = 'canvasdomain.com'
     allow(UnitsService::Queries::GetSubmissions).to receive(:new).and_return(query_instance)
     allow(UnitsService::GradesCalculator).to receive(:new).and_return(calculator_instance)
+    allow(Enrollment).to receive(:where).and_return([enrollment])
   end
 
   it 'returns the calculator results' do
     expect(subject.call).to eq(
       course_id: 1,
+      course_score: 90,
       school_domain: "canvasdomain.com",
       student_id: 1,
       sis_user_id: 1001,
