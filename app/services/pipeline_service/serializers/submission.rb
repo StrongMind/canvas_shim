03997d1d58a0bcde6ff4d9c8d4cda5f92c07f167
Chris Young, Elle Bayless, and Mark Ng <tesla+chris+elle+mark@strongmind.com>
2019-01-22 17:05:56 -0700

    new submission

--- app/services/pipeline_service/serializers/submission.rb
+++ app/services/pipeline_service/serializers/submission.rb
@@ -2,28 +2,24 @@ module PipelineService
   module Serializers
     # This ugly thing lets us call the canvas user api
     class Submission
-      include ::Api
-      include ::Api::V1::Submission
-      include ActionView::Helpers
-      include ActionDispatch::Routing::UrlFor
-      include Rails.application.routes.url_helpers
-      include BaseMethods
-
-      def params;{};end
-
-      def initialize(object:)
-        default_url_options[:host] = host
+      def initialize object:
         @object = object
-        @admin = PipelineService::Account.account_admin
+        @course = object.assignment.course
+        @assignment = object.assignment
+        @user = object.user
       end
 
-      def request
-        super
-      end
 
       def call
-        @current_user = @admin
-        submission_json(@object, @object.assignment, @admin, {}, nil, [])
+        HTTPClient.get(endpoint, headers: { Authorization: "Bearer #{ENV['STRONGMIND_INTEGRATION_KEY']}"})
+      end
+
+      private
+
+      attr_reader :object, :course, :assignment, :user
+
+      def endpoint
+        "https://#{ENV['CANVAS_DOMAIN']}/api/v1/courses/#{course.id}/assignments/#{assignment.id}/submissions/#{user.id}"
       end
     end
   end
