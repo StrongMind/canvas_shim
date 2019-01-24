46322231d2893a9eadfdfb8f839bc70b14453663
Andy Rosenberg <arosenberg3545@fs1432mbpro.avaflipswitch.com>
2019-01-24 10:34:27 -0700

    created more tests

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123234229) do
+ActiveRecord::Schema.define(version: 20190124171243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20190123234229) do
     t.integer  "course_id"
     t.integer  "user_id"
     t.datetime "start_at"
+    t.string   "type"
   end
 
   create_table "failed_jobs", force: :cascade do |t|
--- spec/test_app/spec/models/course_progress_spec.rb
+++ spec/test_app/spec/models/course_progress_spec.rb
@@ -1,44 +1,48 @@
 describe CourseProgress do
   let(:user) { User.create }
-  let(:observer) { User.create }
+  let(:user_2) { User.create }
+  let(:observer) { User.create(observed_users: [user]) }
+  let(:observer_2) { User.create(observed_users: [user, observer]) }
   let(:course) { Course.create }
+  let(:cpo) { CourseProgress.new(course, observer) }
+  let(:cpo_2) { CourseProgress.new(course, observer_2) }
+  let(:cpu) { CourseProgress.new(course, user) }
+  let(:cpu_2) { CourseProgress.new(course, user_2) }
+
 
   it 'playground' do
     # We're just playing with the public api
-    CourseProgress.new(course, user).module_progressions
-    CourseProgress.new(course, user).to_json
-    CourseProgress.new(course, user).requirements
+    cpu.module_progressions
+    cpu.to_json
+    cpu.requirements
   end
 
 
   describe "#find_observed_user" do
     it 'returns the first observed user' do
-
+      expect(cpo.send(:find_user_id)).to eq(user.id)
+      expect(cpo_2.send(:find_user_id)).to eq(user.id)
     end
 
-    it 'returns nil' do
-      #
-    end
-  end
-
-  describe "#set_user_id" do
-    # expect instance.send(:set_user_id).to eq(user.id)
-    it "returns observed user id" do
-
-    end
-
-    it "returns user id" do
-
+    it 'returns the user if no observers' do
+      expect(cpu.send(:find_user_id)).to eq(user.id)
     end
   end
 
   describe "#allow_course_progress?" do
-    it "returns true" do
-
+    it "returns true if the user is enrolled as a student" do
+      allow(course).to receive(:user_is_student?).and_return(true)
+      expect(cpu.send(:allow_course_progress?)).to be true
     end
 
-    it "returns false" do
+    it "returns true if the user is observing a student" do
+      allow(course).to receive(:user_is_student?).and_return(true)
+      expect(cpo.send(:allow_course_progress?)).to be true
+    end
 
+    it "returns falsy if the user is not enrolled" do
+      allow(course).to receive(:user_is_student?).and_return(false)
+      expect(cpu_2.send(:allow_course_progress?)).to be nil
     end
   end
 end
