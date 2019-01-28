bf9a39a845580784eab4dc0dfcfb1c7604da2e0c
Andy Rosenberg <arosenberg3545@fs1432mbpro.avaflipswitch.com>
2019-01-28 15:52:21 -0700

    Abstracted find_user operation

--- app/models/canvas_shim/course_progress.rb
+++ app/models/canvas_shim/course_progress.rb
@@ -2,7 +2,7 @@ module CanvasShim
   module CourseProgress
     def module_progressions
       @_module_progressions ||= course.context_module_progressions.
-                                    where(user_id: User.find(find_user_id), context_module_id: modules).to_a
+                                    where(user_id: course_progress_user, context_module_id: modules).to_a
     end
 
     def self.included(mod)
@@ -27,7 +27,7 @@ module CanvasShim
               :page_visibilities => true, # fm.page_visibilities_for_users(user_ids),
               :quiz_visibilities => true # fm.quiz_visibilities_for_users(user_ids)
             }
-            modules.flat_map { |m| m.completion_requirements_visible_to(User.find(find_user_id), opts) }.uniq
+            modules.flat_map { |m| m.completion_requirements_visible_to(course_progress_user, opts) }.uniq
           end
         end
     end
@@ -58,17 +58,21 @@ module CanvasShim
       observer_enrollment ? observer_enrollment.associated_user_id : @user.id
     end
 
+    def course_progress_user
+      @course_progress_user ||= User.find(find_user_id)
+    end
+
     def observer_enrollment
       @user.enrollments.where(type: 'ObserverEnrollment', course: course).where.not(associated_user_id: nil).first
     end
 
     def allow_course_progress?
       (course.module_based? && course.user_is_student?(user, include_all: true)) ||
-      (course.module_based? && observer_enrollment && course.user_is_student?(User.find(find_user_id), include_all: true))
+      (course.module_based? && observer_enrollment && course.user_is_student?(course_progress_user, include_all: true))
     end
 
     def excused_submission_count
-      course.submissions.where(user: user, excused: true).count
+      course.submissions.where(user: course_progress_user, excused: true).count
     end
   end
 end
