d2ce4c222fa3d0f44ab9ff06ad5a337cea53dc45
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2019-01-28 12:58:47 -0700

    use .env gem and stop delaying jobs

--- spec/test_app/config/environment.rb
+++ spec/test_app/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+Dotenv::Railtie.load
 # Initialize the Rails application.
+
+Delayed::Worker.delay_jobs = false
 Rails.application.initialize!
