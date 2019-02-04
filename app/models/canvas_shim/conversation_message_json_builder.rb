c73472c9df69664e6ad83bc22c334b8e56af6519
Chris Young <beesucker@gmail.com>
2019-02-04 12:10:10 -0700

    853 conversation data fix (#41)
    Watch the correct noun and add a backfill
    
    * publish conversation_message, not message
    
    * add missing conversation_id and timestamps to conversation_participants/messages
    
    * add .env files to git ignore
    
    * Clean up style
    
    * no need to increase Gem version.
    
    * we need to set VERSION to SOMETHING or the tests will not run
    
    * add example for .env

---
+++ app/models/canvas_shim/conversation_message_json_builder.rb
@@ -0,0 +1,10 @@
+module CanvasShim
+  class ConversationMessageJSONBuilder < ActiveRecord::Base
+    self.table_name = "conversation_messages"
+
+    def self.call(id:)
+      # Dont include root.  See active record initializer
+      ConversationMessageJSONBuilder.find(id).to_json(include_root: false)
+    end
+  end
+end
