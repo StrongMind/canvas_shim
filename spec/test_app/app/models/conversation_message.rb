163ae1c36dad381b8a83595aa95a6f65bf1c0e0c
Chris Young <beesucker@gmail.com>
2019-02-04 11:43:47 -0700

    Clean up style

--- spec/test_app/app/models/conversation_message.rb
+++ spec/test_app/app/models/conversation_message.rb
@@ -1,23 +1,7 @@
 class ConversationMessage < ApplicationRecord
   belongs_to :author, :class_name => 'User'
+  belongs_to :conversation
+  belongs_to :author, :class_name => 'User'
+
   after_save { PipelineService.publish(self) }
-
-    belongs_to :conversation
-    belongs_to :author, :class_name => 'User'
-    # delegate :participants, :to => :conversation
-
 end
-
-# class ConversationMessage < ActiveRecord::Base
-#   after_save { PipelineService.publish(self) }
-#
-#   belongs_to :conversation
-#   belongs_to :author, :class_name => 'User'
-#   belongs_to :context, polymorphic: [:account]
-#   has_many :conversation_message_participants
-#   has_many :attachment_associations, :as => :context, :inverse_of => :context
-#   # we used to attach submission comments to conversations via this asset
-#   # TODO: remove this column when we're sure we don't want this relation anymore
-#   belongs_to :asset, polymorphic: [:submission]
-#   delegate :participants, :to => :conversation
-# end
