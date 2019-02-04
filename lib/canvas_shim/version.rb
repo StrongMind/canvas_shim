73ad1fe00036d70b41ef1cf0bbfb70a089a02c5a
Chris Young <beesucker@gmail.com>
2019-02-04 11:58:28 -0700

    we need to set VERSION to SOMETHING or the tests will not run

--- lib/canvas_shim/version.rb
+++ lib/canvas_shim/version.rb
@@ -1,6 +1,5 @@
-# THIS ONLY NEEDS TO CHANGE IF WE ARE PUBLISHING THE GEM,
-# Currently we are using git tags.
-
 module CanvasShim
-  # VERSION = '0.1.52'.freeze
+  # THIS ONLY NEEDS TO CHANGE IF WE ARE PUBLISHING THE GEM,
+  # Currently we are using git tags.
+  VERSION = '0.0.0'.freeze
 end
