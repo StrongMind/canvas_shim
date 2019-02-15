cee54b6289207deafcd42308e840b18751348841
Chris Young <beesucker@gmail.com>
2019-02-15 10:21:32 -0700

    make sure we are testing against the json builder

--- spec/services/pipeline_service/queries/find_by_id_spec.rb
+++ spec/services/pipeline_service/queries/find_by_id_spec.rb
@@ -2,13 +2,12 @@ describe PipelineService::Queries::FindByID do
     include_context 'pipeline_context'
     
     subject { described_class }
-    let(:conversation) { Conversation.create }
-    let(:noun) { PipelineService::Models::Noun.new(conversation) }
-    let(:builder)  {PipelineService::Builders::ConversationJSONBuilder}
+    let(:conversation)  { Conversation.create }
+    let(:noun)          { PipelineService::Models::Noun.new(conversation) }
+    let(:builder)       { PipelineService::Builders::ConversationJSONBuilder }
     
     it 'returns a ruby hash of the object' do
-        expect(subject
-            .query(builder, noun))
-            .to eq(conversation.as_json(include_root: false))
+        expect(subject.query(builder, noun))
+            .to eq(builder.call(noun))
     end
 end
