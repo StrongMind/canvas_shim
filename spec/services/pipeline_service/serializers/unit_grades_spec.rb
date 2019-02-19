c2abca7cfd2e348116846166e3e8b038cefd2d54
Chris Young <beesucker@gmail.com>
2019-02-19 16:12:05 -0700

    fix events testing

--- spec/services/pipeline_service/serializers/unit_grades_spec.rb
+++ spec/services/pipeline_service/serializers/unit_grades_spec.rb
@@ -1,29 +1,7 @@
 describe PipelineService::Serializers::UnitGrades do
-  context do
-    let(:assignment) { Assignment.create(course: course) }
-    let(:course) { Course.create }
-    let(:student) { User.create(pseudonym: pseudonym) }
-    let(:submission) { Submission.create(assignment: assignment, course: course, user: student) }
-    let(:unit_grades) { PipelineService::Nouns::UnitGrades.new(submission) }
-    let(:pseudonym) { Pseudonym.create(sis_user_id: '1001') }
-    let!(:enrollment) { Enrollment.create!(course: course, user: student) }
-    let(:client) {double('client', get_single_submission_courses: [])}
-
-    before do
-      class_double("Pandarus::Client", new: client).as_stubbed_const
-    end
-    
-    describe do
-      it do
-        PipelineService.publish(unit_grades)
-      end
-    end
-  end
-
+  include_context 'pipeline_context'
   describe 'Unit grade serializer' do
     context 'when given a submission' do
-      include_context 'pipeline_context'
-
       subject { described_class.new(object: unit_grades)}
       let(:assignment) { Assignment.create(course: course) }
       let(:course) { Course.create }
