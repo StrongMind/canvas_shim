3b5f9ecfe7afd2ed9d04392a0ebca66b22b56a1a
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2019-02-27 10:35:42 -0700

    get submissions from either assignment or discussion assignment

--- spec/services/units_service/queries/get_submissions_spec.rb
+++ spec/services/units_service/queries/get_submissions_spec.rb
@@ -1,4 +1,5 @@
 describe UnitsService::Queries::GetSubmissions do
+  include_context "pipeline_context"
   let(:student) { User.create }
   let(:course)  { Course.create(context_modules: [unit]) }
   let(:submission) { Submission.create!(user: student, assignment: assignment) }
@@ -15,9 +16,34 @@ describe UnitsService::Queries::GetSubmissions do
   subject { described_class.new(student: student, course: course) }
 
   before do
-    allow(SettingsService).to receive(:get_settings).and_return('enable_unit_grade_calculations' => true)
     allow(subject).to receive(:units).and_return(units_result)
-    allow(PipelineService).to receive(:publish)
+  end
+
+  context 'when item is a discussion topic' do
+    let!(:discussion_course) { Course.create(context_modules: [discussion_context_module]) }
+    let!(:discussion_topic) { DiscussionTopic.create(workflow_state: 'active') }
+    let!(:discussion_context_module) { ContextModule.create(content_tags: [discussion_content_tag]) }
+    let!(:discussion_assignment) { Assignment.create(discussion_topic: discussion_topic, workflow_state: 'published') }
+    let!(:discussion_submission) { Submission.create!(user: student, assignment: discussion_assignment) }
+    let!(:discussion_content_tag) { ContentTag.create(content: discussion_topic) }
+
+    let(:discussion_units_result) do
+      units = {}
+      units[discussion_context_module] = [discussion_content_tag]
+      units
+    end
+
+    before do
+      allow(subject).to receive(:units).and_return(discussion_units_result)
+    end
+
+    subject { described_class.new(student: student, course: discussion_course) }
+
+    it 'returns the submission from the related assignment' do
+      result = {}
+      result[discussion_context_module] = [discussion_submission]
+      expect(subject.query).to eq result
+    end
   end
 
   it 'returns the unit and its submissions' do
