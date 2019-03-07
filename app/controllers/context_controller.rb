b01e961e5dfb49f54b372ecd1374377e017dae6e
Chris Irish <irish@velocitylabs.io>
2019-03-07 10:01:55 -0700

    stop gap fix for letting shim tests run

---
+++ app/controllers/context_controller.rb
@@ -0,0 +1,8 @@
+
+# TODO: DELETE ME WHEN SHIM SPECS MOVE INTO CANVAS LMS, spec_strongmind
+#
+# This is only here so shim tests don't blow up trying to decorate a controller
+# that doesn't exist in Shim
+class ContextController < ApplicationController
+
+end
