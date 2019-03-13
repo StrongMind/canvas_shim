f59a36af0035f2ed8293a9442f22582a446705b6
Zac Brown <me@yupitszac.com>
2019-03-13 11:13:57 -0700

    Added clever.rb integration to shim to extend the available fedarated attributes, added copy of clever.rb to test app and added spec

---
+++ app/models/account_authorization_config/clever.rb
@@ -0,0 +1,15 @@
+class AccountAuthorizationConfig::Clever
+    def self.login_attributes
+        ['id'.freeze, 'sis_id'.freeze, 'email'.freeze, 'student_number'.freeze, 'teacher_number'.freeze].freeze
+    end
+
+    def self.recognized_federated_attributes
+        login_attributes
+    end
+
+    def self.flatten_attributes(attributes, prefix=nil)
+        attributes.each_pair.reduce({}) do |a, (k, v)|
+            v.is_a?(Hash) ? a.merge(flatten_attributes(v, "#{prefix}#{k}.")) : a.merge("#{prefix}#{k}".to_sym => v)
+        end
+    end
+end
