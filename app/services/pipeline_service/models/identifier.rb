25cdf436111b3bb193e0aa240d19fc3621bcdffc
Chris Young <beesucker@gmail.com>
2019-03-13 16:55:06 -0700

    add some documentation

--- app/services/pipeline_service/models/identifier.rb
+++ app/services/pipeline_service/models/identifier.rb
@@ -1,6 +1,15 @@
 module PipelineService
     module Models
         class Identifier
+            attr_accessor :field_alias, :field, :instance
+            
+            # field can be a symbol or a proc.  The proc should return
+            # an array with the field name and call a value from the 
+            # instance passed into #to_a
+            #
+            # Proc.new do |submission| 
+            #   [:course_id, submission.assignment.course.id]}
+            # end
             def initialize(field, options={})
                 @field = field
                 @field_alias = options[:alias]
@@ -15,10 +24,6 @@ module PipelineService
             def to_h
                 return [[field, field_alias]].to_h
             end
-
-            private
-
-            attr_accessor :field_alias, :field, :instance
         end
     end
 end
