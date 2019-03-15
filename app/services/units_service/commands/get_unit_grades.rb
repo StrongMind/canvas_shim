5276444a2d4536979c8e4e04743dc2905d77740c
Chris Young <beesucker@gmail.com>
2019-03-15 10:17:01 -0700

    properly guard

--- app/services/units_service/commands/get_unit_grades.rb
+++ app/services/units_service/commands/get_unit_grades.rb
@@ -16,7 +16,11 @@ module UnitsService
       private
 
       def get_sis_user_id
-        @student.respond_to?(:pseudonym) ? @student.pseudonym.sis_user_id : ''
+        return unless
+          @student.respond_to?(:pseudonym) && 
+          @student.pseudonym &&
+          @student.pseudonym.sis_user_id
+        @student.pseudonym.sis_user_id
       end
 
       def unit_submissions
