28e9f85c8ac63eced719565a3560f98ef8cfb8ce
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-03-20 16:09:44 -0700

    Mocked other environment vars

--- spec/test_app/spec/models/student_enrollment_spec.rb
+++ spec/test_app/spec/models/student_enrollment_spec.rb
@@ -1,4 +1,16 @@
 describe StudentEnrollment do
+  before do
+    ENV['PIPELINE_ENDPOINT'] = 'blah'
+    ENV['PIPELINE_USER_NAME'] = 'blah'
+    ENV['PIPELINE_PASSWORD'] = 'blah'
+    ENV['SIS_ENROLLMENT_UPDATE_API_KEY'] = 'blah'
+    ENV['SIS_ENROLLMENT_UPDATE_ENDPOINT'] = 'blah'
+    ENV['SIS_UNIT_GRADE_ENDPOINT_API_KEY'] = 'blah'
+    ENV['SIS_UNIT_GRADE_ENDPOINT'] = 'blah'
+    allow(SettingsService).to receive(:get_settings).and_return({})
+    allow(PipelineService::HTTPClient).to receive(:post)
+  end
+
   let(:assignment) { Assignment.create }
   let(:course) { Course.create(assignments: [assignment]) }
   let(:command) { double("Command", perform: nil)}
