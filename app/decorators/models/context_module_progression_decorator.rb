25789d7cb4f46bc2301a7734f62880475fe6fa8c
Tesla Team <tesla@strongmind.com>
2019-04-02 14:42:00 -0700

    Guard for nil

--- app/decorators/models/context_module_progression_decorator.rb
+++ app/decorators/models/context_module_progression_decorator.rb
@@ -1,7 +1,7 @@
 ContextModuleProgression.class_eval do
   def strongmind_prerequisites_satisfied?
     enrollment = Enrollment.where(user_id: user.id, course_id: context_module.context.id).first
-    settings = SettingsService.get_enrollment_settings(id: enrollment.id)
+    settings = enrollment ? SettingsService.get_enrollment_settings(id: enrollment.id) : {}
     sequence_control = settings.fetch('sequence_control', true)
     sequence_control ? instructure_prerequisites_satisfied? : true
   end
