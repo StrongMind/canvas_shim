ab95691218ccd4cdda4c73f9d9babf308fd7133b
Chris Irish <irish@velocitylabs.io>
2019-04-02 17:15:09 -0700

    spec fixing

--- spec/services/settings_service/base_spec.rb
+++ spec/services/settings_service/base_spec.rb
@@ -3,21 +3,13 @@ describe SettingsService::Base do
     described_class.new
   }
 
-  before do
-    # to pass TravisCI
-    stub_request(:get, /latest\/meta-data\/iam\/security-credentials/).
-      with(
-       headers: {
-      'Accept'=>'*/*',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'User-Agent'=>'aws-sdk-ruby3/3.48.2'
-    }).to_return(status: 200, body: "", headers: {})
-
-  end
-
   context '#dynamodb' do
     it 'is an instance method not a class method' do
-      expect(subject.dynamodb).to be_a(Aws::DynamoDB::Client)
+      allow(Aws::DynamoDB::Client).to receive(:new).and_return(instance_double(Aws::DynamoDB::Client))
+
+      expect {
+        subject.dynamodb
+      }.to_not raise_error
 
       expect {
         SettingsService::Base.dynamodb
