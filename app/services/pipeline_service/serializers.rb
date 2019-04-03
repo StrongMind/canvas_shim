fad187af661fa484392d10069bbbbe7217c27198
Chris Young <beesucker@gmail.com>
2019-04-03 09:55:34 -0700

    Refactor serializer lookups (#79)
    * use created at if updated at does not exist
    
    * fix tests
    
    * dynamically look up serializers
    
    * docs

---
+++ app/services/pipeline_service/serializers.rb
@@ -0,0 +1,21 @@
+# Preload all of the serializers.  Rails does just in time loading.  This makes sure all 
+# the modules are loaded so when we call for the constants, ruby returns something
+Dir[File.dirname(__FILE__) + '/serializers/*.rb'].each {|file| require_dependency file }
+module PipelineService
+  module Serializers
+    # A list of all of the serializers
+    def self.list
+      names.map{|name| self.const_get(name)}
+    end
+
+    # A list of serializer names
+    def self.names
+      constants - [:BaseMethods]
+    end
+
+    # A list of the active record models that are used by the serializers
+    def self.repositories
+      (names - [:CanvasAPIEnrollment, :UnitGrades]).map { |name| name.to_s.constantize }
+    end
+  end
+end
