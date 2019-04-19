c0dbbcf62533cd78dc32da13ab416da41a996fc9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-04-19 14:42:58 -0700

    added enrollment decorator

---
+++ app/decorators/models/enrollment_decorator.rb
@@ -0,0 +1,16 @@
+Enrollment.class_eval do
+  after_commit :ensure_active_scores
+
+  def all_scores
+    Score.where(enrollment: self)
+  end
+  
+  def ensure_active_scores
+    return if !active_student? || active_scores?
+    all_scores.each { |score| score.update(workflow_state: "active") }
+  end
+  
+  def active_scores?
+    self.scores.any?
+  end
+end
