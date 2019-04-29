04ac90d9b6d6127c581b156f136f14b39147ac46
Chris Irish <irish@velocitylabs.io>
2019-04-29 16:31:22 -0700

    grab branch name from env. try moving check build script to after_script hook

--- scripts/start_lms_build.rb
+++ scripts/start_lms_build.rb
@@ -1,11 +1,13 @@
 require "./scripts/travis.rb"
 require 'yaml'
 
+# If PR grab branch from PR branch env
+# If push build or non-PR, use branch env
+branch = [ENV['TRAVIS_PULL_REQUEST_BRANCH'], ENV['TRAVIS_BRANCH']].find { |item| item.present?
+
 travis = Travis.new auth_token: '2Zs3QvPdUqpWXL1kB-aM5A', repo_slug: "StrongMind%2Fcanvas-lms"
 
-travis.create_request branch: '19-04-15-shim-specs-in-lms'
-
-sleep 2
+travis.create_request branch: branch
 
 puts "Build id assigned: #{travis.build_id}"
 
