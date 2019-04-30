d7c0627f7ef728321d343e0cc456dd9a7de5e32a
Tesla Team <tesla@strongmind.com>
2019-04-30 15:11:12 -0700

    wip

---
+++ app/services/pipeline_service/serializers/page_view.rb
@@ -0,0 +1,14 @@
+module PipelineService
+  module Serializers
+    class PageView
+      # def initialize object:
+      #   @page_view = object
+      # end
+      #
+      # def call
+      #   @payload = Builders::ConversationMessageJSONBuilder.call(page_view) || {}
+      # end
+
+    end
+  end
+end
