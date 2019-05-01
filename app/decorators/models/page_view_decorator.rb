34803790e1f366d389a668406985ac10fbc0aa12
Tesla Team <tesla@strongmind.com>
2019-05-01 10:49:21 -0700

    add feature flag

--- app/decorators/models/page_view_decorator.rb
+++ app/decorators/models/page_view_decorator.rb
@@ -1,3 +1,9 @@
 PageView.class_eval do
-  after_commit -> { PipelineService.publish self }
+  after_commit -> :publish_to_pipeline
+
+  def publish_to_pipeline
+    return unless SettingsService.get_settings(object: :school, id: 1)['publish_page_views']
+    PipelineService.publish self
+  end
+
 end
