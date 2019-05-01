63167ccd7b33f3bf37f40abde74a2fe2d6480abc
Tesla Team <tesla@strongmind.com>
2019-05-01 10:50:27 -0700

    remove arrow

--- app/decorators/models/page_view_decorator.rb
+++ app/decorators/models/page_view_decorator.rb
@@ -1,5 +1,5 @@
 PageView.class_eval do
-  after_commit -> :publish_to_pipeline
+  after_commit :publish_to_pipeline
 
   def publish_to_pipeline
     return unless SettingsService.get_settings(object: :school, id: 1)['publish_page_views']
