71d4be744f321d03a18ec60a1159fbf6184d0768
Zac Brown <me@yupitszac.com>
2019-05-03 12:12:02 -0700

    [PLAY-972] remove feature toggle from route

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,6 @@
 class TodosController < ApplicationController
 
     def index
-        enable_fp_todo = SettingsService.get_settings(object: 'school', id: 1)['enable_full_page_todo']
-        raise ActionController::RoutingError.new('Not Found') unless enable_fp_todo
         render 'todos_list'
     end
 end
