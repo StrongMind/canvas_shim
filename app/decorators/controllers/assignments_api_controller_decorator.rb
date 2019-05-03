a9bbcd61d9cf984b0393856f8739039ec014cb0e
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-05-03 09:51:13 -0700

    Fixed unexcused if one student and wrapped in ar transaction

--- app/decorators/controllers/assignments_api_controller_decorator.rb
+++ app/decorators/controllers/assignments_api_controller_decorator.rb
@@ -2,11 +2,16 @@ AssignmentsApiController.class_eval do
   def strongmind_update
     @assignment = @context.active_assignments.api_id(params[:id])
     if @assignment && params['assignment'] && params['assignment']['excluded_students']
-      params['assignment']['excluded_students'].each do |student|
-        @assignment.toggle_exclusion(student['id'].to_i, true)
+      begin
+        Assignment.transaction do
+          params['assignment']['excluded_students'].each do |student|
+            @assignment.toggle_exclusion(student['id'].to_i, true)
+          end
+          unexcuse_assignments(params['assignment']['excluded_students'])
+        end
+      rescue StandardError => exception
+        Raven.capture_exception(exception)
       end
-
-      unexcuse_assignments(params['assignment']['excluded_students'])
     end
 
     instructure_update
@@ -17,7 +22,9 @@ AssignmentsApiController.class_eval do
 
   def unexcuse_assignments(arr)
     student_ids = arr.map { |student| student['id'] }
-    @assignment.excused_submissions.where("user_id NOT IN (?)", student_ids).each do |sub|
+    excused = @assignment.excused_submissions
+    excused = excused.where("user_id NOT IN (?)", student_ids) if student_ids.any?
+    excused.each do |sub|
       @assignment.toggle_exclusion(sub.user_id, false)
     end
   end
