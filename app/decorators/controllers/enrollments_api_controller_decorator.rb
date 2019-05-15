c4f879e523432cebf343b7057d22e9a126b2b23c
Chris Irish <irish@velocitylabs.io>
2019-05-15 10:05:15 -0700

    mv custom placement action from canvas-lms

---
+++ app/decorators/controllers/enrollments_api_controller_decorator.rb
@@ -0,0 +1,20 @@
+class EnrollmentsApiController < ApplicationController
+  before_action :get_course_from_section, except: :custom_placement
+  before_action :require_context
+
+  def custom_placement
+    @enrollment  = @context.enrollments.find(params[:id])
+    @content_tag = ContentTag.find(params[:content_tag].dig(:id))
+
+    if @enrollment && @content_tag
+      @enrollment.user.custom_placement_at(@content_tag)
+      render :json => {}, :status => :ok
+    else
+      render :json => {}, :status => :unprocessable_entity
+    end
+
+  rescue StandardError
+    render :json => {}, :status => :bad_request
+  end
+
+end
