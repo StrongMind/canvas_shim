58d05be9d0b4d9010e6ccfe8c5be67859ebf8633
John Vanbuskirk <jvanbuskirk@strongmind.com>
2019-05-22 15:34:51 -0700

    try without inheritance

--- app/services/pipeline_service/V2/nouns/content_tag.rb
+++ app/services/pipeline_service/V2/nouns/content_tag.rb
@@ -1,18 +1,27 @@
 module PipelineService
   module V2
     module Nouns
-      class ContentTag < PipelineService::V2::Nouns::Base
+      class ContentTag
         def initialize object:
           @ar_object = object.ar_model
         end
 
         def call
-          super.merge(resource_link_id)
+          # Return the default, non-overrriden from #as_json
+          @ar_object
+            .class
+            .superclass
+            .instance_method(:as_json)
+            .bind(@ar_object)
+            .call(include_root: false)
+            .merge resource_link_id
+
         end
 
         def resource_link_id
           { 'resource_link_id' => Lti::Asset.opaque_identifier_for(@ar_object) }
         end
+
       end
     end
   end
