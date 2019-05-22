95104b955a0d3c1adabd66e13e8676d395fd335f
John Vanbuskirk <jvanbuskirk@strongmind.com>
2019-05-22 15:26:35 -0700

    use full class name

--- app/services/pipeline_service/V2/nouns/content_tag.rb
+++ app/services/pipeline_service/V2/nouns/content_tag.rb
@@ -1,7 +1,7 @@
 module PipelineService
   module V2
     module Nouns
-      class ContentTag < Base
+      class ContentTag < PipelineService::V2::Nouns::Base
         def initialize object:
           @ar_object = object.ar_model
         end
