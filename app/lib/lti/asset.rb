162ea6c31228937db8daaf75b07e17ddb3cc38ca
johnvbk <31219912+johnvbk@users.noreply.github.com>
2019-05-23 17:52:47 -0700

    add a content_tag noun (#110)
    * add a content_tag noun
    
    * use full class name
    
    * try without inheritance

---
+++ app/lib/lti/asset.rb
@@ -0,0 +1,7 @@
+module Lti
+  class Asset
+    def self.opaque_identifier_for(asset)
+        return '123456789123456789'
+      end
+    end
+end
---
+++ spec/models/content_tag_spec.rb
@@ -0,0 +1,10 @@
+describe ContentTag do
+  include_context "stubbed_network"
+
+  describe "#after_commit" do
+    it 'does publish to the pipeline' do
+      expect(PipelineService::V2).to receive(:publish)
+      ContentTag.create
+    end
+  end
+end
