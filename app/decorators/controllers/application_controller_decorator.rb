8453faf892e723f3b4bc2df4550f97a1ba5e8248
Tesla Team <tesla@strongmind.com>
2019-05-24 14:26:19 -0700

    add threshold to env

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -1,6 +1,14 @@
 ApplicationController.class_eval do
   prepend_view_path CanvasShim::Engine.root.join('app', 'views')
 
+  def score_threshold
+    SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
+  end
+  
+  def threshold_set?
+    score_threshold.positive?
+  end
+  
   def strongmind_update_enrollment_last_activity_at
     return if logged_in_user != @current_user
     instructure_update_enrollment_last_activity_at
--- app/decorators/controllers/context_modules_controller_decorator.rb
+++ app/decorators/controllers/context_modules_controller_decorator.rb
@@ -13,14 +13,6 @@ ContextModulesController.class_eval do
     %w{Assignment DiscussionTopic Quizzes::Quiz}.include?(@tag.content_type)
   end
 
-  def score_threshold
-    SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
-  end
-  
-  def threshold_set?
-    score_threshold.positive?
-  end
-
   def add_threshold_to_module
     @module.completion_requirements << {:id => @tag.id, :type => "min_score", :min_score => score_threshold}
     @module.update_column(:completion_requirements, @module.completion_requirements)
