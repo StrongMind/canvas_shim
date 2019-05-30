2a438d93c1aae6eba2fbbe564ad9864a9ec32f3e
Chris Young <beesucker@gmail.com>
2019-05-30 16:41:24 -0700

    clean up cruft

--- app/decorators/models/content_tag_decorator.rb
+++ app/decorators/models/content_tag_decorator.rb
@@ -1,7 +1,6 @@
 ContentTag.class_eval do
-  # after_commit -> { PipelineService::V2.publish self }
+  after_commit -> { PipelineService::V2.publish self }
   after_commit -> do
-    binding.pry
     return unless course
     PipelineService.publish PipelineService::Nouns::ModuleItem.new(self)
   end
