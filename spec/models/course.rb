e6312334bedd7646dcb49ac504eff1a533f84b3c
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-04 11:06:36 -0700

    Placeholder tests

--- spec/models/course.rb
+++ spec/models/course.rb
@@ -7,4 +7,20 @@ describe Course do
       Course.create
     end
   end
+
+  describe "#no_active_students" do
+    let(:first_user) { User.create }
+    context "inactive student course" do
+      let(:inactive_course) { Course.create }
+      let(:inactive_enrollment_1) { Enrollment.create(course: inactive_coutse, user: first_user, workflow_state: 'invited') }
+
+      it "works with an inactive enrollment" do
+        expect(inactive_course.no_active_students?).to be true
+      end
+
+      it "works with no enrollments" do
+        expect(Course.create.no_active_students?).to be true
+      end
+    end
+  end
 end
