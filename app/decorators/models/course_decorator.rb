74a41e2d80b69150d5e18584ba82e4f803a82089
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-04 10:31:44 -0700

    No active students

--- app/decorators/models/course_decorator.rb
+++ app/decorators/models/course_decorator.rb
@@ -1,7 +1,13 @@
 Course.class_eval do
+  has_many :active_students, -> { where("enrollments.workflow_state NOT IN ('rejected', 'deleted', 'inactive', 'invited') AND enrollments.type = 'StudentEnrollment'").preload(:user) }, class_name: 'Enrollment'
+
   after_commit -> { PipelineService.publish(self) }
 
   def force_min_scores
     context_modules.each(&:force_min_score_to_requirements)
+  end
+
+  def no_active_students?
+    active_students.count.zero?
   end
 end
