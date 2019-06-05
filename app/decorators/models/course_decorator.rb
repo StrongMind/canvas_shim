77e374e727a5889796cea569ff13e6b04524d40d
Tesla Team <tesla@strongmind.com>
2019-06-05 13:38:34 -0700

    after_create callback enabled

--- app/decorators/models/course_decorator.rb
+++ app/decorators/models/course_decorator.rb
@@ -2,7 +2,7 @@ Course.class_eval do
   has_many :active_students, -> { where("enrollments.workflow_state NOT IN ('rejected', 'deleted', 'inactive', 'invited') AND enrollments.type = 'StudentEnrollment'").preload(:user) }, class_name: 'Enrollment'
 
   after_commit -> { PipelineService.publish(self) }
-  #after_create :set_default_course_threshold if account_threshold_set?
+  after_create :set_default_course_threshold, if: :account_threshold_set?
 
   def force_min_scores
     context_modules.each(&:force_min_score_to_requirements)
@@ -12,17 +12,21 @@ Course.class_eval do
     active_students.count.zero?
   end
 
-
   private
   def set_default_course_threshold
-    # update dynamo
+    SettingsService.update_settings(
+      object: 'school',
+      id: 1,
+      setting: 'score_threshold',
+      value: account_threshold
+    )
   end
 
   def account_threshold
-    # get setting
+    SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
   end
 
   def account_threshold_set?
-    # account_threshold.to_i.positive?
+    account_threshold.positive?
   end
 end
