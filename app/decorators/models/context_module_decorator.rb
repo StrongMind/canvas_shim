6504a837fb4917a85f03b718d62cccb9005091fb
AndyRosenbergStrongMind <46331093+AndyRosenbergStrongMind@users.noreply.github.com>
2019-06-06 09:30:39 -0700

    Play 1070 set default course threshold (#121)
    * initial setup
    
    * Refactorto assign thresholds if min score is different than actual threshold
    
    * after_create callback enabled
    
    * Correct naming for Dynamo setting
    
    * after_create tests
    
    * Pass in score threshold to add_min_score
    
    * refactoring
    
    * memoize score threshold
    
    * renamed account_score_threshold

--- app/decorators/models/context_module_decorator.rb
+++ app/decorators/models/context_module_decorator.rb
@@ -9,12 +9,7 @@ ContextModule.class_eval do
   end
 
   def force_min_score_to_requirements
-    completion_requirements.each do |requirement|
-      next unless ["must_submit", "must_contribute", "min_score"].include?(requirement[:type])
-      requirement[:type] = "min_score"
-      requirement[:min_score] = score_threshold
-    end
-
+    add_min_score_to_requirements
     update_column(:completion_requirements, completion_requirements)
     touch
   end
@@ -25,8 +20,12 @@ ContextModule.class_eval do
     threshold if threshold.positive?
   end
 
+  def account_score_threshold
+    SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
+  end
+
   def score_threshold
-    course_score_threshold? || SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
+    @score_threshold ||= (course_score_threshold? || account_score_threshold)
   end
 
   def threshold_set?
@@ -34,14 +33,21 @@ ContextModule.class_eval do
   end
 
   def threshold_changes_needed?
-    completion_requirements.any? { |req| ["must_submit", "must_contribute"].include?(req[:type]) }
+    completion_requirements.any? do |req|
+      ["must_submit", "must_contribute"].include?(req[:type]) ||
+      (req[:min_score] && req[:min_score] != score_threshold)
+    end
   end
 
   def add_min_score_to_requirements
-    completion_requirements.each do |requirement|
-      next unless ["must_submit", "must_contribute"].include?(requirement[:type])
-      requirement[:type] = "min_score"
-      requirement[:min_score] = score_threshold
+    completion_requirements.each do |requirement| 
+      next unless ["must_submit", "must_contribute", "min_score"].include?(requirement[:type])
+      update_score(requirement)
     end
+  end
+
+  def update_score(requirement)
+    requirement[:type] = "min_score"
+    requirement[:min_score] = score_threshold
   end
 end
--- app/decorators/models/course_decorator.rb
+++ app/decorators/models/course_decorator.rb
@@ -2,6 +2,7 @@ Course.class_eval do
   has_many :active_students, -> { where("enrollments.workflow_state NOT IN ('rejected', 'deleted', 'inactive', 'invited') AND enrollments.type = 'StudentEnrollment'").preload(:user) }, class_name: 'Enrollment'
 
   after_commit -> { PipelineService.publish(self) }
+  after_create :set_default_course_threshold, if: :account_threshold_set?
 
   def force_min_scores
     context_modules.each(&:force_min_score_to_requirements)
@@ -9,5 +10,23 @@ Course.class_eval do
 
   def no_active_students?
     active_students.count.zero?
+  end
+
+  private
+  def set_default_course_threshold
+    SettingsService.update_settings(
+      object: 'course',
+      id: self.id,
+      setting: 'passing_threshold',
+      value: account_threshold
+    )
+  end
+
+  def account_threshold
+    SettingsService.get_settings(object: :school, id: 1)['score_threshold'].to_f
+  end
+
+  def account_threshold_set?
+    account_threshold.positive?
   end
 end
