72788fd62f79aefa0c244a96dd9ba7f589df5106
Tesla Team <tesla@strongmind.com>
2019-06-06 10:46:00 -0700

    style updates

--- app/decorators/controllers/accounts_controller_decorator.rb
+++ app/decorators/controllers/accounts_controller_decorator.rb
@@ -19,7 +19,7 @@ AccountsController.class_eval do
     set_school_threshold if valid_threshold?(@school_threshold)
     set_holidays if params[:holidays]
     set_course_threshold_enablement
-    set_post_enrollment_thresholds if course_threshold_enablement_params
+    set_post_enrollment_thresholds
     set_module_editing
     instructure_update
   end
@@ -91,7 +91,7 @@ AccountsController.class_eval do
         object: 'school',
         id: 1,
         setting: 'enable_post_enrollment_threshold_updates',
-        value: enable_post_enrollment_threshold_params
+        value: (course_threshold_enablement_params && enable_post_enrollment_threshold_params)
       )
   end
 end
