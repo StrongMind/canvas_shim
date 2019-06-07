caed2d50eb4c69dd62bfd85a4d5b37998b7f2567
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-07 09:17:58 -0700

    Initial draft

--- app/decorators/controllers/context_modules_controller_decorator.rb
+++ app/decorators/controllers/context_modules_controller_decorator.rb
@@ -8,6 +8,15 @@ ContextModulesController.class_eval do
   alias_method :instructure_add_item, :add_item
   alias_method :add_item, :strongmind_add_item
 
+  def strongmind_update
+    @module = @context.context_modules.not_deleted.find(params[:id])
+    add_overrides if authorized_action(@module, @current_user, :update)
+    instructure_update
+  end
+
+  alias_method :instructure_update, :update
+  alias_method :update, :strongmind_update
+
   private
   def gradeable_tag_type?
     %w{Assignment DiscussionTopic Quizzes::Quiz}.include?(@tag.content_type)
@@ -16,5 +25,24 @@ ContextModulesController.class_eval do
   def add_threshold_to_module
     @module.completion_requirements << {:id => @tag.id, :type => "min_score", :min_score => score_threshold}
     @module.update_column(:completion_requirements, @module.completion_requirements)
+  end
+
+  def add_overrides
+    select_changed_requirements.each do |requirement|
+      requirement[:overridden] = true
+    end
+  end
+
+  def select_changed_requirements
+    context_module_params[:completion_requirements].to_h.select do |k, v|
+      requirement = @module.completion_requirements.find {|req| req[:id] == k.to_i }
+      if requirement
+        if v["type"] != requirement[:type]
+          true
+        elsif requirement[:min_score]
+          v[:min_score].to_f != requirement[:min_score]
+        end
+      end
+    end
   end
 end
