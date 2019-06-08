42edc711d79c6a9b80ab962fb1da96d0c991138b
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-07 18:23:43 -0700

    send ids to dynamo

--- app/decorators/controllers/context_modules_controller_decorator.rb
+++ app/decorators/controllers/context_modules_controller_decorator.rb
@@ -1,4 +1,13 @@
 ContextModulesController.class_eval do
+  def strongmind_update
+    @module = @context.context_modules.not_deleted.find(params[:id])
+    add_overrides if authorized_action(@module, @current_user, :update)
+    instructure_update
+  end
+
+  alias_method :instructure_update, :update
+  alias_method :update, :strongmind_update
+
   def strongmind_add_item
     instructure_add_item
     return unless gradeable_tag_type? && threshold_set?
@@ -16,5 +25,31 @@ ContextModulesController.class_eval do
   def add_threshold_to_module
     @module.completion_requirements << {:id => @tag.id, :type => "min_score", :min_score => score_threshold}
     @module.update_column(:completion_requirements, @module.completion_requirements)
+  end
+
+  def add_overrides
+    @changed_reqs = []
+    context_module_params.to_h.each do |k, v|
+      requirement = @module.completion_requirements.find {|req| req[:id] == k.to_i }
+      if requirement && changed_requirement?(v, requirement)
+        @changed_reqs << requirement[:id]
+      end
+    end
+    send_overrides_to_settings
+  end
+
+  def send_overrides_to_settings
+    @changed_reqs = @changed_reqs.blank? ? false : @changed_reqs.join(",")
+    SettingsService.update_settings(
+      object: 'course',
+      id: @context.id,
+      setting: 'threshold_overrides',
+      value: @changed_reqs
+    )
+  end
+
+  def changed_requirement?(param_requirement, current_requirement)
+    param_requirement["type"] != current_requirement[:type] ||
+    (current_requirement[:min_score] && param_requirement[:min_score].to_f != current_requirement[:min_score])
   end
 end
