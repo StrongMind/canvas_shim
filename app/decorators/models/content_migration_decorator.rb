5dba182d97734e8bff52f15a756e0bd6186ac042
Tesla Team <tesla@strongmind.com>
2019-06-10 10:51:53 -0700

    changed_and_imported

--- app/decorators/models/content_migration_decorator.rb
+++ app/decorators/models/content_migration_decorator.rb
@@ -1,9 +1,12 @@
 ContentMigration.class_eval do
-  before_save :set_min_scores, if: :workflow_state_changed?
+  before_save :set_min_scores, if: :changed_and_imported?
 
   def set_min_scores
-    if imported? && self.try(:course)
-      course.context_modules.each(&:assign_threshold)
-    end
+    course.context_modules.each(&:assign_threshold) if self.try(:course)
+  end
+
+  private
+  def changed_and_imported?
+    workflow_state_changed? && imported?
   end
 end
