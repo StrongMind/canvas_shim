9f7a5487a9b2d67ba83d11cab54c99bf5e71f673
Chris Young <beesucker@gmail.com>
2019-06-10 12:26:03 -0700

    payload builder mixin

--- app/services/alerts_service/alerts/max_attempts_reached.rb
+++ app/services/alerts_service/alerts/max_attempts_reached.rb
@@ -1,13 +1,16 @@
 module AlertsService
   module Alerts
     class MaxAttemptsReached
-      attr_accessor :teacher_id, :student_id, :assignment_id, :type
-      
+      def self.required_attributes
+        [:teacher_id, :student_id, :assignment_id]
+      end
+
+      include PayloadBuilder
+
       def initialize(teacher_id:, student_id:, assignment_id:)
         @teacher_id = teacher_id
         @student_id = student_id
         @assignment_id = assignment_id
-        @type = 'max_attempts_reached'
       end
 
       def as_json
@@ -18,31 +21,9 @@ module AlertsService
           type: type
         }
       end
-      
-      # Return a list of alerts from json
-      def self.list_from_json(json)
-        JSON.parse(json, symbolize_names: true).tap do |parsed|
-          return parsed.map do |attributes|
-            from_payload_attributes(attributes)
-          end
-        end
-      end
 
-      # Return a single alert from json
-      def self.from_json(json)
-        JSON.parse(json, symbolize_names: true).tap do |attributes|
-          return from_payload_attributes(attributes)  
-        end
-      end
-
-      # Return an instance by pulling the required attributes 
-      # from a hash
-      def self.from_payload_attributes(attributes)
-        new(
-          teacher_id: attributes[:teacher_id],
-          student_id: attributes[:student_id],
-          assignment_id: attributes[:assignment_id]
-        )
+      def type
+        'max_attempts_reached'
       end
     end
   end
