15600546b476f8f4e007de1e60a93d84b7fa5ee2
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-10 19:44:10 -0700

    syntax fix

--- app/assets/javascripts/canvas_shim/account_settings.js
+++ app/assets/javascripts/canvas_shim/account_settings.js
@@ -6,7 +6,7 @@ $(window).on("load", function(event) {
     var passThreshDisabled = $('#account_settings_score_threshold').prop('disabled');
     $('#account_settings_score_threshold').prop('disabled', !passThreshDisabled);
     $("#threshold_edited").remove();
-    $(this).append("<input type='hidden' id='threshold_edited' name='threshold_edited' value=" + passThreshEnabled + " />")
+    $(this).append("<input type='hidden' id='threshold_edited' name='threshold_edited' value=" + passThreshDisabled + " />")
     if (passThreshDisabled) {
       $('#account_settings_score_threshold').val(initialVal);
     }
