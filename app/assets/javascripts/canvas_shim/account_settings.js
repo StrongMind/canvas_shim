c10f0f369cd23debf8a792227b23acf7773e0f06
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-06-10 20:31:46 -0700

    UI fix

--- app/assets/javascripts/canvas_shim/account_settings.js
+++ app/assets/javascripts/canvas_shim/account_settings.js
@@ -1,13 +1,13 @@
 $(window).on("load", function(event) {
+  var initialVal = $('#account_settings_score_threshold').val();
   $('#edit_school_threshold_btn').click(function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var initialVal = $('#account_settings_score_threshold').val();
     var passThreshDisabled = $('#account_settings_score_threshold').prop('disabled');
     $('#account_settings_score_threshold').prop('disabled', !passThreshDisabled);
     $("#threshold_edited").remove();
     $(this).append("<input type='hidden' id='threshold_edited' name='threshold_edited' value=" + passThreshDisabled + " />")
-    if (passThreshDisabled) {
+    if ($('#account_settings_score_threshold').prop('disabled')) {
       $('#account_settings_score_threshold').val(initialVal);
     }
   });
--- app/assets/javascripts/canvas_shim/course_settings.js
+++ app/assets/javascripts/canvas_shim/course_settings.js
@@ -1,13 +1,13 @@
 $(window).on("load", function(event) {
+  var initialVal = $('#passing_threshold').val();
   $('#edit_threshold_btn').click(function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var initialVal = $('#passing_threshold').val();
     var passThreshDisabled = $('#passing_threshold').prop('disabled');
     $('#passing_threshold').prop('disabled', !passThreshDisabled);
     $("#threshold_edited").remove();
     $(this).append("<input type='hidden' id='threshold_edited' name='threshold_edited' value=" + passThreshDisabled + " />")
-    if (passThreshDisabled) {
+    if ($('#passing_threshold').prop('disabled')) {
       $('#passing_threshold').val(initialVal);
     }
   });
