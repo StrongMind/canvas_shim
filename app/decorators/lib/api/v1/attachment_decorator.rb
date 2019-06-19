71712c1a2e18bf9237c4fd762105de28dd9006ad
Zac Brown <me@yupitszac.com>
2019-06-19 09:55:16 -0700

    [PLAY-1080] Ignored cartridge uploads from filetype restrictions

--- app/decorators/lib/api/v1/attachment_decorator.rb
+++ app/decorators/lib/api/v1/attachment_decorator.rb
@@ -5,16 +5,14 @@ Api::V1::Attachment.class_eval do
     def api_attachment_preflight(context, request, opts = {})
         params = opts[:params] || request.params
 
-        allowed_filetypes = SettingsService.get_settings(object: 'school', id: 1)['allowed_filetypes']
-        allowed_filetypes = allowed_filetypes.split(',') if allowed_filetypes
-        unless allowed_filetypes
-            render :json => {:message => I18n.t('lib.api.attachments.invalid_filetype', "Filetype not supported")}, :status => 422
-            return 
-        end
+        unless context.instance_of?(ContentMigration)
+            allowed_filetypes = SettingsService.get_settings(object: 'school', id: 1)['allowed_filetypes']
+            allowed_filetypes = allowed_filetypes ? allowed_filetypes.split(',') : []
 
-        unless allowed_filetypes.include?(File.extname(params[:name]))
-            render :json => {:message => I18n.t('lib.api.attachments.invalid_filetype', "Filetype not supported")}, :status => 422
-            return
+            unless allowed_filetypes.include?(File.extname(params[:name]))
+                render :json => {:message => I18n.t('lib.api.attachments.invalid_filetype', "Filetype not supported")}, :status => 422
+                return
+            end
         end
 
         api_attachment_preflight_alias(context, request, opts)
