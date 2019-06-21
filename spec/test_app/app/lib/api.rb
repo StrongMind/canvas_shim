62637fab4393b2c225b50378530c820dd9ab8035
Chris Young <beesucker@gmail.com>
2019-06-21 12:15:57 -0700

    set null scores to 0 (#140)
    * set null scores to 0
    
    * remove cruft
    
    * only change scores to 0
    
    * leave grades alone
    
    * rename for clarity
    
    * add jquery back

--- spec/test_app/app/lib/api.rb
+++ spec/test_app/app/lib/api.rb
@@ -1,8 +1,17 @@
 module Api
-    module V1
-        module User
-            def enrollment_json(object, user, options={})
-            end 
-        end
+  module V1
+    module User
+      def enrollment_json(object, user, options={})
+        {
+            "grades" => {
+                "html_url" => "",
+                "current_score" => 0,
+                "current_grade" => nil,
+                "final_score" => 0,
+                "final_grade" => nil
+            }  
+        }
+      end 
     end
+  end
 end
