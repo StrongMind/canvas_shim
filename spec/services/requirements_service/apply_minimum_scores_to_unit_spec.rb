f2f2e13eae2096629685c6a40dc52944062c2e2c
Tesla Team <tesla@strongmind.com>
2019-06-24 15:46:19 -0700

    tests for threshold_changes_needed

--- spec/services/requirements_service/apply_minimum_scores_to_unit_spec.rb
+++ spec/services/requirements_service/apply_minimum_scores_to_unit_spec.rb
@@ -23,15 +23,26 @@ describe RequirementsService::Commands::ApplyMinimumScoresToUnit do
     ]
   end
 
+  before do
+    allow(SettingsService).to receive(:get_settings).and_return('passing_threshold' => 70)
+  end
+
   describe '#call' do
     it 'does not strip overrides' do
       expect(subject).to_not receive(:strip_overrides)
+      expect(subject).to receive(:add_min_score_to_requirements)
       subject.call
     end
 
-    it 'wont run if there is no threshold or no changes are needed' do
-      expect(subject).to_not receive(:add_min_score_to_requirements)
-      subject.call
+    context 'passing threshold is not set' do
+      before do
+        allow(SettingsService).to receive(:get_settings).and_return('passing_threshold' => 0)
+      end
+    
+      it 'wont run if there is no threshold or no changes are needed' do
+        expect(subject).to_not receive(:add_min_score_to_requirements)
+        subject.call
+      end
     end
 
     context 'force clearing threshold overrides' do
