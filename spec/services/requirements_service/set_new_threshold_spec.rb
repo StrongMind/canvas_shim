688b641faffc2578a21057a7e6b6ff2028ca4ed8
Tesla Team <tesla@strongmind.com>
2019-06-25 11:49:12 -0700

    added spec

--- spec/services/requirements_service/set_new_threshold_spec.rb
+++ spec/services/requirements_service/set_new_threshold_spec.rb
@@ -1,48 +1,83 @@
 describe RequirementsService::Commands::SetNewThreshold do
   include_context 'stubbed_network'
   
-  context "Happy path" do
-    subject { described_class.new(type: 'school', threshold: 70, edited: 'true') }
+  describe "#call" do
+    context "Happy path school" do
+      subject { described_class.new(type: 'school', threshold: 70, edited: 'true') }
 
-    it "receives set_threshold" do
-      expect(subject).to receive(:set_threshold)
-      subject.call
+      it "receives set_threshold" do
+        expect(subject).to receive(:set_threshold)
+        subject.call
+      end
+
+      it "has the correct setting" do
+        new_threshold = {
+          object: 'school',
+          id: 1,
+          setting: "score_threshold",
+          value: 70
+        }
+
+        expect(SettingsService).to receive(:update_settings).with(new_threshold)
+        subject.call
+      end
     end
-  end
 
-  context "Sent a 0" do
-    subject { described_class.new(type: 'school', threshold: 0, edited: 'true') }
+    context "Happy path course" do
+      subject { described_class.new(type: 'course', threshold: 70, edited: 'true') }
 
-    it "receives set_threshold" do
-      expect(subject).to receive(:set_threshold)
-      subject.call
+      it "receives set_threshold" do
+        expect(subject).to receive(:set_threshold)
+        subject.call
+      end
+
+      it "has the correct setting" do
+        new_threshold = {
+          object: 'course',
+          id: 1,
+          setting: "passing_threshold",
+          value: 70
+        }
+
+        expect(SettingsService).to receive(:update_settings).with(new_threshold)
+        subject.call
+      end
     end
-  end
 
-  context "Threshold not edited" do
-    subject { described_class.new(type: 'school', threshold: 70, edited: 'false') }
+    context "Sent a 0" do
+      subject { described_class.new(type: 'school', threshold: 0, edited: 'true') }
 
-    it "does not receive set_threshold" do
-      expect(subject).to_not receive(:set_threshold)
-      subject.call
+      it "receives set_threshold" do
+        expect(subject).to receive(:set_threshold)
+        subject.call
+      end
     end
-  end
 
-  context "Threshold too high" do
-    subject { described_class.new(type: 'school', threshold: 101, edited: 'false') }
+    context "Threshold not edited" do
+      subject { described_class.new(type: 'school', threshold: 70, edited: 'false') }
 
-    it "does not receive set_threshold" do
-      expect(subject).to_not receive(:set_threshold)
-      subject.call
+      it "does not receive set_threshold" do
+        expect(subject).to_not receive(:set_threshold)
+        subject.call
+      end
     end
-  end
 
-  context "Threshold too low" do
-    subject { described_class.new(type: 'school', threshold: -1, edited: 'false') }
+    context "Threshold too high" do
+      subject { described_class.new(type: 'school', threshold: 101, edited: 'false') }
 
-    it "does not receive set_threshold" do
-      expect(subject).to_not receive(:set_threshold)
-      subject.call
+      it "does not receive set_threshold" do
+        expect(subject).to_not receive(:set_threshold)
+        subject.call
+      end
+    end
+
+    context "Threshold too low" do
+      subject { described_class.new(type: 'school', threshold: -1, edited: 'false') }
+
+      it "does not receive set_threshold" do
+        expect(subject).to_not receive(:set_threshold)
+        subject.call
+      end
     end
   end
 end
