fc64f2804b62c071c95282290cec2b56327521d1
Tesla Team <tesla@strongmind.com>
2019-06-26 10:12:14 -0700

    Add threshold overrides

--- app/decorators/controllers/context_modules_controller_decorator.rb
+++ app/decorators/controllers/context_modules_controller_decorator.rb
@@ -10,7 +10,7 @@ ContextModulesController.class_eval do
 
   def strongmind_add_item
     instructure_add_item
-    return unless gradeable_tag_type? && RequirementsService.course_has_set_threshold?(@context)
+    return unless can_add_threshold_overrides?
     add_threshold_to_module
   end
 
@@ -41,61 +41,10 @@ ContextModulesController.class_eval do
   alias_method :item_redirect, :strongmind_item_redirect
 
   private
-  def gradeable_tag_type?
-    %w{Assignment DiscussionTopic Quizzes::Quiz}.include?(@tag.content_type)
-  end
-
-  def add_threshold_to_module
-    @module.completion_requirements << {:id => @tag.id, :type => "min_score", :min_score => score_threshold}
-    @module.update_column(:completion_requirements, @module.completion_requirements)
-  end
 
   def can_add_threshold_overrides?
     RequirementsService.course_has_set_threshold?(@context) && RequirementsService.module_editing_enabled? &&
     context_module_params[:completion_requirements] && authorized_action(@module, @current_user, :update)
-  end
-
-  def add_threshold_overrides
-    @changed_reqs = []
-    context_module_params[:completion_requirements].each do |k, v|
-      requirement = @module.completion_requirements.find {|req| req[:id] == k.to_i }
-      if requirement && changed_requirement?(v, requirement)
-        @changed_reqs << requirement[:id]
-      end
-    end
-    send_overrides_to_settings
-  end
-
-  def send_overrides_to_settings
-    @all_reqs = conjoin_threshold_overrides(@changed_reqs)
-    @all_reqs = @all_reqs.blank? ? false : @all_reqs.join(",")
-    SettingsService.update_settings(
-      object: 'course',
-      id: @context.id,
-      setting: 'threshold_overrides',
-      value: @all_reqs
-    )
-  end
-
-  def get_threshold_overrides
-    @threshold_overrides ||= SettingsService.get_settings(object: :course, id: @context.try(:id))['threshold_overrides']
-  end
-
-  def conjoin_threshold_overrides(new_overrides)
-    if get_threshold_overrides
-      current_overrides = get_threshold_overrides.split(",")
-      return current_overrides unless new_overrides.any?
-      current_overrides.concat(new_overrides).uniq
-    elsif new_overrides.any?
-      new_overrides
-    else
-      []
-    end
-  end
-
-  def changed_requirement?(param_requirement, current_requirement)
-    param_requirement["type"] != current_requirement[:type] ||
-    (current_requirement[:min_score] && param_requirement[:min_score].to_f != current_requirement[:min_score])
   end
 
   def find_max_attempts
