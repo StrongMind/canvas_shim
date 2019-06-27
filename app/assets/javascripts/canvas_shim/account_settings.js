94f7afceefc9d3ceea37433585c96ee7a58c6b98
Tesla Team <tesla@strongmind.com>
2019-06-27 13:02:26 -0700

    Initial setting of unit exam threshold

--- app/assets/javascripts/canvas_shim/account_settings.js
+++ app/assets/javascripts/canvas_shim/account_settings.js
@@ -11,4 +11,17 @@ $(window).on("load", function(event) {
       $('#account_settings_score_threshold').val(initialVal);
     }
   });
+
+  var initialUnitVal = $('#account_settings_unit_score_threshold').val();
+  $('#edit_school_unit_threshold_btn').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var unitPassThreshDisabled = $('#account_settings_unit_score_threshold').prop('disabled');
+    $('#account_settings_unit_score_threshold').prop('disabled', !unitPassThreshDisabled);
+    $("#unit_threshold_edited").remove();
+    $(this).append("<input type='hidden' id='unit_threshold_edited' name='unit_threshold_edited' value=" + unitPassThreshDisabled + " />")
+    if ($('#account_settings_unit_score_threshold').prop('disabled')) {
+      $('#account_settings_unit_score_threshold').val(initialUnitVal);
+    }
+  });
 });
