0fdeef969994bd05af26aaf8ce9502479efafac1
Chris Young <beesucker@gmail.com>
2019-07-08 13:36:32 -0700

    no need to call load attachments

--- app/services/pipeline_service/V2/nouns/submission.rb
+++ app/services/pipeline_service/V2/nouns/submission.rb
@@ -2,11 +2,11 @@ module PipelineService
   module V2
     module Nouns
       class Submission < Base
-
         include Api
         include Api::V1::Submission
         include Rails.application.routes.url_helpers
         include PipelineService::Serializers::BaseMethods
+
         Rails.application.routes.default_url_options[:host] = ENV['CANVAS_DOMAIN']
 
         def initialize(object:)
@@ -18,7 +18,6 @@ module PipelineService
         end
 
         def call
-          load_attachments
           submission_json(
             @submission,
             @submission.assignment,
@@ -34,17 +33,6 @@ module PipelineService
             Identifier.new(Proc.new {|ar_model| [:assignment_id, ar_model.assignment.id]}),
             Identifier.new(Proc.new {|ar_model| [:course_id, ar_model.assignment.course.id]})
           ]
-        end
-  
-        def load_attachments
-          submissions = [@submission]
-          ::Submission.bulk_load_versioned_attachments(submissions)
-          attachments = submissions.flat_map &:versioned_attachments
-          ActiveRecord::Associations::Preloader.new.preload(
-            attachments,
-            [:canvadoc, :crocodoc_document]
-          )
-          Version.preload_version_number(submissions)
         end
       end
     end
