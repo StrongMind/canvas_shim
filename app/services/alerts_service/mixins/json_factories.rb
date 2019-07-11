ae31e111e65ce44ba21cf52ec812a805595c8fc7
Chris Young <beesucker@gmail.com>
2019-07-11 12:57:13 -0700

    get rid of taps

--- app/services/alerts_service/mixins/json_factories.rb
+++ app/services/alerts_service/mixins/json_factories.rb
@@ -3,24 +3,25 @@ module AlertsService
     module JSONFactories
       module ClassMethods    
         def list_from_json(json)
-          JSON.parse(json, symbolize_names: true).tap do |parsed|
-            return parsed.map { |attributes| from_payload(attributes) }
+          JSON.parse(json, symbolize_names: true).map do |attributes| 
+            from_payload attributes
           end
         end
   
         def from_json(json)
-          JSON.parse(json, symbolize_names: true).tap do |attributes|
-            return from_payload(attributes)  
-          end
+          from_payload JSON.parse(json, symbolize_names: true)
+        end
+
+        def all_fields
+          self::ALERT_ATTRIBUTES + self::SERVICE_ATTRIBUTES
         end
   
-        def from_payload(attributes)
-          attributes.merge(attributes[:alert]).tap do |flattened|
-            flattened.delete(:alert)
-            return self.new((self::ALERT_ATTRIBUTES + self::SERVICE_ATTRIBUTES).map do |field_name|
-              [field_name, flattened[field_name]] 
-            end.to_h)
-          end
+        def from_payload(attributes)          
+          new(
+            all_fields.map do |field_name|
+              [field_name, attributes.merge(attributes[:alert])[field_name]] 
+            end.to_h
+          )
         end
       end
     end
