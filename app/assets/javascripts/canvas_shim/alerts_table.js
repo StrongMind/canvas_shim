43332dea35ff90daf4d23c91982526a689ee5f20
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-08-06 10:33:39 -0700

    Moved to its own file

---
+++ app/assets/javascripts/canvas_shim/alerts_table.js
@@ -0,0 +1,33 @@
+  var alertsTable;
+  var xIcon;
+
+  $(function(){
+    alertsTable = $('#alertsTable').DataTable(
+      {
+        "columnDefs": [
+          {
+            "targets": [4],
+            "orderable": false,
+            "searchable": false,
+          },
+          {
+            targets: [ 0, 1, 2, 3 ],
+            className: 'mdl-data-alertsTable__cell--non-numeric'
+          }
+        ]
+      }
+    );
+
+    $('#alertsTable .icon-x').click(function(e) {
+      xIcon = $(this);
+
+      $.ajax({
+        url: xIcon.data('url'),
+        type: 'DELETE',
+        data: { alert_id: xIcon.data('alert') },
+        success: function() {
+          alertsTable.row(xIcon.parents('tr')).remove().draw();
+        }
+      });
+    });
+  });
