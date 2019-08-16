506427fb5e010dd118e3a6911198498b21ce0189
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-08-16 13:20:12 -0700

    Got excused discussion ids

--- app/decorators/controllers/discussion_topics_controller_decorator.rb
+++ app/decorators/controllers/discussion_topics_controller_decorator.rb
@@ -1,7 +1,8 @@
 DiscussionTopicsController.class_eval do
   def strongmind_index
-    instructure_index
+    @discussions = @context&.discussion_topics
     js_env(excused_discussions: excused_discussion_topics)
+    instructure_index
   end
 
   alias_method :instructure_index, :index
@@ -10,13 +11,12 @@ DiscussionTopicsController.class_eval do
   private
   def excused_discussion_topics
     return [] unless topics_available?
-    @topics.map do |topic|
-      return nil unless topic.is_excused?(@current_user)
-      "##{id}_discussion_content"
+    @discussions.map do |topic|
+      topic.is_excused?(@current_user) ? "##{topic.id}_discussion_content" : nil
     end.compact
   end
 
   def topics_available?
-    @topics && @context.is_a?(Course) && @context.user_is_student?(@current_user)
+    @discussions && @context.is_a?(Course) && @context.user_is_student?(@current_user)
   end
 end
