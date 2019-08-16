f3c12f3a6da92d5d509a1b4d9a1e06cb4f80a69f
Tesla Team <tesla@strongmind.com>
2019-08-15 17:23:36 -0700

    broken test

--- spec/services/pipeline_service/V2/nouns/pseudonym_spec.rb
+++ spec/services/pipeline_service/V2/nouns/pseudonym_spec.rb
@@ -3,14 +3,14 @@ describe PipelineService::V2::Nouns::Pseudonym do
 
   subject { described_class.new(object: noun) }
 
-  let(:User) { ::User.create }
-  let!(:active_record_object) { ::Pseudonym.create(user_id: active_record_object.id, sis_user_id: '1234') }
+  let(:active_record_object) { ::Pseudonym.create(user_id: user.id, sis_user_id: '1234') }
+  let!(:user) { ::User.create }
 
   let(:noun) { PipelineService::V2::Noun.new(active_record_object)}
 
-  describe '#call'do
+  describe '#call' do
     it 'returns with last login time' do
-      expect(subject.call).to include({})
+      expect(subject.call).to eq({})
     end
   end
 end
