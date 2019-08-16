9d54907f763cc9569144e96677ec049701689b62
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-08-16 14:26:01 -0700

    Elderly syntax but it works

---
+++ app/assets/javascripts/canvas_shim/excused_discussions.js
@@ -0,0 +1,14 @@
+$(window).on("load", function(event) {
+  if (ENV['excused_discussions'].length) {
+    var excusedInterval = setInterval(function() {
+      ENV['excused_discussions'].forEach(function(topic) { 
+        var discussion = $(topic).find('.title');
+        discussion.text(discussion.text() + " (Excused)");
+      });
+
+      if ($(ENV['excused_discussions'][0]).length) {
+        clearInterval(excusedInterval);
+      }
+    }, 1000);
+  }
+});
