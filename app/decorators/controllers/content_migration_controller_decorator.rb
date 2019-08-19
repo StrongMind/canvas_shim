3d013571c2262247a09d5c3b0b40cde6f7bc967d
Tesla Team <tesla@strongmind.com>
2019-08-19 16:17:41 -0700

    add due date nag environment var

--- app/decorators/controllers/content_migration_controller_decorator.rb
+++ app/decorators/controllers/content_migration_controller_decorator.rb
@@ -1,7 +1,13 @@
 ContentMigrationsController.class_eval do 
 
   def strongmind_index
-    js_env(:AUTO_DUE_DATES => SettingsService.get_settings(object: :school, id: 1)['auto_due_dates'] == 'on')
+    auto_due_dates = SettingsService.get_settings(object: :school, id: 1)['auto_due_dates'] == 'on'
+    js_env(:AUTO_DUE_DATES => auto_due_dates)
+    if auto_due_dates and !@context.try(:start_at) or !@context.try(:conclude_at)
+      js_env(:DUE_DATE_NAG => true)
+    else
+      js_env(:DUE_DATE_NAG => false)
+    end
     instructure_index
   end
 
