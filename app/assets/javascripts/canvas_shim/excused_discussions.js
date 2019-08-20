9ae718bc13bb32320de35d5cc7ca2b7ed57e3537
Jessica Swanson <jessica.swanson@strongmind.com>
2019-08-20 09:05:52 -0700

    Update excused tag appearance and styles

--- app/assets/javascripts/canvas_shim/excused_discussions.js
+++ app/assets/javascripts/canvas_shim/excused_discussions.js
@@ -1,9 +1,9 @@
 $(window).on("load", function(event) {
   if ((ENV['excused_discussions'] || []).length) {
     var excusedInterval = setInterval(function() {
-      ENV['excused_discussions'].forEach(function(topic) { 
+      ENV['excused_discussions'].forEach(function(topic) {
         var discussion = $(topic).find('.title');
-        discussion.text(discussion.text() + " (Excused)");
+        discussion.addClass('excused-discussion');
       });
 
       if ($(ENV['excused_discussions'][0]).length) {
