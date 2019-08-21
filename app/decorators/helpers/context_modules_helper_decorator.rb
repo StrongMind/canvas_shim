82cb186d4af8e07e465d1f53401bf1b823e7af01
Jessica Swanson <jessica.swanson@strongmind.com>
2019-08-21 10:51:36 -0700

    Homogenize excuse tag styles

--- app/decorators/helpers/context_modules_helper_decorator.rb
+++ app/decorators/helpers/context_modules_helper_decorator.rb
@@ -1,5 +1,5 @@
 ContextModulesHelper.class_eval do
   def module_item_excused?(module_item)
-    I18n.t("(Excused)") if module_item && module_item.try_rescue(:assignment).try_rescue(:is_excused?, @current_user)
+    module_item && module_item.try_rescue(:assignment).try_rescue(:is_excused?, @current_user)
   end
 end
