16da2d815c764c0180a7f116337a000f0cff3bc6
Tesla Team <tesla@strongmind.com>
2019-08-27 13:23:49 -0700

    Remove cruft

--- app/services/excused_service/commands/handle_exclusions.rb
+++ app/services/excused_service/commands/handle_exclusions.rb
@@ -20,7 +20,7 @@ module ExcusedService
               exclusions.each do |student|
                 assignment.toggle_exclusion(student['id'].to_i, true)
               end
-              unexcuse_assignments(exclusions)
+              unexcuse_assignments
             end
           rescue StandardError => exception
             Raven.capture_exception(exception)
@@ -28,8 +28,8 @@ module ExcusedService
         end
       end
 
-      def unexcuse_assignments(arr)
-        student_ids = arr.map { |student| student['id'] }
+      def unexcuse_assignments
+        student_ids = exclusions.map { |student| student['id'] }
         excused = assignment.excused_submissions
         excused = excused.where("user_id NOT IN (?)", student_ids) if student_ids.any?
         excused.each do |sub|
