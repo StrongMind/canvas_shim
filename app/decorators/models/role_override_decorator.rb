d6f9f7b8151d25ec41f7d31602802ecb222947e2
Tesla Team <tesla@strongmind.com>
2019-09-05 15:10:36 -0700

    Take away initializer

--- app/decorators/models/role_override_decorator.rb
+++ app/decorators/models/role_override_decorator.rb
@@ -1,15 +1,37 @@
 RoleOverride.class_eval do
   Permissions.register({
-    :beer_truck => {
-      :label => lambda { "Beer Truck" },
+    :custom_placement => {
+      :label => lambda { "Apply Custom Placement to Courses" },
       :available_to => [
         'TaEnrollment',
         'TeacherEnrollment',
         'AccountAdmin',
-        'AccountMembership'
       ],
       :true_for => [
         'AccountAdmin',
+      ]
+    },
+    :delete_inbox_messages => {
+      :label => lambda { "Delete inbox messages" },
+      :available_to => [
+        'StudentEnrollment',
+        'TaEnrollment',
+        'TeacherEnrollment',
+        'DesignerEnrollment',
+        'TeacherlessStudentEnrollment',
+        'ObserverEnrollment',
+        'AccountAdmin',
+        'AccountMembership'
+      ],
+      :true_for => [
+        'StudentEnrollment',
+        'TaEnrollment',
+        'TeacherEnrollment',
+        'DesignerEnrollment',
+        'TeacherlessStudentEnrollment',
+        'ObserverEnrollment',
+        'AccountAdmin',
+        'AccountMembership'
       ]
     }
   })
