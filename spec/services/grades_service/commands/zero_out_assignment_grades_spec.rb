f2664284ab1b9dd11344cb987e44d1ef0643dd4b
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-09-25 16:25:44 -0700

    Fabricated assignment id in zg specs

--- spec/services/grades_service/commands/zero_out_assignment_grades_spec.rb
+++ spec/services/grades_service/commands/zero_out_assignment_grades_spec.rb
@@ -11,6 +11,7 @@ describe GradesService::Commands::ZeroOutAssignmentGrades do
   let(:assignment) do
     double(
       "assignment",
+      id: 1,
       grade_student: nil,
       published?: true,
       due_at: nil,
