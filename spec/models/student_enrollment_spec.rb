fe94581042494b538631350bb954fd524cb116f3
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-09-25 18:00:54 -0700

    Got rid of weird time issue

--- spec/models/student_enrollment_spec.rb
+++ spec/models/student_enrollment_spec.rb
@@ -25,7 +25,7 @@ describe StudentEnrollment do
      end
 
      it "returns two days ago" do
-       subject.update(last_activity_at: (3.days.ago - 1.hour))
+       subject.update(last_activity_at: (3.days.ago))
        expect(subject.days_since_active).to eq 3
      end
   end
@@ -41,7 +41,7 @@ describe StudentEnrollment do
     end
 
      it "returns one day ago" do
-       submission.update(submitted_at: (2.days.ago - 1.hour))
+       submission.update(submitted_at: (2.days.ago))
        expect(subject.days_since_last_submission).to eq 2
      end
   end
