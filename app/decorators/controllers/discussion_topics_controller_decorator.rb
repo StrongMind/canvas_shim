5641b6ee2416fd0cf1a787746b8a5da667555f96
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-09-26 11:41:22 -0700

    Don't need discussion edit env var

--- app/decorators/controllers/discussion_topics_controller_decorator.rb
+++ app/decorators/controllers/discussion_topics_controller_decorator.rb
@@ -10,14 +10,6 @@ DiscussionTopicsController.class_eval do
   alias_method :instructure_index, :index
   alias_method :index, :strongmind_index
 
-  def strongmind_edit
-    js_env(no_assignment: !get_discussion_assignment)
-    instructure_edit
-  end
-
-  alias_method :instructure_edit, :edit
-  alias_method :edit, :strongmind_edit
-
   def strongmind_update
     get_discussion_assignment
     ExcusedService.bulk_excuse(assignment: @assignment, exclusions: params['excluded_students'])
