87e9c655c5ebbd9b16dc7b16649b66b105d9e7e9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-11-12 15:39:06 -0700

    Strand tall

--- app/services/assignments_service.rb
+++ app/services/assignments_service.rb
@@ -14,12 +14,12 @@ module AssignmentsService
 
   def self.distribute_dates_job(args={})
     command = Commands::DistributeDueDates.new(course: args[:course])
-    send_later(command)
+    send_later(command, args[:course])
   end
 
   def self.clear_due_dates(course:)
     command = Commands::ClearDueDates.new(course: course)
-    send_later(command)
+    send_later(command, course)
   end
 
   def self.clear_due_dates!(course:)
@@ -30,11 +30,9 @@ module AssignmentsService
     Delayed::Job.current.exists?(strand: "course_due_dates:#{course.try(:id)}")
   end
 
-  def self.send_later(command)
-    command.send_later_if_production_enqueue_args(
-      :perform,
-      { strand: "course_due_dates:#{course.try(:id)}" },
-      { course: course }
+  def self.send_later(command, course)
+    command.send_later_enqueue_args(
+      :perform, { strand: "course_due_dates:#{course.try(:id)}" }
     )
   end
 end
