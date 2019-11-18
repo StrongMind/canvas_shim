f0a475739c6b1b9e042d8eb664d16f5d5d784d66
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-11-18 16:24:35 -0700

    Fake app args

--- spec/test_app/lib/api/v1/submission.rb
+++ spec/test_app/lib/api/v1/submission.rb
@@ -1,5 +1,5 @@
 module Api::V1::Submission
-  def submission_json submission, assignment, user, session, context=nil, includes=[]
+  def submission_json submission, assignment, user, session, context=nil, includes=[], timestamps=nil
     {}
   end
 end
