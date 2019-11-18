f7480f537142d2c13c333c75599450847cc2893d
Andy Rosenberg, Jessica Swanson, and John VanBuskirk <tesla+andy+jessica+john@strongmind.com>
2019-11-18 16:10:44 -0700

    its in lms

--- spec/services/pipeline_service/V2/payload_spec.rb
+++ spec/services/pipeline_service/V2/payload_spec.rb
@@ -12,16 +12,12 @@ describe PipelineService::V2::Payload do
       PageView.new)).call
   end
 
-
-
   it 'adds additional identifiers to submissions' do
     res = described_class.new(object: PipelineService::V2::Noun.new(submission)).call
     expect(res[:identifiers]).to eq ({
         :assignment_id => assignment.id,
         :course_id => course.id,
         :id => submission.id,
-        :created_at => submission.created_at,
-        :updated_at => submission.updated_at
       })
   end
 end
