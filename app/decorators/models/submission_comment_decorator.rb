f0ad1fb2c2172f228b27ba5cfcd7c08641bf26eb
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-01-14 13:40:23 -0700

    Initial setup

---
+++ app/decorators/models/submission_comment_decorator.rb
@@ -0,0 +1,30 @@
+SubmissionComment.class_eval do
+  after_create :send_feedback_alert, if: :is_submission_comment_from_student?
+
+  def is_submission_comment_from_student?
+    return unless reply_alerts_on? && context.is_a?(Course)
+    !teacher_ids.include?(author_id)
+  end
+
+  def send_feedback_alert
+    teacher_ids.each do |teacher_id|
+      AlertsService::Client.create(
+        :student_feedback,
+        teacher_id: teacher_id,
+        student_id: author_id,
+        assignment_id: submission.assignment_id,
+        course_id: context_id,
+        comment: comment
+      )
+    end
+  end
+
+  private
+  def teacher_ids
+    context.teacher_enrollments.pluck(:id)
+  end
+
+  def reply_alerts_on?
+    SettingsService.get_settings(object: :school, id: 1)['reply_alerts']
+  end
+end
---
+++ app/services/alerts_service/alerts/student_feedback.rb
@@ -0,0 +1,27 @@
+module AlertsService
+  module Alerts
+    class StudentFeedback < Alert
+      ALERT_ATTRIBUTES = %i{ teacher_id student_id assignment_id comment course_id}
+
+      def initialize(atts)
+        super
+      end
+
+      def assignment
+        Assignment.find(assignment_id)
+      end
+
+      def student
+        User.find(student_id)
+      end
+
+      def detail
+        @comment
+      end
+
+      def description
+        'A Student Provided Feedback On An Assignment'
+      end
+    end
+  end
+end
