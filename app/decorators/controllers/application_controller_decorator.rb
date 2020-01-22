e3b7b2a73269ab2fa5d2968be3785bc6f56e6f0b
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-01-22 09:36:02 -0700

    Nil guard

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -38,7 +38,7 @@ ApplicationController.class_eval do
   private
   def destructive_options_hidden?
     get_context
-    return false if @current_user.roles(@domain_root_account).include?('admin')
+    return false if @current_user && @current_user.roles(@domain_root_account).include?('admin')
     SettingsService.get_settings(object: :school, id: 1)['hide_destructive_course_options']
   end
 end
