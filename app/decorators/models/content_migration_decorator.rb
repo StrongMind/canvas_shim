3038c2f0cb733632674fdff988c10b11258983fc
Tesla Team <tesla@strongmind.com>
2020-02-03 10:02:08 -0700

    full send, no half sends

--- app/decorators/models/content_migration_decorator.rb
+++ app/decorators/models/content_migration_decorator.rb
@@ -1,11 +1,3 @@
 ContentMigration.class_eval do
-  after_save :publish_after_imported
-
-  def publish_after_imported
-    if changes[:workflow_state] and self.workflow_state == "imported"
-      PipelineService::V2.publish(self)
-    end
-  end
-
-
+  after_save { PipelineService::V2.publish(self) }
 end
--- spec/models/content_migration_spec.rb
+++ spec/models/content_migration_spec.rb
@@ -1,20 +1,10 @@
 describe "ContentMigration" do
   include_context "stubbed_network"
 
-  context "when workflow state hasn't changed to imported" do
-    it 'wont publish' do
-      expect(PipelineService::V2).to_not receive(:publish).with an_instance_of(ContentMigration)
-      ContentMigration.create
-    end
-  end
-
-  context "when workflow state changes to imported" do
-    it 'will publish' do
+    it 'will publish when saved' do
       expect(PipelineService::V2).to receive(:publish).with an_instance_of(ContentMigration)
-      cm = ContentMigration.create(workflow_state: 'importing')
+      cm = ContentMigration.create()
       cm.save
-      cm.update(workflow_state: 'imported')
     end
-  end
 
 end
