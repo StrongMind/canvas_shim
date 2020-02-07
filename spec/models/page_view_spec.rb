28213824c212e2000a372263876365e96d2ab98d
Tesla Team <tesla@strongmind.com>
2020-02-07 12:48:57 -0700

    add update_time spec

--- spec/models/page_view_spec.rb
+++ spec/models/page_view_spec.rb
@@ -2,6 +2,8 @@ describe PageView do
   include_context "stubbed_network"
 
   describe "#after_commit" do
+    let(:user) {User.create}
+
     before do
       allow(SettingsService).to receive(:update_settings)
     end
@@ -22,5 +24,14 @@ describe PageView do
         PageView.create
       end
     end
+
+    it 'updates the users last access time' do
+        Timecop.freeze
+        current_time = Time.now
+        expect(SettingsService).to receive(:update_settings).with({:id=>1, :object=>"user", :setting=>"last_access_time", :value=> current_time.utc.to_s})
+        PageView.create
+        Timecop.return
+    end
+
   end
 end
