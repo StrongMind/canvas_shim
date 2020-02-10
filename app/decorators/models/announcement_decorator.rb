24de063be7d142801e43aaf7d28cfe27f95adb09
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-02-10 16:42:58 -0700

    Mo chex

--- app/decorators/models/announcement_decorator.rb
+++ app/decorators/models/announcement_decorator.rb
@@ -1,9 +1,9 @@
 Announcement.class_eval do
   def toggle_pin(pin_announcements)
-    if pin_announcements.include?(announcement.id.to_s)
-      announcement.update(pinned: true) unless announcement.pinned
-    elsif announcement.pinned
-      announcement.update(pinned: false)
+    if pin_announcements.include?(id.to_s)
+      update(pinned: true) unless pinned
+    elsif pinned
+      update(pinned: false)
     end
   end
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,3 +1,36 @@
 describe Announcement do
   include_context "stubbed_network"
+
+  describe "#toggle_pin" do
+    let(:pinned_announcement) { Announcement.create(pinned: true) }
+    let(:unpinned_announcement) { Announcement.create(pinned: false) }
+
+    context "no pins" do
+      let(:pin_params) {[]}
+      
+      it "sets pinned to false" do
+        pinned_announcement.toggle_pin(pin_params)
+        expect(pinned_announcement.pinned).to eq(false)
+      end
+
+      it "does not update if already false" do
+        expect(unpinned_announcement).to_not receive(:update)
+        unpinned_announcement.toggle_pin(pin_params)
+      end
+    end
+
+    context "has pins" do
+      let(:pin_params) {["#{pinned_announcement.id}", "#{unpinned_announcement.id}"]}
+
+      it "sets pinned to true" do
+        unpinned_announcement.toggle_pin(pin_params)
+        expect(unpinned_announcement.pinned).to eq(true)
+      end
+
+      it "does not update if already true" do
+        expect(pinned_announcement).to_not receive(:update)
+        pinned_announcement.toggle_pin(pin_params)
+      end
+    end
+  end
 end
