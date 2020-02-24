2cfd21d1577856429e11c37bad1da5c68e6c90af
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-02-24 16:15:22 -0700

    Run service if non strongmind cartridge

--- app/decorators/models/content_migration_decorator.rb
+++ app/decorators/models/content_migration_decorator.rb
@@ -1,3 +1,8 @@
 ContentMigration.class_eval do
   after_save { PipelineService::V2.publish(self) }
+  after_save -> { RequirementsService.set_third_party_requirements(course: context) }, if: :non_strongmind_cartridge?
+
+  def non_strongmind_cartridge?
+    imported? && Rails.cache.read("#{context_id}_vendor") != 'StrongMind'
+  end
 end
