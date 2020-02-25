7e75fdc76c80d8eefedd4150b2808fc14e2a411a
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-02-25 15:12:10 -0700

    Spec for set_requirements in new service

---
+++ spec/services/requirements_service/default_third_party_requirements_spec.rb
@@ -0,0 +1,33 @@
+describe RequirementsService::Commands::DefaultThirdPartyRequirements do
+  subject { described_class.new(context_module: context_module) }
+
+  let(:context_module) do 
+    double(
+      'context module', 
+      completion_requirements: [],
+      update_column: nil,
+      touch: nil,
+      content_tags: content_tags,
+    )
+  end
+
+  let(:content_tags) do
+    [
+      double(:content_tag, content_type: "Assignment", id: 1),
+      double(:content_tag, content_type: "DiscussionTopic", id: 2),
+      double(:content_tag, content_type: "WikiPage", id: 3),
+      double(:content_tag, content_type: "ContextExternalTool", id: 4),
+    ]
+  end
+
+  describe "#set_requirements" do
+    before do
+      subject.send(:set_requirements)
+    end
+
+    it "Sets an assignment to must_submit" do
+      assignment = subject.send(:completion_requirements).find {|req| req[:id] == 1 }
+      expect(assignment[:type]).to eq("must_submit")
+    end
+  end
+end
