527b7caba55e5a1916cc3f4a98a1504797a48410
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-03-17 11:05:36 -0700

    Case insensitive specs

--- app/decorators/models/context_external_tool_spec.rb
+++ app/decorators/models/context_external_tool_spec.rb
@@ -16,6 +16,21 @@ describe "ContentMigration" do
   it 'will return false when the domains are listed in the settings service' do
     expect(context_thirdparty_tool.is_oauth_lti_domain?).to eq false
   end
+
+  context "Case changes" do
+    before do
+      allow(SettingsService).to receive(:global_settings).and_return({ 'oauth_lti_domains' => 'sTrOnGmInD.cOm' })
+    end
+
+    it 'will return true when the domains are listed in the settings service' do
+      expect(context_external_tool.is_oauth_lti_domain?).to eq true
+    end
+
+    it "will also respond to tool domain case differences" do
+      cased_tool = ContextExternalTool.create(domain: 'LTI.STRONGMIND.COM')
+      expect(context_external_tool.is_oauth_lti_domain?).to eq true
+    end
+  end
 end
 
 
