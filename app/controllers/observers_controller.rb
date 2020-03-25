3b18e078ce23a38b2ba22ea82055b8c0bb0dc216
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-03-25 16:46:09 -0700

    Modified permissions check

--- app/controllers/observers_controller.rb
+++ app/controllers/observers_controller.rb
@@ -1,5 +1,13 @@
 class ObserversController < ApplicationController
   def index
-    render_unauthorized_action unless @current_user.observer_enrollments.any?
+    render_unauthorized_action unless can_see_observer_dashboard?
+  end
+
+  private
+  def can_see_observer_dashboard?
+    @current_user && (
+      @current_user.roles(@domain_root_account).include?('admin') ||
+      @current_user.observer_enrollments.any?
+    )
   end
 end
