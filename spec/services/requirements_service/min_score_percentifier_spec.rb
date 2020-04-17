7b6882792baabe30fbda72a83f1956e81077dca5
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-04-17 10:30:47 -0700

    Chex

---
+++ spec/services/requirements_service/min_score_percentifier_spec.rb
@@ -0,0 +1,73 @@
+describe RequirementsService::Commands::MinScorePercentifier do
+  subject { described_class.new(content_tag: content_tag, passing_threshold: 50) }
+
+  let(:context_module) do 
+    double(
+      'context module', 
+      completion_requirements: [],
+      update_column: nil,
+      touch: nil,
+      content_tags: content_tags,
+    )
+  end
+
+  let(:content_tag) do
+    double(:content_tag, content: assignment)
+  end
+
+  let(:assignment) do
+    Assignment.create(points_possible: 50)
+  end
+
+  describe "#points_possible" do
+    context "no content" do
+      let(:content_tag) do
+        double(:content_tag, content: nil)
+      end
+
+      it "returns 100" do
+        expect(subject.send(:points_possible)).to eq(100.0)
+      end
+    end
+
+    context "Assignment with points possible" do
+      let(:content_tag) do
+        double(:content_tag, content: assignment)
+      end
+
+      it "returns assignment points possible" do
+        expect(subject.send(:points_possible)).to eq(50.0)
+      end
+    end
+
+    context "DiscussionTopic with Assignment with points possible" do
+      let(:content_tag) do
+        double(:content_tag, content: assignment)
+      end
+
+      let(:discussion_topic) do
+        DiscussionTopic.create(assignment: assignment)
+      end
+
+      it "returns assignment points possible" do
+        expect(subject.send(:points_possible)).to eq(50.0)
+      end
+    end
+  end
+
+  describe "#call" do
+    it "will return half the points possible" do
+      expect(subject.call).to eq(25.0)
+    end
+
+    context "no content" do
+      let(:content_tag) do
+        double(:content_tag, content: nil)
+      end
+
+      it "returns 100" do
+        expect(subject.call).to eq(50.0)
+      end
+    end
+  end
+end
