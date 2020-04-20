6fbab7dd14f0dd334ce418cdf5d1bd0bd9f7b200
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-04-20 10:42:01 -0700

    Bulk add draft

---
+++ app/decorators/controllers/user_observees_controller_decorator.rb
@@ -0,0 +1,33 @@
+UserObserveesController.class_eval do
+  def bulk_create
+    observee_ids.each do |obsv_id|
+      bulk_add_observee(bulk_observee(obsv_id))
+    end
+  end
+
+  private
+  def observee_ids
+    @observee_ids ||= params.permit(observee_ids: []).fetch("observee_ids", [])
+  end
+
+  def observer
+    @observer ||= api_find(User.active, params[:user_id])
+  end
+
+  def bulk_observee(id)
+    api_find(User.active, id)
+  end
+
+  def bulk_add_observee(observee)
+    observer.shard.activate do
+      unless bulk_has_observee?(observee)
+        observer.user_observees.create_or_restore(user_id: observee)
+        observer.touch
+      end
+    end
+  end
+
+  def bulk_has_observee?(observee)
+    observer.user_observees.active.where(user_id: observee).exists?
+  end
+end
