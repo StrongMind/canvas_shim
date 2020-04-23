8684ffdda5305d5362f0b7a1c7f5c4bb6076a57d
AndyRosenbergStrongMind <46331093+AndyRosenbergStrongMind@users.noreply.github.com>
2020-04-23 13:05:08 -0700

    Third party of thresh (#255)
    * Draft
    
    * put all third party stuff into one method
    
    * Take out lambda arrow

--- app/decorators/models/content_migration_decorator.rb
+++ app/decorators/models/content_migration_decorator.rb
@@ -1,7 +1,6 @@
 ContentMigration.class_eval do
   after_save { PipelineService::V2.publish(self) }
-  after_save -> { RequirementsService.set_third_party_requirements(course: context) }, if: :non_strongmind_cartridge?
-  after_save -> { update_oauth_settings? }, if: :non_strongmind_cartridge?
+  after_save :run_third_party_services, if: :non_strongmind_cartridge?
 
   def non_strongmind_cartridge?
     SettingsService.get_settings(object: :school, id: 1)['third_party_imports'] &&
@@ -12,5 +11,11 @@ ContentMigration.class_eval do
     self.context.context_external_tools.each do |cet|
       cet.set_setting_oauth_to_false unless cet.is_oauth_lti_domain?
     end
+  end
+
+  def run_third_party_services
+    RequirementsService.set_third_party_requirements(course: context)
+    RequirementsService.force_min_scores(course: context)
+    update_oauth_settings?
   end
 end
