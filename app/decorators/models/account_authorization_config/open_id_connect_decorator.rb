05a255ef22ad3e921eef79fbf2593c14bb2e5f0e
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-08 11:05:21 -0700

    switch private & public methods

--- app/decorators/models/account_authorization_config/open_id_connect_decorator.rb
+++ app/decorators/models/account_authorization_config/open_id_connect_decorator.rb
@@ -1,17 +1,18 @@
 AccountAuthorizationConfig::OpenIDConnect.class_eval do
-  def roles(token)
-    claims(token)["role"]
-  end
-
   def admin_role?(token)
     setting = SettingsService.global_settings["admin_roles"] || ""
     setting.split(",").any? { |role| roles(token).include?(role) }
   end
-  
-  private
+
   def identity_email_address(token)
     parsed_claims = claims(token)
     key = parsed_claims.keys.find { |key| key.split("/").last == "emailaddress" }
     parsed_claims[key]
+  end
+
+  private
+
+  def roles(token)
+    claims(token)["role"]
   end
 end
