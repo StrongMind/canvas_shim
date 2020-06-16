77ff1cbb1a1bec56150ccb14b3b44bc3af557e41
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-16 16:28:06 -0700

    Get data in format

--- app/controllers/cs_alerts_controller.rb
+++ app/controllers/cs_alerts_controller.rb
@@ -1,9 +1,11 @@
 class CsAlertsController < ApplicationController
+  include ERB::Util
+
   def index
   end
 
   def teacher_alerts
-    render :json => AlertsService::Client.teacher_alerts(@current_user.id).payload
+    render :json => mapped_teacher_alerts
   end
 
   def destroy
@@ -12,5 +14,31 @@ class CsAlertsController < ApplicationController
 
   def bulk_delete
     AlertsService::Client.bulk_delete(params[:alert_ids])
+  end
+
+  private
+  def mapped_teacher_alerts
+    AlertsService::Client.teacher_alerts(@current_user.id).payload.map do |alert|
+      assignment = alert.assignment
+
+      alert.as_json(include_root: false).merge(
+        "student_name" => alert.student.name,
+        "assignment_name" => assignment.try(:name),
+        "course_name" => assignment.try(:course).try(:name) || "-",
+        "alert_link" => alert_link(alert),
+        "updated_at" => alert.updated_at.try(:in_time_zone).try(:strftime, "%m/%d/%y %I:%M %P")
+      )
+    end
+  end
+
+  def alert_link(alert)
+    if alert.class == AlertsService::Alerts::StudentDiscussionEntry
+      "/courses/#{alert.assignment.course.id}/assignments/#{alert.assignment.id}#discussion_subentries"
+    else
+      [
+        "/courses/#{alert.assignment.course.id}/gradebook/speed_grader?assignment_id=#{alert.assignment.id}#",
+        url_encode("{\"student_id\":\"#{alert.student.id}\"}")
+      ].join
+    end
   end
 end
