756f9b75d43e085f1b6d7d7064d8e868a1c8d958
John Vanbuskirk <jvanbuskirk@strongmind.com>
2020-07-06 13:02:03 -0700

    spec the flag

--- spec/services/pipeline_service/serializers/course_spec.rb
+++ spec/services/pipeline_service/serializers/course_spec.rb
@@ -9,7 +9,8 @@ describe PipelineService::Serializers::Course do
     before do
       allow(SettingsService).to receive(:get_settings).and_return({
         'passing_threshold' => 50,
-        'passing_exam_threshold' => 50
+        'passing_exam_threshold' => 50,
+        'powerschool_integration' => true
       })
 
       allow(IdentifierMapperService::Client).to receive(:get_powerschool_course_id).and_return(254)
@@ -37,4 +38,37 @@ describe PipelineService::Serializers::Course do
         :powerschool_school_id => 452
       })
     end
+
+    context "powerschool intergration is unfeatured" do
+
+    before do
+      allow(SettingsService).to receive(:get_settings).and_return({
+        'passing_threshold' => 50,
+        'passing_exam_threshold' => 50,
+        'powerschool_integration' => false
+      })
+    end
+
+    it 'Return an attribute hash of the noun' do
+      expect(subject.call).to include({
+        noun.primary_key => active_record_object.id,
+        'passing_thresholds' => {
+          "assignment"=>50.0,
+          "exam"=>50.0
+        }
+      })
+    end
+
+    it 'Excludes the powerschool_course_id attribute' do
+      expect(subject.call).to_not include({
+        :powerschool_course_id => 254
+      })
+    end
+
+    it 'Excludes the powerschool_school_id attribute' do
+      expect(subject.call).to_not include({
+        :powerschool_school_id => 452
+      })
+    end
+  end
 end
