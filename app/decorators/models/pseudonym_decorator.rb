0466dfffd38c0f35be5d7b5c8f59b3ea4fd4c926
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-07-21 15:42:48 -0700

    Send later if production

--- app/decorators/models/pseudonym_decorator.rb
+++ app/decorators/models/pseudonym_decorator.rb
@@ -3,6 +3,10 @@ Pseudonym.class_eval do
   after_create :update_identity_mapper?
 
   def update_identity_mapper?
+    self.send_later_if_production(:update_identity_mapper)
+  end
+
+  def update_identity_mapper
     return unless identity_integration_regex.match(integration_id) && confirm_user
     IdentifierMapperService::Client.post_canvas_user_id(user_id, integration_id)
   end
