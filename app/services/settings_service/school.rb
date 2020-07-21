dfa0c8e96fed4ba9c660ad30239a28c4560705dc
John Vanbuskirk <jvanbuskirk@strongmind.com>
2020-07-21 10:01:39 -0700

    try json parsing this to prevent the redis marshalling issue

--- app/services/settings_service/school.rb
+++ app/services/settings_service/school.rb
@@ -1,34 +1,34 @@
-module SettingsService
-  class School
-    OBJECT_NAME = 'school_settings'
+  module SettingsService
+    class School
+      OBJECT_NAME = 'school_settings'
 
-    def self.create_table
-      Repository.create_table(name: table_name)
-    end
-
-    def self.get(id:)
-      school_settings = Rails.cache.read(self.table_name)
-      unless school_settings
-        school_settings = Repository.get(table_name: table_name, id: id)
-        Rails.cache.write(self.table_name, school_settings, expires_in: 5.minutes)
+      def self.create_table
+        Repository.create_table(name: table_name)
       end
-      school_settings
-    end
 
-    def self.put(id:, setting: , value:)
-      res = Repository.put(
-        table_name: table_name,
-        id: id,
-        setting: setting,
-        value: value
-      )
-      school_settings = Repository.get(table_name: table_name, id: id)
-      Rails.cache.write(self.table_name, school_settings, expires_in: 5.minutes)
-      res
-    end
+      def self.get(id:)
+        school_settings = Rails.cache.read(self.table_name)
+        unless school_settings
+          school_settings = JSON.parse(Repository.get(table_name: table_name, id: id).to_json)
+          Rails.cache.write(self.table_name, school_settings, expires_in: 5.minutes)
+        end
+        school_settings
+      end
 
-    def self.table_name
-      [SettingsService.settings_table_prefix, '-', OBJECT_NAME].join('')
+      def self.put(id:, setting: , value:)
+        res = Repository.put(
+          table_name: table_name,
+          id: id,
+          setting: setting,
+          value: value
+        )
+        school_settings = JSON.parse(Repository.get(table_name: table_name, id: id).to_json)
+        Rails.cache.write(self.table_name, school_settings, expires_in: 5.minutes)
+        res
+      end
+
+      def self.table_name
+        [SettingsService.settings_table_prefix, '-', OBJECT_NAME].join('')
+      end
     end
   end
-end
