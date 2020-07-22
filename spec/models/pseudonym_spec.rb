c8aa033f2b582b5dcb006a7c817d0d921700a616
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-07-22 13:55:21 -0700

    Spex

--- spec/models/pseudonym_spec.rb
+++ spec/models/pseudonym_spec.rb
@@ -7,4 +7,42 @@ describe Pseudonym do
       Pseudonym.create
     end
   end
+
+  describe "#update_identity_mapper" do
+    let(:pseudonym) { described_class.create }
+
+    it "gets called when integration_id changes" do
+      allow(pseudonym).to receive(:update_identity_mapper?)
+      expect(pseudonym).to receive(:update_identity_mapper?)
+      pseudonym.update(integration_id: "1234")
+    end
+
+    it "doesn't called when integration_id changes" do
+      allow(pseudonym).to receive(:update_identity_mapper?)
+      expect(pseudonym).not_to receive(:update_identity_mapper?)
+      pseudonym.update(unique_id: "1234")
+    end
+
+    context "guard clauses" do
+      before do
+        allow(IdentifierMapperService::Client).to receive(:post_canvas_user_id).and_return true
+      end
+
+      context "match uuid" do
+        before do
+          allow(pseudonym).to receive(:confirm_user).and_return true
+        end
+
+        it "returns nil when the integration id is not a uuid" do
+          pseudonym.integration_id = "abc"
+          expect(pseudonym.update_identity_mapper).to be_falsy
+        end
+
+        it "returns nil when the integration id is not a uuid" do
+          pseudonym.integration_id = SecureRandom.uuid
+          expect(pseudonym.update_identity_mapper).to be_truthy
+        end
+      end
+    end
+  end
 end
