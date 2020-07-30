be684de960fd6cb62bd7df84897142a02768e38c
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-07-29 18:30:32 -0700

    Can be on one line

--- app/services/identifier_mapper_service/client.rb
+++ app/services/identifier_mapper_service/client.rb
@@ -46,9 +46,7 @@ module IdentifierMapperService
         "com.instructure.canvas.users": { "#{school_name}": canvas_user_id }
       }
 
-      params.merge!(
-        { "com.strongmind.canvas.sis.id": sis_ids.first }
-      ) if sis_ids.any?
+      params.merge!({ "com.strongmind.canvas.sis.id": sis_ids.first }) if sis_ids.any?
 
       post(endpoints(:post_canvas_user_id), params.to_json) == 201
     end
