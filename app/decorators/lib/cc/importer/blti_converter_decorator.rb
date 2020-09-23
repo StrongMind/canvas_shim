f8c18713a43a15fa27ab86b558d0932e3141f4f9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-09-23 16:50:02 -0700

    Better this way

--- app/decorators/lib/cc/importer/blti_converter_decorator.rb
+++ app/decorators/lib/cc/importer/blti_converter_decorator.rb
@@ -1,7 +1,6 @@
 CC::Importer::BLTIConverter.class_eval do
   def convert_blti_links_no_dupes!(blti_resources, converter)
-    blti_links = convert_blti_links(blti_resources, converter)
-    blti_links.reject! do |blti_link|
+    convert_blti_links(blti_resources, converter).reject do |blti_link|
       Account.default.context_external_tools.find_by(
         blti_link.slice(:url, :domain)
       )
