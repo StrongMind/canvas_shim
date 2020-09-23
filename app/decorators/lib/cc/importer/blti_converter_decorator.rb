b0f72540aec6bf0f1cce38f40063a572e109934c
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-09-23 16:51:34 -0700

    Take bang out of method name

--- app/decorators/lib/cc/importer/blti_converter_decorator.rb
+++ app/decorators/lib/cc/importer/blti_converter_decorator.rb
@@ -1,5 +1,5 @@
 CC::Importer::BLTIConverter.class_eval do
-  def convert_blti_links_no_dupes!(blti_resources, converter)
+  def convert_blti_links_no_dupes(blti_resources, converter)
     convert_blti_links(blti_resources, converter).reject do |blti_link|
       Account.default.context_external_tools.find_by(
         blti_link.slice(:url, :domain)
