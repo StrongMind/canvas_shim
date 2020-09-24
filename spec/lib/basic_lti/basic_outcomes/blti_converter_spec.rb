aab086c10e1110a31a3b067ce639066fd263e1f9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-09-24 10:21:57 -0700

    Look for no key and secret

---
+++ spec/lib/basic_lti/basic_outcomes/blti_converter_spec.rb
@@ -0,0 +1,18 @@
+describe CC::Importer::BLTIConverter do
+  let(:blti_links) do
+    [{:description=>"",
+    :title=>"Pointful Ed",
+    :url=>"https://pointfuleducation.coursearc.com/lti",
+    :custom_fields=>{},
+    :extensions=>[],
+    :privacy_level=>"name_only",
+    :not_selectable=>nil,
+    :domain=>"pointfuleducation.coursearc.com",
+    :consumer_key=>nil,
+    :shared_secret=>nil,
+    :tool_id=>nil,
+    :assignment_points_possible=>nil,
+    :settings=>{},
+    :migration_id=>"ge938258aa328081b4c95692eb56850b4"}]
+  end
+end
