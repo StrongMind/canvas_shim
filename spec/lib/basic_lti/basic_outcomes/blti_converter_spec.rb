6d5821a2ce401c9c30cca5e58ba10f760dd4c2c9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-09-24 11:27:33 -0700

    Real Spex

--- spec/lib/basic_lti/basic_outcomes/blti_converter_spec.rb
+++ spec/lib/basic_lti/basic_outcomes/blti_converter_spec.rb
@@ -1,5 +1,6 @@
 describe CC::Importer::BLTIConverter do
-  let(:blti_links) do
+  subject { described_class.new }
+  let(:blti_links_empty) do
     [{:description=>"",
     :title=>"Pointful Ed",
     :url=>"https://pointfuleducation.coursearc.com/lti",
@@ -14,5 +15,34 @@ describe CC::Importer::BLTIConverter do
     :assignment_points_possible=>nil,
     :settings=>{},
     :migration_id=>"ge938258aa328081b4c95692eb56850b4"}]
+  end
+
+  let(:blti_links_one) do
+    blti_links_empty.append(
+      blti_links_empty.first.merge(consumer_key: "hi", shared_secret: "there")
+    )
+  end
+
+  let(:blti_resources) { nil }
+  let(:converter) { nil }
+
+  context "missing key and secret" do
+    before do
+      allow(subject).to receive(:convert_blti_links).and_return(blti_links_empty)
+    end
+
+    it "returns an empty array" do
+      expect(subject.convert_blti_links_no_dupes(blti_resources, converter)).to be_empty
+    end
+  end
+
+  context "one has a key and secret" do
+    before do
+      allow(subject).to receive(:convert_blti_links).and_return(blti_links_one)
+    end
+
+    it "returns an array with one item" do
+      expect(subject.convert_blti_links_no_dupes(blti_resources, converter).one?).to be
+    end
   end
 end
