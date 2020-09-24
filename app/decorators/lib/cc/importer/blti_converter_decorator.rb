e16aeb7a0c992bb356df66a53188dc692e0bd66a
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-09-24 12:11:20 -0700

    slightly more optimized version

--- app/decorators/lib/cc/importer/blti_converter_decorator.rb
+++ app/decorators/lib/cc/importer/blti_converter_decorator.rb
@@ -1,7 +1,7 @@
 CC::Importer::BLTIConverter.class_eval do
   def convert_blti_links_no_dupes(blti_resources, converter)
     convert_blti_links(blti_resources, converter).reject do |blti_link|
-      blti_link.slice(:shared_key, :shared_secret).values.none?
+      blti_link.values_at(:shared_key, :shared_secret).none?
     end
   end
 end
