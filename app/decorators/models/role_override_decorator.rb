f98dce333792d4d09501f061749c61b2dab76821
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-02 15:44:59 -0700

    Whoops

--- app/decorators/models/role_override_decorator.rb
+++ app/decorators/models/role_override_decorator.rb
@@ -1,38 +1,38 @@
-# RoleOverride.class_eval do
-#   Permissions.register({
-#     :custom_placement => {
-#       :label => lambda { "Apply Custom Placement to Courses" },
-#       :available_to => [
-#         'TaEnrollment',
-#         'TeacherEnrollment',
-#         'AccountAdmin',
-#       ],
-#       :true_for => [
-#         'AccountAdmin',
-#       ]
-#     },
-#     :delete_inbox_messages => {
-#       :label => lambda { "Delete inbox messages" },
-#       :available_to => [
-#         'StudentEnrollment',
-#         'TaEnrollment',
-#         'TeacherEnrollment',
-#         'DesignerEnrollment',
-#         'TeacherlessStudentEnrollment',
-#         'ObserverEnrollment',
-#         'AccountAdmin',
-#         'AccountMembership'
-#       ],
-#       :true_for => [
-#         'StudentEnrollment',
-#         'TaEnrollment',
-#         'TeacherEnrollment',
-#         'DesignerEnrollment',
-#         'TeacherlessStudentEnrollment',
-#         'ObserverEnrollment',
-#         'AccountAdmin',
-#         'AccountMembership'
-#       ]
-#     }
-#   })
-# end
+RoleOverride.class_eval do
+  Permissions.register({
+    :custom_placement => {
+      :label => lambda { "Apply Custom Placement to Courses" },
+      :available_to => [
+        'TaEnrollment',
+        'TeacherEnrollment',
+        'AccountAdmin',
+      ],
+      :true_for => [
+        'AccountAdmin',
+      ]
+    },
+    :delete_inbox_messages => {
+      :label => lambda { "Delete inbox messages" },
+      :available_to => [
+        'StudentEnrollment',
+        'TaEnrollment',
+        'TeacherEnrollment',
+        'DesignerEnrollment',
+        'TeacherlessStudentEnrollment',
+        'ObserverEnrollment',
+        'AccountAdmin',
+        'AccountMembership'
+      ],
+      :true_for => [
+        'StudentEnrollment',
+        'TaEnrollment',
+        'TeacherEnrollment',
+        'DesignerEnrollment',
+        'TeacherlessStudentEnrollment',
+        'ObserverEnrollment',
+        'AccountAdmin',
+        'AccountMembership'
+      ]
+    }
+  })
+end
