3426490625f0cb817ef9a1924f692777c5ac0cb3
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-12 11:32:58 -0700

    Fix placeholder table and compact helper

--- app/decorators/helpers/courses_helper_decorator.rb
+++ app/decorators/helpers/courses_helper_decorator.rb
@@ -1,6 +1,6 @@
 CoursesHelper.class_eval do
   def present_sis_ids(stu_enr)
-    joined_ids = stu_enr[3..-1].join(',')
+    joined_ids = stu_enr[3..-1].compact.join(',')
     joined_ids.empty? ? "N/A" : joined_ids
   end
 end
