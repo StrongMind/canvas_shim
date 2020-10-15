bb6567cba5301f9315270ed902ddbba62a51dbd2
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-15 16:10:53 -0700

    Pseudocode

--- app/decorators/lib/cc/importer/blti_converter_decorator.rb
+++ app/decorators/lib/cc/importer/blti_converter_decorator.rb
@@ -1,7 +1,16 @@
 CC::Importer::BLTIConverter.class_eval do
   def convert_blti_links_no_dupes(blti_resources, converter)
-    convert_blti_links(blti_resources, converter).reject do |blti_link|
-      blti_link.values_at(:shared_key, :shared_secret).none?
-    end
+    convert_blti_links(blti_resources, converter).map do |blti_link|
+      if blti_link.values_at(:shared_key, :shared_secret).none?
+        account_tool = Account.default.context_external_tools.find_by(domain: blti_link.domain)
+        if account_tool
+          blti_link.merge!(shared_key: account_tool.shared_key, shared_secret: account_tool.shared_secret)
+        else
+          nil
+        end
+      else
+        blti_tool
+      end
+    end.compact
   end
 end
