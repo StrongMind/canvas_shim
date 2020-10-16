f5f58c73d5c53df28472edd6f424286f3e6331f2
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-16 10:26:31 -0700

    Change to callback

--- app/decorators/models/context_external_tool_decorator.rb
+++ app/decorators/models/context_external_tool_decorator.rb
@@ -1,4 +1,18 @@
 ContextExternalTool.class_eval do
+  before_create :copy_account_config?
+
+  def copy_account_config?
+    if context_type == "Course" && [consumer_key, shared_secret].all? &"fake".method(:==)
+      account_tool = Account.default.context_external_tools.find_by(domain: domain)
+      if account_tool
+        assign_attributes(
+          consumer_key: account_tool.consumer_key,
+          shared_secret: account_tool.shared_secret
+        )
+      end
+    end
+  end
+
   def is_oauth_lti_domain?
     return false unless self.domain
     (SettingsService.global_settings["oauth_lti_domains"] || '').split(",").any? do |setting_domain|
