317cf1713e7c4a1168e6a8d952543c1b63f68d0a
AndyRosenbergStrongMind <46331093+AndyRosenbergStrongMind@users.noreply.github.com>
2020-10-16 14:59:02 -0700

    Lti match admin tool (#321)
    * Pseudocode
    
    * Change to callback
    
    * Remove blti decorator
    
    * Need parens
    
    * spex

--- app/decorators/lib/cc/importer/blti_converter_decorator.rb
+++
@@ -1,7 +0,0 @@
-CC::Importer::BLTIConverter.class_eval do
-  def convert_blti_links_no_dupes(blti_resources, converter)
-    convert_blti_links(blti_resources, converter).reject do |blti_link|
-      blti_link.values_at(:shared_key, :shared_secret).none?
-    end
-  end
-end
--- spec/test_app/app/models/context_external_tool.rb
+++ spec/test_app/app/models/context_external_tool.rb
@@ -1,3 +1,4 @@
 class ContextExternalTool < ActiveRecord::Base
   belongs_to :course
+  belongs_to :context, polymorphic: true
 end
