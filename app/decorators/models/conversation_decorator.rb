ef5e7527e1f156dea5791bc269015a068a36a620
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-28 09:55:15 -0700

    Refactoring

---
+++ app/decorators/models/conversation_decorator.rb
@@ -0,0 +1,3 @@
+Conversation.class_eval do
+  after_commit { PipelineService.publish_as_v2(self) }
+end
--- spec/test_app/app/models/conversation.rb
+++ spec/test_app/app/models/conversation.rb
@@ -1,6 +1,4 @@
 class Conversation < ActiveRecord::Base
-  after_commit { PipelineService.publish(self) }
-
   has_many :conversation_participants, :dependent => :destroy
   has_many :conversation_messages, -> { order("created_at DESC, id DESC") }, dependent: :delete_all
   has_many :conversation_message_participants, :through => :conversation_messages
