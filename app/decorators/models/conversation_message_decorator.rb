b2fd0a5df286aa8405894a80e0223daa204cdd9d
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-10-28 10:02:34 -0700

    Conversation message

---
+++ app/decorators/models/conversation_message_decorator.rb
@@ -0,0 +1,3 @@
+ConversationMessage.class_eval do
+  after_commit { PipelineService.publish_as_v2(self) }
+end
--- spec/services/conversation_backfiller_spec.rb
+++ spec/services/conversation_backfiller_spec.rb
@@ -12,7 +12,7 @@ describe ConversationBackfiller do
       conversation.conversation_participants.create
 
       expect(PipelineService).to receive(:publish_as_v2).with(an_instance_of(Conversation))
-      expect(PipelineService).to receive(:publish).with(an_instance_of(ConversationMessage)).once
+      expect(PipelineService).to receive(:publish_as_v2).with(an_instance_of(ConversationMessage)).once
       expect(PipelineService).to receive(:publish).with(an_instance_of(ConversationParticipant)).twice
 
       ConversationBackfiller.call
