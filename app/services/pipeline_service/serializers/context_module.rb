c3affc1df960ddf7b0918ea4fcce520464bafbd9
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-11-06 15:28:11 -0700

    Less breaking version

--- app/services/pipeline_service/serializers/context_module.rb
+++ app/services/pipeline_service/serializers/context_module.rb
@@ -5,6 +5,8 @@ module PipelineService
       include Api::V1::ContextModule
       include Rails.application.routes.url_helpers
 
+      Rails.application.routes.default_url_options[:host] = ENV['CANVAS_DOMAIN']
+
       def initialize(object:)
         @object = object
         @context_module = ::ContextModule.find(@object.id)
@@ -12,7 +14,7 @@ module PipelineService
       end
 
       def call
-        module_json(object)
+        module_json(object, GradesService::Account.account_admin, nil)
       end
 
       def self.additional_identifier_fields
@@ -23,42 +25,8 @@ module PipelineService
 
       attr_reader :object
 
-      def domain
-        ENV['CANVAS_DOMAIN']
-      end
-
-      def endpoint
-        [
-          protocol, domain, ':', port,
-          "/api/v1/courses/#{@course_id}/modules/#{@object.id}"
-        ].join('')
-      end
-
-      def use_ssl?
-        ENV['CANVAS_SSL'] == 'true'
-      end
-
-      def port
-        return '3000' if Rails.env == 'development'
-        return '443' if use_ssl?
-        '80'
-      end
-
-      def headers
-        { Authorization: "Bearer #{ENV['STRONGMIND_INTEGRATION_KEY']}" }
-      end
-
-      def fetch
-        PipelineService::HTTPClient.get(endpoint, headers: headers).parsed_response
-      end
-
       def course_id
         ::DiscussionTopic.find(object.id).context_id
-      end
-
-      def protocol
-        return 'https://' if use_ssl?
-        'http://'
       end
     end
   end
