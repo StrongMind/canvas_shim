ae080a29e2a0d14a225f5f3c0c2fe28e6c6de401
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-11-09 11:34:44 -0700

    Fix test suite

--- spec/services/pipeline_service/serializers/context_module_spec.rb
+++ spec/services/pipeline_service/serializers/context_module_spec.rb
@@ -4,18 +4,10 @@ describe PipelineService::Serializers::ContextModule do
 
   subject { described_class.new(object: noun) }
 
-  before do
-    allow(PipelineService::HTTPClient).to receive(:get).and_return(
-      double('response', parsed_response: { "id" => active_record_object.id} )
-    )
-  end
-
   let(:active_record_object) { ContextModule.create!(context: course) }
   let(:noun) { PipelineService::Models::Noun.new(active_record_object)}
 
-  it 'Calls the api' do
-    expect(PipelineService::HTTPClient).to receive(:get)
-      .with("http://#{ENV['CANVAS_DOMAIN']}:80/api/v1/courses/#{course.id}/modules/#{noun.id}", any_args)
+  it 'Returns json from api modules' do
     expect(subject.call).to include( { 'id' => active_record_object.id } )
   end
 end
