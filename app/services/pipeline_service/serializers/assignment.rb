bdfa8325221519757fbcd37534cd7e128c8efc55
John VanBuskirk <john.vanbuskirk@strongmind.com>
2020-11-10 15:01:39 -0700

    do it this way

--- app/services/pipeline_service/serializers/assignment.rb
+++ app/services/pipeline_service/serializers/assignment.rb
@@ -1,12 +1,6 @@
 module PipelineService
   module Serializers
     class Assignment
-      include Api
-      include Api::V1::Assignment
-      include Rails.application.routes.url_helpers
-
-      Rails.application.routes.default_url_options[:host] = ENV['CANVAS_DOMAIN']
-
       def initialize(object:)
         @object = ::Assignment.find(object.id)
         @course_id = @object.context_id
@@ -17,7 +11,7 @@ module PipelineService
       end
 
       def call
-        assignment_json(@object, GradesService::Account.account_admin, nil)
+        @object.as_json(:permissions => {:user => GradesService::Account.account_admin, :session => nil})
       end
     end
   end
