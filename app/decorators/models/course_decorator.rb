fc3896eb7f62eb7a7e48038ba288a52e5f3e3f69
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-01-25 11:51:28 -0700

    whitespace

--- app/decorators/models/course_decorator.rb
+++ app/decorators/models/course_decorator.rb
@@ -151,7 +151,6 @@ Course.class_eval do
   def scale_count(count)
     return 0 if no_active_students?
     return count if SettingsService.get_settings(id: 1, object: 'school')['unweighted_course_access_report']
-
     enrs = active_students.size
     return 10 if count >= enrs * 10
     count.divmod(enrs).first
