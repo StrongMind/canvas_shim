10799de59eba5ea81b84527900fc5248eac1f0b5
johnvbk <31219912+johnvbk@users.noreply.github.com>
2021-01-28 16:27:16 -0700

    Publish completion requirements (#345)
    * Get the items
    
    * Fix test app
    
    Co-authored-by: Andy Rosenberg <andy.rosenberg@strongmind.com>

--- spec/test_app/lib/api/v1/context_module.rb
+++ spec/test_app/lib/api/v1/context_module.rb
@@ -1,5 +1,5 @@
 module Api::V1::ContextModule
-  def module_json(context_module, user, session)
+  def module_json(context_module, user, session, progression = nil, includes = [])
     {"id" => context_module.id}
   end
 end
