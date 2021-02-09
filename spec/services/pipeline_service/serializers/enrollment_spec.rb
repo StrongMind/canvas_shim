f2a480666a29751616ca9c1fa7bf504fdebd95d0
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-02-09 09:58:27 -0700

    will this fix the test

--- spec/services/pipeline_service/serializers/enrollment_spec.rb
+++ spec/services/pipeline_service/serializers/enrollment_spec.rb
@@ -1,7 +1,7 @@
 describe PipelineService::Serializers::Enrollment do
   let(:noun) {double('noun', id: 1)}
 
-  let(:enrollment) { double('enrollment', is_a?: true) }
+  let(:enrollment) { double('enrollment', is_a?: true, scores: []) }
 
   let(:subject) {
     described_class.new(object: noun)
