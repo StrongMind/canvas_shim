93dcbe14e74ba7426347db10b2e9cd58269c3396
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-02-11 15:48:12 -0700

    make sure we only do this to student enrollment

--- app/decorators/models/score_decorator.rb
+++ app/decorators/models/score_decorator.rb
@@ -1,3 +1,11 @@
 Score.class_eval do
   after_commit -> { self.enrollment.publish_as_v2 }
+
+
+  def publish_enrollment_as_v2
+    if self.enrollment.type == "StudentEnrollment"
+      self.enrollment.publish_as_v2
+    end
+  end
+
 end
