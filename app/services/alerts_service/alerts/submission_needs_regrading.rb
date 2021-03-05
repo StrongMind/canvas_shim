eae83bbde9f2b4b4855eb15b85582b18dd73e962
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-03-05 10:56:37 -0700

    shell of alerts/submission_needs_regrading.rb

---
+++ app/services/alerts_service/alerts/submission_needs_regrading.rb
@@ -0,0 +1,28 @@
+module AlertsService
+  module Alerts
+    class SubmissionNeedsRegrading < Alert
+      ALERT_ATTRIBUTES = %i{ teacher_id student_id assignment_id score course_id}
+
+      def initialize(atts)
+        super
+      end
+
+      # def assignment
+      #   Assignment.find(assignment_id)
+      # end
+
+      # def student
+      #   User.find(student_id)
+      # end
+
+      # def detail
+      #   return unless @score.present?
+      #   "Last Score: %g" % ("%.2f" % score)
+      # end
+
+      # def description
+      #   'Submission Needs Regrading'
+      # end
+    end
+  end
+end
