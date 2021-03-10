54912cdb79ee119b2e6d7364caf44fc9fe41a602
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-03-10 15:48:17 -0700

    attempt to scope to certain assignment types

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -61,24 +61,42 @@ describe Submission do
 
 context "Submission Needs Regrading" do
     let!(:student) { User.create() }
-    let!(:submission) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false) }
+    let!(:submission_1) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'basic_lti_launch') }
+    let!(:submission_2) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'discussion_topic') }
+    let!(:submission_3) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'online_upload') }
+    let!(:submission_4) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'external_tool') }
+
     let!(:course) { Course.create() }
     let!(:teacher_enrollment) { TeacherEnrollment.create(course: course, user: teacher) }
     let!(:assignment) { Assignment.new(course: course) }
     let!(:teacher) { User.create() }
 
-    it "sends an alert when a submission needs regrading" do
+    it "sends an alert when a zero graded basic_lti_launch submission is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
       expect(AlertsService::Client).to receive(:create)
-      submission.update(submitted_at: Time.now)
+      submission_1.update(submitted_at: Time.now)
     end
 
-    it "does not send an alert when a submission needs regrading if grader changes" do
+    it "sends an alert when a zero graded discussion_topic submission is submitted" do
+      allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
+      allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      expect(AlertsService::Client).to receive(:create)
+      submission_2.update(submitted_at: Time.now)
+    end
+
+    it "sends an alert when a zero graded online_upload submission is submitted" do
+      allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
+      allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      expect(AlertsService::Client).to receive(:create)
+      submission_3.update(submitted_at: Time.now)
+    end
+
+    it "does not send an alert when a zero graded 'external_tool is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
       expect(AlertsService::Client).to_not receive(:create)
-      submission.update(submitted_at: Time.now, grader_id: -5)
+      submission_4.update(submitted_at: Time.now)
     end
 
   end 
