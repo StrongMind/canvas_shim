ee9f6cfb8aad6c97a930277569df5a7ccf7bc9a1
John VanBuskirk <john.vanbuskirk@strongmind.com>
2021-03-10 16:03:28 -0700

    mock submission_type

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -61,10 +61,7 @@ describe Submission do
 
 context "Submission Needs Regrading" do
     let!(:student) { User.create() }
-    let!(:submission_1) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'basic_lti_launch') }
-    let!(:submission_2) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'discussion_topic') }
-    let!(:submission_3) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'online_upload') }
-    let!(:submission_4) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false, submission_type: 'external_tool') }
+    let!(:submission) { Submission.create(score: 0, grader_id: 1, submitted_at: 1.hour.ago, assignment: assignment, user: student, excused: false }
 
     let!(:course) { Course.create() }
     let!(:teacher_enrollment) { TeacherEnrollment.create(course: course, user: teacher) }
@@ -74,29 +71,33 @@ context "Submission Needs Regrading" do
     it "sends an alert when a zero graded basic_lti_launch submission is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      allow(submission).to receive(:submission_type).and_return('basic_lti_launch')
       expect(AlertsService::Client).to receive(:create)
-      submission_1.update(submitted_at: Time.now)
+      submission.update(submitted_at: Time.now)
     end
 
     it "sends an alert when a zero graded discussion_topic submission is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      allow(submission).to receive(:submission_type).and_return('discussion_topic')
       expect(AlertsService::Client).to receive(:create)
-      submission_2.update(submitted_at: Time.now)
+      submission.update(submitted_at: Time.now)
     end
 
     it "sends an alert when a zero graded online_upload submission is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      allow(submission).to receive(:submission_type).and_return('online_upload')
       expect(AlertsService::Client).to receive(:create)
-      submission_3.update(submitted_at: Time.now)
+      submission.update(submitted_at: Time.now)
     end
 
-    it "does not send an alert when a zero graded 'external_tool is submitted" do
+    it "does not send an alert when a zero graded external_tool is submitted" do
       allow(AlertsService::SecretManager).to receive(:get_secret).and_return({'API_ENDPOINT' => '12345'})
       allow(HTTParty).to receive(:post).and_return(AlertsService::Response.new(200, nil))
+      allow(submission).to receive(:submission_type).and_return('external_tool')
       expect(AlertsService::Client).to_not receive(:create)
-      submission_4.update(submitted_at: Time.now)
+      submission.update(submitted_at: Time.now)
     end
 
   end 
