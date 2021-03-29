4dc67f68adeed20385e8ded85cddedb3f21c7bb2
Andy Rosenberg <andy.rosenberg@strongmind.com>
2021-03-29 15:59:47 -0700

    Programs

--- spec/services/special_programs_service/commands/get_programs_spec.rb
+++ spec/services/special_programs_service/commands/get_programs_spec.rb
@@ -1,3 +1,21 @@
+def create_program(start_date, end_date)
+  {
+    "id" => "55555555-0000-0000-0000-000000000000",
+    "programReference" => {
+      "programName" => "IEP"
+    },
+    "educationOrganizationReference" => {
+      "educationOrganizationId" => "00000000-0000-0000-0000-000000000000"
+    },
+    "studentReference" => {
+      "studentUniqueId" => "00000000-0000-0000-0000-000000000000"
+    },
+    "beginDate" => start_date,
+    "endDate" => end_date
+  }
+end
+
+
 describe SpecialProgramsService::Commands::GetPrograms do
   include_context "stubbed_network"
   let(:user) { User.create(name: "rks") }
@@ -9,22 +27,39 @@ describe SpecialProgramsService::Commands::GetPrograms do
 
   describe "#program_applicable" do
     context "program hasn't started" do
-      let(:program) do
-        {
-          "id" => "55555555-0000-0000-0000-000000000000",
-          "programReference" => {
-            "programName" => "IEP"
-          },
-          "educationOrganizationReference" => {
-            "educationOrganizationId" => "00000000-0000-0000-0000-000000000000"
-          },
-          "studentReference" => {
-            "studentUniqueId" => "00000000-0000-0000-0000-000000000000"
-          },
-          "beginDate" => "#{Date.tomorrow}",
-          "endDate" => "#{2.months.from_now}"
-        }
+      let(:program) { create_program("#{Date.tomorrow}", "#{2.months.from_now}") }
+
+      it "is not applicable" do
+        expect(subject.send(:program_applicable?, program)).to be false
       end
+    end
+
+    context "program has ended" do
+      let(:program) { create_program("#{1.month.ago}", "#{Date.yesterday}") }
+  
+      it "is not applicable" do
+        expect(subject.send(:program_applicable?, program)).to be false
+      end
+    end
+  end
+
+  context "program has no start or end date" do
+    let(:program) { create_program(nil, nil) }
+
+    it "is applicable" do
+      expect(subject.send(:program_applicable?, program)).to be true
+    end
+  end
+
+  context "program has no start date and has end date" do
+    let(:program) { create_program(nil, "#{Date.tomorrow}") }
+
+    it "is applicable" do
+      expect(subject.send(:program_applicable?, program)).to be true
+    end
+
+    context "end date before now" do
+      let(:program) { create_program(nil, "#{Date.yesterday}") }
 
       it "is not applicable" do
         expect(subject.send(:program_applicable?, program)).to be false
