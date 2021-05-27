81b0f5ad4e9968b9b31b64884c73828876403c0c
Josh Brito <jbrito6492@gmail.com>
2021-05-27 16:32:40 -0700

    added updated_at to course progress

--- app/services/pipeline_service/builders/course_progress_json_builder.rb
+++ app/services/pipeline_service/builders/course_progress_json_builder.rb
@@ -4,7 +4,10 @@ module PipelineService
         def self.call(noun)
           user = User.find noun.user_id
           course = Course.find noun.course_id
-          CourseProgress.new(course, user).to_json
+          course_progress = CourseProgress.new(course, user).to_json
+          course_updated_at = ContextModuleProgression.find(noun.id).updated_at
+          course_progress['updated_at'] = course_updated_at
+          course_progress
         end
       end
     end
