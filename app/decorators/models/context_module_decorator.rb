5596919e10ab660e9fd2493b050b359ac8afa9fe
Andy Rosenberg <andy.rosenberg@strongmind.com>
2021-06-08 13:04:21 -0700

    Try was empty

--- app/decorators/models/context_module_decorator.rb
+++ app/decorators/models/context_module_decorator.rb
@@ -3,7 +3,7 @@ ContextModule.class_eval do
   after_commit -> { PipelineService.publish_as_v2(self, alias: 'module') }
 
   def assign_threshold
-    if completion_requirements_was.empty?
+    if completion_requirements_was&.empty?
       RequirementsService.apply_minimum_scores(context_module: self)
     end
   end
