df4c4ad644a8f3ed411cb09974b82fd94300a496
Andy Rosenberg <andy.rosenberg@strongmind.com>
2021-06-08 13:02:14 -0700

    Try only when completion_requirements are created

--- app/decorators/models/context_module_decorator.rb
+++ app/decorators/models/context_module_decorator.rb
@@ -1,7 +1,10 @@
 ContextModule.class_eval do
+  after_save :assign_threshold
   after_commit -> { PipelineService.publish_as_v2(self, alias: 'module') }
 
   def assign_threshold
-    RequirementsService.apply_minimum_scores(context_module: self)
+    if completion_requirements_was.empty?
+      RequirementsService.apply_minimum_scores(context_module: self)
+    end
   end
 end
