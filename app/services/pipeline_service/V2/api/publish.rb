b91262c7fe2aaeff5eebc5fc1f854d109569ff63
Angel Diaz <angel.diaz@strongmind.com>
2021-12-13 13:53:03 -0700

    STEAK-724: modified Publish::v2 to handle call asynchronously

--- app/services/pipeline_service/V2/api/publish.rb
+++ app/services/pipeline_service/V2/api/publish.rb
@@ -4,16 +4,17 @@ module PipelineService
       class Publish
         def initialize(model)
           @model = model
-          @noun = Noun.new(model)
-          @payload = Payload.new(
-            object: @noun
-          )
         end
 
         def call
+          @noun = Noun.new(@model)
+          @payload = Payload.new(
+            object: @noun
+          )
           payload = @payload.call
           PipelineService::V2::Commands::PublishToPipeline.new(payload).call
         end
+        handle_asynchronously :call, :priority => 1000000
       end
     end
   end
