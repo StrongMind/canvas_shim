a897c802751e0217d3cc3e27ce98129a11f65606
Angel Diaz <angel.diaz@strongmind.com>
2022-06-06 15:13:55 -0700

    added debug

--- app/decorators/models/content_tag_decorator.rb
+++ app/decorators/models/content_tag_decorator.rb
@@ -2,6 +2,13 @@ ContentTag.class_eval do
   after_commit -> { PipelineService::V2.publish self }
   after_commit -> do
     return unless course
+    puts "######################################################################"
+    puts "######################################################################"
+    puts "self: #{self.inspect}"
+    puts "######################################################################"
+    puts caller
+    puts "######################################################################"
+    puts "######################################################################"
     PipelineService.publish PipelineService::Nouns::ModuleItem.new(self)
   end
 end
