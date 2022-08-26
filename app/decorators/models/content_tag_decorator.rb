13edf5c9a08aeaa0e15a54c57631dc573b835730
cpython1234 <charles.parisi@strongmind.com>
2022-08-26 10:38:03 -0700

    adding return before publish if noun content_tag is learning_outcome. this type of object does not need to be published to the pipeline. Co-Authored-by Angel Diaz <angel.diaz@strongmind.com>

--- app/decorators/models/content_tag_decorator.rb
+++ app/decorators/models/content_tag_decorator.rb
@@ -1,11 +1,11 @@
 ContentTag.class_eval do
   after_commit -> do
-    return if self.tag_type is == "learning_outcome"
+    return if self.tag_type == "learning_outcome"
     PipelineService::V2.publish self
   end
   after_commit -> do
     return unless course
-    return if self.tag_type is == "learning_outcome"
+    return if self.tag_type == "learning_outcome"
     PipelineService.publish PipelineService::Nouns::ModuleItem.new(self)
   end
 end
