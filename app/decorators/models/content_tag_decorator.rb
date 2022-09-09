ca97866cf6445e2b350846675fc31c34b278eb01
Alejandra Torres <agtorre3@yahoo.com>
2022-09-09 13:27:33 -0700

    Revert "QTY-355 don't push learning outcomes to pipeline"
    This reverts commit ad1d6a006e07d7f020508ab2a415ce2144ac71eb.

--- app/decorators/models/content_tag_decorator.rb
+++ app/decorators/models/content_tag_decorator.rb
@@ -1,11 +1,11 @@
 ContentTag.class_eval do
   after_commit -> do
-    return if self.content_type == "LearningOutcome"
+    return if self.tag_type == "learning_outcome"
     PipelineService::V2.publish self
   end
   after_commit -> do
     return unless course
-    return if self.content_type == "LearningOutcome"
+    return if self.tag_type == "learning_outcome"
     PipelineService.publish PipelineService::Nouns::ModuleItem.new(self)
   end
 end
