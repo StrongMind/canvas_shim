c3a224858eebbfac7adab11f51a569f18709fd41
Angel Diaz <angel.diaz@strongmind.com>
2022-09-24 14:15:12 -0700

    configured factory_bot_rails correctly

--- spec/factories/context_module_progression_factory.rb
+++
@@ -1,7 +0,0 @@
-module Factories
-  def context_module_progression_factory(opts = {})
-    {
-      collapsed: true
-    }
-  end
-end
---
+++ spec/support/factories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :context_module_progression do
+    collapsed true
+  end
+end
