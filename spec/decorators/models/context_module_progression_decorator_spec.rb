259e03c80ac571dee59e29485faed316bd472de2
Angel Diaz <angel.diaz@strongmind.com>
2022-09-26 16:02:44 -0700

    temp

--- spec/decorators/models/context_module_progression_decorator_spec.rb
+++ spec/decorators/models/context_module_progression_decorator_spec.rb
@@ -1,9 +1,30 @@
 describe "ContextModuleProgression" do
 
   context "#uncollapse!" do
-    it 'does not raise an exception' do
-      context_module_progression = FactoryBot.build(:context_module_progression)
-      expect(context_module_progression).to_not raise_exception
+    it 'does not raise an exception when one record is being updated by one variable' do
+      # arrange
+      cmp = FactoryBot.build(:context_module_progression)
+      allow(cmp).to receive(:publish_course_progress).and_return(nil)
+      cmp.save
+      # act / #assert
+      expect { cmp.uncollapse! }.to_not raise_error(ActiveRecord::StaleObjectError)
+    end
+
+    it 'does not raise an exception when one record is being updated by two different variables' do
+      cmp0 = FactoryBot.build(:context_module_progression)
+      allow(cmp0).to receive(:publish_course_progress).and_return(nil)
+      cmp0.collapsed = false
+      cmp0.save!
+
+      cmp1 = ContextModuleProgression.find(cmp0.id)
+      allow(cmp1).to receive(:publish_course_progress).and_return(nil)
+      cmp1.collapse = false
+      cmp1.save!
+
+      cmp2 = ContextModuleProgression.find(cmp0.id)
+      allow(cmp2).to receive(:publish_course_progress).and_return(nil)
+
+      expect{ cmp2.save}.to_not raise_exception(ActiveRecord::StaleObjectError)
     end
   end
 end
