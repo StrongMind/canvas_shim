945ae8cbd0b260a764753300cfbd2729da73d5eb
C.J. Adeszko <christopher.adeszko@strongmind.com>
2023-01-09 10:34:40 -0700

    QTY-1751: Fix import and completion req update
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>
    Co-authored-by: Alexandra Davis <alexandra.davis@strongmind.com>

--- app/decorators/models/context_module_decorator.rb
+++ app/decorators/models/context_module_decorator.rb
@@ -1,14 +1,3 @@
 ContextModule.class_eval do
-  after_save :assign_threshold
   after_commit -> { PipelineService.publish_as_v2(self, alias: 'module') }
-
-  def assign_threshold
-    if completion_requirements_was&.empty?
-      RequirementsService.apply_minimum_scores(context_module: self, assignment_group_names: AssignmentGroup.passing_threshold_group_names)
-    end
-  end
-
-  def get_assignment_threshold_overrides
-    SettingsService.get_settings(object: 'course', id: self.course.try(:id))['threshold_overrides']
-  end
 end
