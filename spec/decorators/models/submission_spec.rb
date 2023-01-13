e48c0d73a9107a32b11e58ae61f2e7ea4d17782b
C.J. Adeszko <christopher.adeszko@strongmind.com>
2023-01-13 09:46:05 -0700

    QTY-1779: Update happy path spec
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>
    Co-authored-by: James Wingert <james.wingert@strongmind.com>

--- spec/decorators/models/submission_spec.rb
+++ spec/decorators/models/submission_spec.rb
@@ -4,14 +4,8 @@ describe Submission do
   
   let(:subject_instance) { double('subject_instance')}
   
-  # before do
-  #   allow(subject).to receive(:new).and_return(subject_instance)
-  #   allow(subject_instance).to receive(:save)
-  #   allow(subject_instance).to receive(:send_guided_practice_submitted_alert)
-  # end
-  
   describe 'callbacks' do
-    describe 'send_guided_practice_submitted_alert' do
+    describe '#send_guided_practice_submitted_alert' do
       let(:course) { create(:course) }
       let(:teacher) { create(:user) }
       let(:student) { create(:user) }
@@ -19,20 +13,26 @@ describe Submission do
       let(:teacher_enrollment) { create(:enrollment, user: teacher, course: course, type: 'TeacherEnrollment') }
       let(:student_enrollment) { create(:enrollment, user: student, course: course, type: 'StudentEnrollment') }
       
-      it "should alert the teacher when a student submits a guided practice" do
-        assignment = content_tag.content
-        assignment.assignment_group.update(name: 'Guided Practice')
-        alert_instance = instance_double(AlertsService::Client)
-        allow(AlertsService::Client).to receive(:create).and_return(alert_instance)
-        submission = Submission.new(user: student, assignment: assignment)
-        binding.pry
-        expect(submission).to receive(:send_guided_practice_submitted_alert)
-        submission.save
-        # expect(alert_instance).to have_received(:create).with(:guided_practice_submitted, teacher.id, student.id, assignment.id, course.id)
-        # expect(subject_instance).to receive(:send_guided_practice_submitted_alert)
-        # subject.create(user: student, assignment: assignment, course: course, workflow_state: 'submitted')
-      end
+      context 'when the assignment is a Guided Practice' do 
+        it "should call #send_guided_practice_alert" do
+          assignment = content_tag.content
+          assignment.update(course_id: course.id)
+          assignment.assignment_group.update(name: 'Guided Practice')
+          submission = Submission.new(user: student, assignment: assignment)
+          expect(submission).to receive(:send_guided_practice_submitted_alert)
+          submission.update(submitted_at: Time.now)
+        end
 
+        it "should create a new AlertService::Client instance" do
+          assignment = content_tag.content
+          assignment.update(course_id: course.id)
+          assignment.assignment_group.update(name: 'Guided Practice')
+          course.enrollments.concat([teacher_enrollment, student_enrollment])
+          submission = Submission.new(user: student, assignment: assignment)
+          expect(AlertsService::Client).to receive(:create).with(:guided_practice_submitted, teacher_id: teacher.id, student_id: student.id, assignment_id: assignment.id, course_id: course.id)
+          submission.update(submitted_at: Time.now)
+        end
+      end
     end
   end
 end
