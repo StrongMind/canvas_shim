7866a5bdf083912e4792127aa4149a764586936e
C.J. Adeszko <christopher.adeszko@strongmind.com>
2023-01-23 08:26:23 -0700

    QTY-1504: Add logic to check if graded submission, restric view
    Co-authored-by: Alexandra Davis <alexandra.davis@strongmind.com>

--- app/decorators/models/discussion_topic_decorator.rb
+++ app/decorators/models/discussion_topic_decorator.rb
@@ -4,4 +4,8 @@ DiscussionTopic.class_eval do
   def is_excused?(user)
     self&.assignment&.is_excused?(user)
   end
+
+  def graded_discussion?
+    self.root_topic.nil? && self.type.nil? && self.threaded? && self.assignment.present?
+  end
 end
---
+++ app/decorators/models/materialized_view_decorator.rb
@@ -0,0 +1,28 @@
+DiscussionTopic::MaterializedView.class_eval do
+  def strongmind_materialized_view_json(opts = {})
+    graded_discussion = self.discussion_topic.graded_discussion?
+    opts[:show_other_users_submissions] = false
+    entry_ids = nil
+    if graded_discussion
+      student_submission_graded = self.discussion_topic.assignment.submissions.graded.where(user_id: opts[:user_id]).any?
+      if student_submission_graded
+        opts[:show_other_users_submissions] = true
+        entry_ids = self.entry_ids_array
+      else 
+        entries = self.discussion_topic.discussion_entries.where(user_id: opts[:user_id])
+        entry_ids = entries.pluck(:id)
+        ungraded_json_structure = discussion_entry_api_json(entries, discussion_topic.context, nil, nil, [])
+      end
+    else
+      entry_ids = self.entry_ids_array
+    end
+    # binding.pry
+
+    opts[:entry_ids] = entry_ids
+    opts[:json_structure] = ungraded_json_structure.present? ? ungraded_json_structure.to_json : self.json_structure
+    instructure_materialized_view_json(opts)
+  end
+
+  alias_method :instructure_materialized_view_json, :materialized_view_json
+  alias_method :materialized_view_json, :strongmind_materialized_view_json
+end
