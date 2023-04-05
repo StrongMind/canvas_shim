922dbbffeee1463029531f960fd9d25f03810f07
C.J. Adeszko <christopher.adeszko@strongmind.com>
2023-04-05 10:39:42 -0700

    QTY-2633: Move logging logic out of delayed job decorartor
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>

--- app/decorators/models/delayed/job.rb
+++ app/decorators/models/delayed/job.rb
@@ -6,11 +6,4 @@ Delayed::Job.class_eval do
     puts self.source if self.source.length > limit
     self.source = self.source[0..limit-1] if self.source.length > limit
   end
-
-  def self.alert_long_running_jobs
-    jobs = Delayed::Job.where('locked_at <= ?', 12.hours.ago)
-    if jobs.any?
-      Rails.logger.warn "[JOB QUEUE] #{jobs.count} delayed jobs running for more than 12 hours"
-    end
-  end
 end
