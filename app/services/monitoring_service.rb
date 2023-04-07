ddb500f97230210621933f10791f6b248e6f28ee
C.J. Adeszko <christopher.adeszko@strongmind.com>
2023-04-07 14:25:46 -0700

    QTY-2663: Change logging to 'Original Job IDs'
    Co-authored-by: Angel Diaz <angel.diaz@strongmind.com>

--- app/services/monitoring_service.rb
+++ app/services/monitoring_service.rb
@@ -2,7 +2,7 @@ module MonitoringService
   def self.alert_long_running_jobs
     jobs = Delayed::Job.where('locked_at <= ?', 12.hours.ago)
     if jobs.any?
-      Rails.logger.warn "[JOB QUEUE] #{jobs.count} delayed #{"job".pluralize(jobs.count)} running for more than 12 hours. Job IDs: #{jobs.pluck(:id)}"
+      Rails.logger.warn "[JOB QUEUE] #{jobs.count} delayed #{"job".pluralize(jobs.count)} running for more than 12 hours. Original Job IDs: #{jobs.pluck(:id)}"
       MonitoringService.requeue_long_running_jobs(jobs)
     end
   end
