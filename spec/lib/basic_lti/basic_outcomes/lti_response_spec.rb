1254fb6f9915273311038af07350e5bd5f32c852
Paul Shippy <paul.shippy@strongmind.com>
2024-11-14 10:21:46 -0700

    (T) PLT-13623: Add tests to ensure setting of higher score on submission when feature enabled
    Co-authored-by: Jarred Qualls <jarred.qualls@strongmind.com>

--- spec/lib/basic_lti/basic_outcomes/lti_response_spec.rb
+++ spec/lib/basic_lti/basic_outcomes/lti_response_spec.rb
@@ -3,12 +3,20 @@ describe BasicLTI::BasicOutcomes::LtiResponse do
   describe '#create_homework_submission' do
     context 'featured' do
       let(:submission) do
-        Submission.create(excused: false, versions: [version, version2, version3], score: 10, grade: 10)
+        Submission.create(excused: false, versions: [version, version2, version3], score: 100, grade: 100)
       end
 
-      let(:version) { SubmissionVersion.create(yaml: {score: 100, grade: 100, excused: false}.to_yaml) }
-      let(:version2) { SubmissionVersion.create(yaml: {score: 90, grade: 90, excused: false}.to_yaml) }
-      let(:version3) { SubmissionVersion.create(yaml: {score: 80, grade: 80, excused: false}.to_yaml) }
+      let(:version) { SubmissionVersion.create(yaml: { score: 100, grade: 100, excused: false }.to_yaml) }
+      let(:version2) { SubmissionVersion.create(yaml: { score: 90, grade: 90, excused: false }.to_yaml) }
+      let(:version3) { SubmissionVersion.create(yaml: { score: 80, grade: 80, excused: false }.to_yaml) }
+
+      let(:tool_id) { Faker::Number.number(digits: 10) }
+      let(:submission_hash) { Faker::Number.number(digits: 10) }
+      let(:assignment) { Faker::Number.number(digits: 10) }
+      let(:user) { Faker::Number.number(digits: 10) }
+      let(:score) { Faker::Number.between(from: 0, to: 100) }
+      let(:new_score) { score }
+      let(:raw_score) { score }
 
       before do
         allow(PipelineService).to receive(:publish)
@@ -18,24 +26,59 @@ describe BasicLTI::BasicOutcomes::LtiResponse do
 
       it 'should call the shimmed method' do
         expect(subject).to receive(:update_submission_with_best_score)
-        subject.create_homework_submission(1,2,3,4,5,6)
+        subject.create_homework_submission(1, 2, 3, 4, 5, 6)
       end
 
       it 'sets the score and grade to the highest in the submission history' do
-        expect(submission).to receive(:update_columns).with({grade: 100, score: 100, published_grade: 100, published_score: 100})
-        subject.create_homework_submission(1,2,3,4,5,6)
+        expect(submission).to receive(:update_columns).with({ grade: 100, score: 100, published_grade: 100, published_score: 100 })
+        subject.create_homework_submission(1, 2, 3, 4, 5, 6)
       end
 
-      context "no submission" do
+      context "when there are no previous submissions" do
+
+
         before do
           subject.instance_variable_set('@submission', nil)
         end
 
         it 'wont update' do
           expect(submission).to_not receive(:update)
-          subject.create_homework_submission(1,2,3,4,5,6)
+          subject.create_homework_submission(tool_id, submission_hash, assignment, user, new_score, raw_score)
+        end
+
+        it 'has the saved score' do
+          subject.create_homework_submission(tool_id, submission_hash, assignment, user, new_score, raw_score)
+          expect(submission.reload.score).to eq(score)
         end
       end
+
+      context 'when there is a previous submission' do
+        let(:higher_score) { Faker::Number.between(from: 51, to: 100) }
+        let(:lower_score) { Faker::Number.between(from: 0, to: 50) }
+
+        context 'when the previous submission score is higher than the new score' do
+          let(:submission) do
+            Submission.create(excused: false, versions: [version, version2, version3], score: higher_score, grade: higher_score)
+          end
+
+          it 'updates the submission score with the previous submission score' do
+            subject.create_homework_submission(tool_id, submission_hash, assignment, user, lower_score, lower_score)
+            expect(submission.reload.score).to eq(higher_score)
+          end
+        end
+
+        context 'when the previous submission score is lower than the new score' do
+          let(:submission) do
+            Submission.create(excused: false, versions: [version, version2, version3], score: lower_score, grade: lower_score)
+          end
+
+          it 'does not update the submission score' do
+            subject.create_homework_submission(tool_id, submission_hash, assignment, user, higher_score, higher_score)
+            expect(submission.reload.score).to eq(higher_score)
+          end
+        end
+      end
+
     end
 
     context 'unfeatured' do
@@ -45,7 +88,7 @@ describe BasicLTI::BasicOutcomes::LtiResponse do
 
       it 'should not call the shimmed method' do
         expect(subject).to_not receive(:update_submission_with_best_score)
-        subject.create_homework_submission(1,2,3,4,5,6)
+        subject.create_homework_submission(1, 2, 3, 4, 5, 6)
       end
     end
 
@@ -58,7 +101,7 @@ describe BasicLTI::BasicOutcomes::LtiResponse do
       end
       it 'wont update' do
         expect(submission).to_not receive(:update)
-        subject.create_homework_submission(1,2,3,4,5,6)
+        subject.create_homework_submission(1, 2, 3, 4, 5, 6)
       end
     end
 
