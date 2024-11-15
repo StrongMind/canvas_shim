5af92253cc26d8b9349d322ca28f68ae7a3ddb28
Paul Shippy <paul.shippy@strongmind.com>
2024-11-14 22:10:48 -0700

    (B) PLT-13623: Follow pattern seen elsewhere
    - https://blog.daveallie.com/clean-monkey-patching/

--- app/decorators/lib/basic_lti/basic_outcomes_decorator.rb
+++ app/decorators/lib/basic_lti/basic_outcomes_decorator.rb
@@ -1,34 +1,31 @@
 BasicLTI::BasicOutcomes::LtiResponse.class_eval do
   attr_accessor :submission
 
-  def strongmind_create_homework_submission(_tool, submission_hash, assignment, user, new_score, raw_score)
+  alias_method :original_create_homework_submission, :create_homework_submission
+
+  def create_homework_submission(_tool, submission_hash, assignment, user, new_score, raw_score)
     submissions = assignment.all_submissions.where(user_id: user.id)
     if submissions.present?
       @current_score = submissions.last.score
       @current_grade = submissions.last.grade
     end
 
-    instructure_create_homework_submission(_tool, submission_hash, assignment, user, new_score, raw_score)
+    original_create_homework_submission(_tool, submission_hash, assignment, user, new_score, raw_score)
 
     if SettingsService.get_settings(object: :school, id: 1)['lti_keep_highest_score']
       update_submission_with_best_score
     end
   end
 
-  alias_method :instructure_create_homework_submission, :create_homework_submission
-  alias_method :create_homework_submission, :strongmind_create_homework_submission
-
-
   protected
 
-  def strongmind_handle_replaceResult(_tool, _course, assignment, user)
+  alias_method :original_handle_replaceResult, :handle_replaceResult
+
+  def handle_replaceResult(_tool, _course, assignment, user)
     existing_submission = assignment.submissions.where(user_id: user.id).first
     return true if existing_submission&.excused?
-    instructure_handle_replaceResult(_tool, _course, assignment, user)
+    original_handle_replaceResult(_tool, _course, assignment, user)
   end
-
-  alias_method :instructure_handle_replaceResult, :handle_replaceResult
-  alias_method :handle_replaceResult, :strongmind_handle_replaceResult
 
   private
 
