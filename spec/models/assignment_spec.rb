fa618400bc02bb610688013c5cac3288a85711d4
pairingstation <qwertytalk@strongmind.com>
2024-12-02 16:10:34 -0700

    (Spec) QTY-10125: Add specs to check that grade and score are not changed when not excused
    Co-authored-by: Jothan Kelepolo <jothan.kelepolo@strongmind.com>
    Co-authored-by: Spencer Creer <Spencer.Creer@strongmind.com>

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -2,39 +2,54 @@ describe Assignment do
   include_context "stubbed_network"
   let(:assignment) { create(:assignment, :with_assignment_group)}
   let(:user) { User.create }
-  let(:submission) { Submission.create(assignment: assignment, user: user, excused: nil, score: 0.0, grade: 1) }
+  let(:submission) { Submission.create(assignment: assignment, user: user, excused: nil, score: 50, grade: 1) }
   let(:user_2) { User.create }
 
   describe "#toggle_exclusion" do
-    it "flips the excused status to true" do
+    it "sets submission excused to true if is_excused" do
       submission
       assignment.toggle_exclusion(user.id, true)
       submission = assignment.submissions.find_by(user_id: user.id)
       expect(submission.excused).to be true
     end
 
-    it "flips the excused status to false" do
+    it "sets submission score to be nil if is_excused" do
+      submission
+      assignment.toggle_exclusion(user.id, true)
+      submission = assignment.submissions.find_by(user_id: user.id)
+      expect(submission.score).to be nil
+    end
+
+    it "sets submission grade to be nil if is_excused" do
+      submission
+      assignment.toggle_exclusion(user.id, true)
+      submission = assignment.submissions.find_by(user_id: user.id)
+      expect(submission.grade).to be nil
+    end
+
+    it "sets submission excused to false if is_excused is false" do
       submission
       assignment.toggle_exclusion(user.id, false)
       submission = assignment.submissions.find_by(user_id: user.id)
       expect(submission.excused).to be false
     end
 
-    it "returns nil if no user found" do
-      expect(assignment.toggle_exclusion(user_2.id, true)).to be nil
+    it "does not change submission score if is_excused is false" do
+      submission
+      assignment.toggle_exclusion(user.id, false)
+      submission = assignment.submissions.find_by(user_id: user.id)
+      expect(submission.score).to be 50
     end
 
-    it "sets score to be nil if is_excused is true" do
+    it "does not change submission grade to be nil if is_excused is true" do
       submission
-      assignment.toggle_exclusion(user.id, true)
+      assignment.toggle_exclusion(user.id, false)
       submission = assignment.submissions.find_by(user_id: user.id)
-      expect(submission.score).to be nil
+      expect(submission.grade).to be 1
     end
-    it "sets grade to be nil if is_excused is true" do
-      submission
-      assignment.toggle_exclusion(user.id, true)
-      submission = assignment.submissions.find_by(user_id: user.id)
-      expect(submission.grade).to be nil
+
+    it "returns nil if no user found" do
+      expect(assignment.toggle_exclusion(user_2.id, true)).to be nil
     end
   end
 
