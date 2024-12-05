35a345928924e2f923f826430bc60faa8b2b760e
Mark Ng <mark@markng.co.uk>
2024-12-05 09:53:27 -0800

    BEL-3936 Ensure that ECS scaling will not kill running work
    In order to ensure our ECS scaling does not kill running jobs, we need to
    set task protection while workers are doing work.
    
    Do this by sharing a mutex across all running threads, keeping a count of runnings jobs
    in the perform state. When the count gets to 0, unset protected status, and when it increments past 0,
    set protected status.

---
+++ app/decorators/lib/delayed/worker.rb
@@ -0,0 +1,43 @@
+Delayed::Worker.class_eval do
+  @@mutex = Mutex.new
+  @@active_jobs = 0
+
+  alias_method :original_perform, :perform
+
+  def perform(job)
+    @@mutex.synchronize do
+      @@active_jobs += 1
+      Rails.logger.info("Active jobs for worker #{name}: #{@@active_jobs}")
+      set_task_protection(true) if @@active_jobs == 1
+    end
+    original_perform(job)
+    @@mutex.synchronize do
+      @@active_jobs -= 1
+      Rails.logger.info("Active jobs for worker #{name}: #{@@active_jobs}")
+      set_task_protection(false) if @@active_jobs == 0
+    end
+  end
+
+  def set_task_protection(state)
+    begin
+      ecs_agent_uri = ENV['ECS_AGENT_URI']
+      state_endpoint = "#{ecs_agent_uri}/task-protection/v1/state"
+      uri = URI.parse(state_endpoint)
+      request = Net::HTTP::Put.new(uri)
+      request['Content-Type'] = 'application/json'
+      request.body = { 'ProtectionEnabled' => state }.to_json
+
+      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(request)
+      end
+
+      unless response.is_a?(Net::HTTPSuccess)
+        raise "Failed to update ECS task protection state: #{response.body}"
+      end
+
+    rescue StandardError => e
+      raise "An error occurred: #{e.message}"
+    end
+
+  end
+end
